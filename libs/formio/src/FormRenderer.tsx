7fa969e1e198af4faa52c2d4d5486e70fd8fd2e6
Aram Limpens <a.limpens@amsterdam.nl>
2024-03-25 13:52:49 +0100

    Fix style leak

--- libs/formio/src/FormRenderer.tsx
+++ libs/formio/src/FormRenderer.tsx
@@ -3,9 +3,9 @@ import { Components, Templates, Form } from '@formio/react'
 import { Button, Textarea } from './components'
 import { template } from './template'
 
-Components.setComponents({ button: Button, textarea: Textarea })
-Templates.current = template
-
 export function FormRenderer({ form }: any) {
+  Components.setComponents({ button: Button, textarea: Textarea })
+  Templates.current = template
+
   return <Form form={form} />
 }
