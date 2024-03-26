287a0b2343a7251e30c347c7b53723085a15409c
Aram Limpens <a.limpens@amsterdam.nl>
2024-03-26 14:51:31 +0100

    Run linter, fix issues

--- libs/ui/src/Header/Header.tsx
+++ libs/ui/src/Header/Header.tsx
@@ -2,22 +2,20 @@
 
 import { Header as ADSHeader, Grid, PageMenu, SkipLink } from '@amsterdam/design-system-react'
 
-export function Header() {
-  return (
-    <Grid>
-      <Grid.Cell span="all">
-        <SkipLink href="#main">Direct naar inhoud</SkipLink>
-        <ADSHeader
-          logoLinkTitle="Signalen homepage"
-          links={
-            <PageMenu alignEnd>
-              <PageMenu.Link href="#">Doe een melding</PageMenu.Link>
-              <PageMenu.Link href="#">Meldingenkaart</PageMenu.Link>
-              <PageMenu.Link href="#">Mijn meldingen</PageMenu.Link>
-            </PageMenu>
-          }
-        />
-      </Grid.Cell>
-    </Grid>
-  )
-}
+export const Header = () => (
+  <Grid>
+    <Grid.Cell span="all">
+      <SkipLink href="#main">Direct naar inhoud</SkipLink>
+      <ADSHeader
+        logoLinkTitle="Signalen homepage"
+        links={
+          <PageMenu alignEnd>
+            <PageMenu.Link href="#">Doe een melding</PageMenu.Link>
+            <PageMenu.Link href="#">Meldingenkaart</PageMenu.Link>
+            <PageMenu.Link href="#">Mijn meldingen</PageMenu.Link>
+          </PageMenu>
+        }
+      />
+    </Grid.Cell>
+  </Grid>
+)
