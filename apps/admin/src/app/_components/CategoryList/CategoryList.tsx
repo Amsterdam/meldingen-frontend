3185e3af5558b740c220157216b2752f8fa7ac02
Aram Limpens <a.limpens@amsterdam.nl>
2024-04-02 15:54:09 +0200

    Add Create buttons

--- apps/admin/src/app/_components/CategoryList/CategoryList.tsx
+++ apps/admin/src/app/_components/CategoryList/CategoryList.tsx
@@ -3,7 +3,7 @@ import { Datagrid, List, TextField, TextInput } from 'react-admin'
 const categoryFilters = [<TextInput label="Search" source="q" alwaysOn />]
 
 export const CategoryList = () => (
-  <List filters={categoryFilters}>
+  <List filters={categoryFilters} hasCreate>
     <Datagrid rowClick="edit">
       <TextField source="name" />
       <TextField source="id" />
--- apps/admin/src/app/_components/SurveyList/SurveyList.tsx
+++ apps/admin/src/app/_components/SurveyList/SurveyList.tsx
@@ -3,7 +3,7 @@ import { Datagrid, List, TextField, TextInput } from 'react-admin'
 const surveyFilters = [<TextInput label="Search" source="q" alwaysOn />]
 
 export const SurveyList = () => (
-  <List filters={surveyFilters}>
+  <List filters={surveyFilters} hasCreate>
     <Datagrid rowClick="edit">
       <TextField source="name" />
       <TextField source="id" />
