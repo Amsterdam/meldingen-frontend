1bb6db284aa8e8e8bf5b11d7722c3e744fa1a22c
Aram Limpens <a.limpens@amsterdam.nl>
2024-04-16 14:25:56 +0200

    Add builder to Form Edit screen

--- apps/admin/src/app/_components/FormEdit/FormEdit.tsx
+++ apps/admin/src/app/_components/FormEdit/FormEdit.tsx
@@ -1,9 +1,12 @@
 import { Edit, SimpleForm, TextInput } from 'react-admin'
 
+import { Builder } from '../Builder'
+
 export const FormEdit = () => (
   <Edit>
     <SimpleForm>
       <TextInput source="name" />
+      <Builder />
     </SimpleForm>
   </Edit>
 )
