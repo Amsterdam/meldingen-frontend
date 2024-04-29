1fb57ee42263a7f1d8e453e4b1aa34127aba7961
Aram Limpens <a.limpens@amsterdam.nl>
2024-04-29 11:58:26 +0200

    Remove unnecessary actions

--- apps/admin/src/app/_components/FormList/FormList.tsx
+++ apps/admin/src/app/_components/FormList/FormList.tsx
@@ -1,36 +1,9 @@
-import { CreateButton, Datagrid, ExportButton, List, TextField, TextInput, TopToolbar } from 'react-admin'
+import { Datagrid, List, TextField, TextInput } from 'react-admin'
 
 const surveyFilters = [<TextInput label="Search" source="q" alwaysOn />]
 
-const ListActions = () => (
-  <TopToolbar>
-    <CreateButton
-      resource="form"
-      state={{
-        data: {
-          title: 'testula',
-          display: 'form',
-          classification: null,
-          components: [
-            {
-              label: 'string',
-              description: 'string',
-              key: 'string',
-              type: 'string',
-              input: true,
-              autoExpand: true,
-              showCharCount: true,
-            },
-          ],
-        },
-      }}
-    />
-    <ExportButton />
-  </TopToolbar>
-)
-
 export const FormList = () => (
-  <List filters={surveyFilters} actions={<ListActions />}>
+  <List filters={surveyFilters}>
     <Datagrid rowClick="edit">
       <TextField source="id" />
       <TextField source="title" />
