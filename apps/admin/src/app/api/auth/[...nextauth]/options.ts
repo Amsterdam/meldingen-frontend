d23701ded6c5d8e7be16cf275cc8044accbd299d
Vincent <vdegraaf.vincent@gmail.com>
2024-05-03 17:02:54 +0200

    Add refresh token logic

--- apps/admin/src/app/api/auth/[...nextauth]/options.ts
+++ apps/admin/src/app/api/auth/[...nextauth]/options.ts
@@ -1,12 +1,50 @@
 /* eslint-disable no-param-reassign */
 import type { NextAuthOptions } from 'next-auth'
+import type { JWT } from 'next-auth/jwt'
 import KeycloakProvider from 'next-auth/providers/keycloak'
 
-type Options = NextAuthOptions & {
-  accessToken?: string
+/**
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
+const refreshAccessToken = async (token: JWT) => {
+  if (token.refreshTokenExpired && Date.now() > token.refreshTokenExpired) throw new Error()
+
+  try {
+    const response = await fetch(`${process.env.AUTH_ISSUER}/protocol/openid-connect/token`, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams({
+        client_id: process.env.CLIENT_ID || '',
+        client_secret: process.env.CLIENT_SECRET || '',
+        grant_type: 'refresh_token',
+        refresh_token: token.refreshToken || '',
+      }),
+      method: 'POST',
+      cache: 'no-store',
+    })
+
+    const refreshedTokens = await response.json()
+
+    if (!response.ok) {
+      throw refreshedTokens
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpired: Date.now() + refreshedTokens.expires_in * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    }
+  } catch (error) {
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    }
+  }
 }
 
-export const authOptions: Options = {
+export const authOptions: NextAuthOptions = {
   providers: [
     KeycloakProvider({
       clientId: `${process.env.CLIENT_ID}`,
@@ -15,12 +53,24 @@ export const authOptions: Options = {
     }),
   ],
   callbacks: {
-    jwt: async ({ account, token }) => {
-      if (account) {
+    jwt: async ({ account, token, user }) => {
+      if (account && user) {
         // account is only available the first time this callback is called on a new session (after the user signs in)
-        token.accessToken = account.access_token
+        return {
+          accessToken: account.access_token,
+          accessTokenExpired: account.expires_at && account.expires_at * 1000,
+          refreshToken: account.refresh_token,
+          refreshTokenExpired: Date.now() + account.refresh_expires_in * 1000,
+          user,
+        }
       }
-      return token
+
+      if (token.accessTokenExpired && Date.now() < token.accessTokenExpired) {
+        return token
+      }
+
+      // Access token has expired, try to update it
+      return refreshAccessToken(token)
     },
     session: async ({ session, token }) => {
       session.accessToken = token.accessToken
