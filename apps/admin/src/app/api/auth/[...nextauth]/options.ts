012eadafe914c66794ed093dd7e36cc4912d4cdc
Vincent <vdegraaf.vincent@gmail.com>
2024-05-06 09:13:51 +0200

    separate requestAccessToken

--- apps/admin/src/app/api/auth/[...nextauth]/options.ts
+++ apps/admin/src/app/api/auth/[...nextauth]/options.ts
@@ -1,48 +1,8 @@
 /* eslint-disable no-param-reassign */
 import type { NextAuthOptions } from 'next-auth'
-import type { JWT } from 'next-auth/jwt'
 import KeycloakProvider from 'next-auth/providers/keycloak'
 
-/**
- * Takes a token, and returns a new token with updated
- * `accessToken` and `accessTokenExpires`. If an error occurs,
- * returns the old token and an error property
- */
-const refreshAccessToken = async (token: JWT) => {
-  if (token.refreshTokenExpired && Date.now() > token.refreshTokenExpired) throw new Error()
-
-  try {
-    const response = await fetch(`${process.env.AUTH_ISSUER}/protocol/openid-connect/token`, {
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams({
-        client_id: process.env.CLIENT_ID || '',
-        client_secret: process.env.CLIENT_SECRET || '',
-        grant_type: 'refresh_token',
-        refresh_token: token.refreshToken || '',
-      }),
-      method: 'POST',
-      cache: 'no-store',
-    })
-
-    const refreshedTokens = await response.json()
-
-    if (!response.ok) {
-      throw refreshedTokens
-    }
-
-    return {
-      ...token,
-      accessToken: refreshedTokens.access_token,
-      accessTokenExpired: Date.now() + refreshedTokens.expires_in * 1000,
-      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
-    }
-  } catch (error) {
-    return {
-      ...token,
-      error: 'RefreshAccessTokenError',
-    }
-  }
-}
+import { refreshAccessToken } from './refresh-access-token'
 
 export const authOptions: NextAuthOptions = {
   providers: [
