982314bb21d1d256965c70bf505e72fbad322bd4
Aram Limpens <a.limpens@amsterdam.nl>
2024-05-06 12:44:06 +0200

    Move dataProvider to Admin folder

--- apps/admin/src/components/admin/Admin/Admin.tsx
+++ apps/admin/src/components/admin/Admin/Admin.tsx
@@ -1,64 +1,65 @@
-import fakeDataProvider from 'ra-data-fakerest'
+// import fakeDataProvider from 'ra-data-fakerest'
 import { Admin as ReactAdmin, Resource } from 'react-admin'
 
-// import { dataProvider } from '../../../app/dataProvider'
 import { CategoryCreate } from '../../category/CategoryCreate/CategoryCreate'
 import { CategoryEdit } from '../../category/CategoryEdit'
 import { CategoryList } from '../../category/CategoryList'
 import { FormCreate } from '../../form/FormCreate'
 import { FormEdit } from '../../form/FormEdit'
 import { FormList } from '../../form/FormList'
+
+import { dataProvider } from './dataProvider'
 // import { MainForm } from '../MainForm'
 
-const dataProvider = fakeDataProvider({
-  classification: [
-    {
-      name: 'afval',
-      id: 1,
-    },
-    {
-      name: 'afval-container',
-      id: 2,
-    },
-    {
-      name: 'boom-illegale-kap',
-      id: 3,
-    },
-    {
-      name: 'bouw-sloop-overlast',
-      id: 4,
-    },
-  ],
-  form: [
-    {
-      title: 'Afval',
-      id: 1,
-      components: [
-        {
-          label: 'Text Area',
-          description: 'Description text from api',
-          autoExpand: false,
-          showCharCount: false,
-          key: 'textArea',
-          type: 'textarea',
-          input: true,
-        },
-      ],
-    },
-    {
-      title: 'Afval bij container',
-      id: 2,
-    },
-    {
-      title: 'Illegale boomkap',
-      id: 3,
-    },
-    {
-      title: 'Bouw- en sloopoverlast',
-      id: 4,
-    },
-  ],
-})
+// const dataProvider = fakeDataProvider({
+//   classification: [
+//     {
+//       name: 'afval',
+//       id: 1,
+//     },
+//     {
+//       name: 'afval-container',
+//       id: 2,
+//     },
+//     {
+//       name: 'boom-illegale-kap',
+//       id: 3,
+//     },
+//     {
+//       name: 'bouw-sloop-overlast',
+//       id: 4,
+//     },
+//   ],
+//   form: [
+//     {
+//       title: 'Afval',
+//       id: 1,
+//       components: [
+//         {
+//           label: 'Text Area',
+//           description: 'Description text from api',
+//           autoExpand: false,
+//           showCharCount: false,
+//           key: 'textArea',
+//           type: 'textarea',
+//           input: true,
+//         },
+//       ],
+//     },
+//     {
+//       title: 'Afval bij container',
+//       id: 2,
+//     },
+//     {
+//       title: 'Illegale boomkap',
+//       id: 3,
+//     },
+//     {
+//       title: 'Bouw- en sloopoverlast',
+//       id: 4,
+//     },
+//   ],
+// })
 
 export const Admin = () => (
   <ReactAdmin dataProvider={dataProvider}>
