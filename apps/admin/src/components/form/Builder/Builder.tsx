e3ba72ae5de026d35a35fee3f4c3630eabf7d3b8
Vincent <vdegraaf.vincent@gmail.com>
2024-06-03 14:16:57 +0200

    Remove renderer from builder

--- apps/admin/src/components/form/Builder/Builder.tsx
+++ apps/admin/src/components/form/Builder/Builder.tsx
@@ -8,85 +8,77 @@ const FormBuilder = dynamic(() => import('@meldingen/formio').then((mod) => mod.
   loading: () => <p>Loading...</p>,
 })
 
-const FormRenderer = dynamic(() => import('@meldingen/formio').then((mod) => mod.FormRenderer), {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
-})
-
 type BuilderProps = {
   data?: ComponentSchema[]
   onChange: (schema: FormioSchema) => void
 }
 
 export const Builder = ({ data, onChange }: BuilderProps) => (
-  <>
-    <FormBuilder
-      onChange={onChange}
-      form={data ? { display: 'wizard', components: data } : { display: 'wizard' }}
-      options={{
-        noDefaultSubmitButton: true,
-        builder: {
-          custom: {
-            title: 'Input components',
-            weight: 10,
-            components: {
-              textArea: {
-                title: 'text area',
-                key: 'textArea',
-                schema: {
-                  label: 'Custom label',
-                  type: 'textarea',
-                  key: 'textAreaKey',
-                  input: true,
-                },
+  <FormBuilder
+    onChange={onChange}
+    form={data ? { display: 'wizard', components: data } : { display: 'wizard' }}
+    options={{
+      noDefaultSubmitButton: true,
+      builder: {
+        custom: {
+          title: 'Input components',
+          weight: 10,
+          components: {
+            textArea: {
+              title: 'text area',
+              key: 'textArea',
+              schema: {
+                label: 'Custom label',
+                type: 'textarea',
+                key: 'textAreaKey',
+                input: true,
               },
             },
           },
-          basic: false,
-          advanced: false,
-          layout: false,
-          data: false,
-          premium: false,
         },
-        editForm: {
-          textarea: [
-            {
-              key: 'display',
-              components: [
-                {
-                  key: 'applyMaskOn', // Use this to hide fields from edit form panels ('display' in this case)
-                  ignore: true,
-                },
-                {
-                  key: 'displayMask',
-                  ignore: true,
-                },
-                {
-                  key: 'labelPosition',
-                  ignore: true,
-                },
-                {
-                  key: 'tooltip',
-                  ignore: true,
-                },
-                {
-                  key: 'prefix',
-                  ignore: true,
-                },
-                {
-                  key: 'suffix',
-                  ignore: true,
-                },
-              ],
-            },
-            {
-              key: 'api',
-              ignore: true, // Use this to hide entire panels
-            },
-          ],
-        },
-      }}
-    />
-    <FormRenderer form={{ display: 'wizard', components: data }} />
-  </>
+        basic: false,
+        advanced: false,
+        layout: false,
+        data: false,
+        premium: false,
+      },
+      editForm: {
+        textarea: [
+          {
+            key: 'display',
+            components: [
+              {
+                key: 'applyMaskOn', // Use this to hide fields from edit form panels ('display' in this case)
+                ignore: true,
+              },
+              {
+                key: 'displayMask',
+                ignore: true,
+              },
+              {
+                key: 'labelPosition',
+                ignore: true,
+              },
+              {
+                key: 'tooltip',
+                ignore: true,
+              },
+              {
+                key: 'prefix',
+                ignore: true,
+              },
+              {
+                key: 'suffix',
+                ignore: true,
+              },
+            ],
+          },
+          {
+            key: 'api',
+            ignore: true, // Use this to hide entire panels
+          },
+        ],
+      },
+    }}
+  />
 )
