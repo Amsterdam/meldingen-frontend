7a5e7b12dba49d1468dd15dc1d17db8b5c89ba44
Vincent <vdegraaf.vincent@gmail.com>
2024-06-03 12:16:03 +0200

    adjust anvullende vragen route

--- apps/public/src/app/additional-questions/page.tsx
+++ apps/public/src/app/additional-questions/page.tsx
@@ -3,6 +3,8 @@
 import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
 
+import { Grid } from '@meldingen/ui'
+
 const mockCategoryId = '2'
 
 const FormRenderer = dynamic(() => import('@meldingen/formio').then((mod) => mod.FormRenderer), {
@@ -32,7 +34,13 @@ const AdditionalQuestionsPage = () => {
     if (!data) result()
   }, [data])
 
-  return <FormRenderer form={data} />
+  return (
+    <Grid paddingBottom="large" paddingTop="medium">
+      <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 2 }}>
+        <FormRenderer form={data} />
+      </Grid.Cell>
+    </Grid>
+  )
 }
 
 export default AdditionalQuestionsPage
--- apps/public/src/app/page.tsx
+++ apps/public/src/app/page.tsx
@@ -6,8 +6,6 @@ import { Grid } from '@meldingen/ui'
 
 import mockData from '../mocks/wizard-test.json'
 
-import AdditionalQuestionsPage from './additionalQuestions/page'
-
 const FormRenderer = dynamic(() => import('@meldingen/formio').then((mod) => mod.FormRenderer), {
   ssr: false,
   loading: () => <p>Loading...</p>,
@@ -17,7 +15,6 @@ const Home = () => (
   <Grid paddingBottom="large" paddingTop="medium">
     <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 2 }}>
       <FormRenderer form={mockData} />
-      <AdditionalQuestionsPage />
     </Grid.Cell>
   </Grid>
 )
