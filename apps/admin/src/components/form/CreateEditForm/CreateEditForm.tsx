be16a0f73db7d99ec1212bdfc75c69e024283473
Vincent <vdegraaf.vincent@gmail.com>
2024-06-03 13:44:47 +0200

    Move renderer outside form

--- apps/admin/src/components/form/CreateEditForm/CreateEditForm.tsx
+++ apps/admin/src/components/form/CreateEditForm/CreateEditForm.tsx
@@ -1,4 +1,6 @@
+import { Heading } from '@amsterdam/design-system-react'
 import type { ComponentSchema, ExtendedComponentSchema } from 'formiojs'
+import dynamic from 'next/dynamic'
 import { useState } from 'react'
 import {
   useRefresh,
@@ -11,6 +13,8 @@ import {
   required,
 } from 'react-admin'
 
+import { Grid } from '@meldingen/ui'
+
 import type { FormioSchema } from '../../../types/formio'
 import { filterAttributes } from '../../../utils/filterAttributes'
 import { Builder } from '../Builder'
@@ -19,6 +23,10 @@ import { HiddenComponentsInput } from '../HiddenComponentsInput'
 
 import styles from './CreateEditForm.module.css'
 
+const FormRenderer = dynamic(() => import('@meldingen/formio').then((mod) => mod.FormRenderer), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+})
 type CreateEditFormProps = {
   isEditForm?: boolean
 }
@@ -39,23 +47,35 @@ export const CreateEditForm = ({ isEditForm = false }: CreateEditFormProps) => {
   }
 
   return (
-    <SimpleForm
-      toolbar={
-        <Toolbar>
-          <div className={ToolbarClasses.defaultToolbar}>
-            <SaveButton alwaysEnable />
-            {isEditForm && <DeleteWithConfirmButton />}
-          </div>
-        </Toolbar>
-      }
-    >
-      <TextInput source="title" validate={required()} />
-      <TextInput source="display" defaultValue="wizard" hidden />
-      <CategoryInput />
-      <HiddenComponentsInput value={builderJson} setInitialValue={setInitialValue} />
-      <div className={styles.builder}>
-        <Builder data={initialValue} onChange={onChange} />
-      </div>
-    </SimpleForm>
+    <>
+      <SimpleForm
+        toolbar={
+          <Toolbar>
+            <div className={ToolbarClasses.defaultToolbar}>
+              <SaveButton alwaysEnable />
+              {isEditForm && <DeleteWithConfirmButton />}
+            </div>
+          </Toolbar>
+        }
+      >
+        <TextInput source="title" validate={required()} />
+        <TextInput source="display" defaultValue="wizard" hidden />
+        <CategoryInput />
+        <HiddenComponentsInput value={builderJson} setInitialValue={setInitialValue} />
+        <div className={styles.builder}>
+          <Builder data={initialValue} onChange={onChange} />
+        </div>
+      </SimpleForm>
+      {builderJson && (
+        <>
+          <Heading size="level-2">Preview</Heading>
+          <Grid paddingBottom="large" paddingTop="medium">
+            <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 2 }}>
+              <FormRenderer form={{ display: 'wizard', components: builderJson }} />
+            </Grid.Cell>
+          </Grid>
+        </>
+      )}
+    </>
   )
 }
