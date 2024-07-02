f24daeee2c9c4bddbf1fae572a952aabcb45c5ed
alimpens <a.limpens@amsterdam.nl>
2024-07-02 16:25:41 +0200

    Add type to FormRenderer

--- libs/formio/src/FormRenderer.tsx
+++ libs/formio/src/FormRenderer.tsx
@@ -1,9 +1,13 @@
 import { Components, Templates, Form } from '@formio/react'
+import type { FormProps } from '@formio/react/lib/components/Form'
 
 import { Button, Textarea } from './components'
 import { template } from './template'
 
-export const FormRenderer = (props: any) => {
+// options.language isn't defined in FormProps, so we add it here
+type FormRendererProps = FormProps & { options: { language: string } }
+
+export const FormRenderer = (props: FormRendererProps) => {
   Components.setComponents({ button: Button, textarea: Textarea })
   Templates.current = template
 
