ca140b2ec915cbcf47a93e10850388f459d4a177
Vincent <vdegraaf.vincent@gmail.com>
2024-07-05 10:58:55 +0200

    Remove next-auth references

--- apps/admin/src/components/admin/Admin/LogoutButton.tsx
+++ apps/admin/src/components/admin/Admin/LogoutButton.tsx
@@ -1,6 +1,5 @@
 import ExitIcon from '@mui/icons-material/PowerSettingsNew'
 import { MenuItem } from '@mui/material'
-import { signOut } from 'next-auth/react'
 import { forwardRef } from 'react'
 import type { Ref } from 'react'
 import { useTranslate } from 'react-admin'
@@ -8,7 +7,7 @@ import { useTranslate } from 'react-admin'
 export const LogoutButton = forwardRef((props, ref: Ref<HTMLAnchorElement>) => {
   const translate = useTranslate()
   return (
-    <MenuItem href="#" onClick={() => signOut()} ref={ref} {...props}>
+    <MenuItem href="#" onClick={() => console.log('signOut§')} ref={ref} {...props}>
       <ExitIcon /> {translate('ra.auth.logout')}
     </MenuItem>
   )
