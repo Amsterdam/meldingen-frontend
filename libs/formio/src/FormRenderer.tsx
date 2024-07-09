ba36cd3ef75938230a3e020927d9350daf120928
alimpens <a.limpens@amsterdam.nl>
2024-07-09 14:32:17 +0200

    Add Textfield component

--- libs/formio/src/FormRenderer.tsx
+++ libs/formio/src/FormRenderer.tsx
@@ -1,10 +1,11 @@
 import { Components, Templates, Form } from '@formio/react'
 
 import { Button, Textarea } from './components'
+import { Textfield } from './components/Textfield'
 import { template } from './template'
 
 export const FormRenderer = ({ form, onSubmit }: any) => {
-  Components.setComponents({ button: Button, textarea: Textarea })
+  Components.setComponents({ button: Button, textarea: Textarea, textfield: Textfield })
   Templates.current = template
 
   return <Form form={form} onSubmit={onSubmit} />
