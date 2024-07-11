806816b722b144045f29686c463b75c42b12970f
Vincent <vdegraaf.vincent@gmail.com>
2024-07-11 12:59:18 +0200

    Pr feedback

--- apps/admin/src/components/admin/Admin/Admin.tsx
+++ apps/admin/src/components/admin/Admin/Admin.tsx
@@ -1,10 +1,3 @@
-// import fakeDataProvider from 'ra-data-fakerest'
-
-import type { KeycloakConfig, KeycloakInitOptions } from 'keycloak-js'
-import Keycloak from 'keycloak-js'
-import { keycloakAuthProvider, httpClient } from 'ra-keycloak'
-import { useEffect, useRef, useState } from 'react'
-import type { AuthProvider, DataProvider } from 'react-admin'
 import { Admin as ReactAdmin, Resource } from 'react-admin'
 
 import { CategoryCreate } from '../../category/CategoryCreate/CategoryCreate'
@@ -15,121 +8,12 @@ import { FormEdit } from '../../form/FormEdit'
 import { FormList } from '../../form/FormList'
 
 import { CustomLayout } from './CustomLayout'
-import { dataProvider } from './dataProvider'
 import { i18nProvider } from './i18nProvider'
-
-// const dataProvider = fakeDataProvider({
-//   classification: [
-//     {
-//       name: 'afval',
-//       id: 1,
-//       form: 1,
-//     },
-//     {
-//       name: 'afval-container',
-//       id: 2,
-//       form: 2,
-//     },
-//     {
-//       name: 'boom-illegale-kap',
-//       id: 3,
-//       form: null,
-//     },
-//     {
-//       name: 'bouw-sloop-overlast',
-//       id: 4,
-//       form: null,
-//     },
-//   ],
-//   form: [
-//     {
-//       title: 'Afval',
-//       id: 1,
-//       classification: 1,
-//       components: [
-//         {
-//           label: 'Text Area',
-//           description: 'Description text from api',
-//           autoExpand: false,
-//           showCharCount: false,
-//           key: 'textArea',
-//           type: 'textarea',
-//           input: true,
-//         },
-//       ],
-//     },
-//     {
-//       title: 'Afval bij container',
-//       id: 2,
-//       classification: 2,
-//     },
-//     {
-//       title: 'Illegale boomkap',
-//       id: 3,
-//       classification: null,
-//     },
-//     {
-//       title: 'Bouw- en sloopoverlast',
-//       id: 4,
-//       classification: null,
-//     },
-//   ],
-// })
-
-const config: KeycloakConfig = {
-  url: 'http://localhost:8002',
-  realm: 'meldingen',
-  clientId: `meldingen`,
-}
-
-// here you can set options for the keycloak client
-const initOptions: KeycloakInitOptions = { onLoad: 'login-required', checkLoginIframe: false }
+import { useAuthProvider } from './useAuthProvider'
 
 export const Admin = () => {
-  const [keycloak, setKeycloak] = useState<Keycloak | undefined>(undefined)
-  const authProvider = useRef<AuthProvider | undefined>(undefined)
-  const dataProviderRef = useRef<DataProvider | undefined>()
+  const { keycloak, dataProviderRef, authProvider } = useAuthProvider()
 
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      const initKeyCloakClient = async () => {
-        const keycloakClient = new Keycloak(config)
-
-        keycloakClient.onTokenExpired = () => {
-          keycloakClient
-            .updateToken(5)
-            .then((refreshed) => {
-              if (refreshed) {
-                // eslint-disable-next-line no-console
-                console.log('Token refreshed', refreshed)
-              } else {
-                // eslint-disable-next-line no-console
-                console.log('Token not refreshed')
-              }
-            })
-            .catch(() => {
-              // eslint-disable-next-line no-console
-              console.error('Failed to refresh token')
-              keycloakClient.login()
-            })
-        }
-
-        await keycloakClient.init(initOptions)
-
-        authProvider.current = keycloakAuthProvider(keycloakClient)
-        dataProviderRef.current = dataProvider(httpClient(keycloakClient), process.env.BACKEND_URL)
-
-        setKeycloak(keycloakClient)
-      }
-      if (!keycloak) {
-        initKeyCloakClient()
-      }
-    }, 0)
-
-    return () => clearTimeout(timerId)
-  }, [keycloak])
-
-  // hide the admin until the keycloak client is ready
   if (!keycloak) return <p>Loading...</p>
 
   return (
---
+++ apps/admin/src/components/admin/Admin/useAuthProvider.ts
@@ -0,0 +1,52 @@
+import type { KeycloakConfig, KeycloakInitOptions } from 'keycloak-js'
+import Keycloak from 'keycloak-js'
+import { keycloakAuthProvider, httpClient } from 'ra-keycloak'
+import { useEffect, useRef, useState } from 'react'
+import type { AuthProvider, DataProvider } from 'react-admin'
+
+import { dataProvider } from './dataProvider'
+
+const initOptions: KeycloakInitOptions = { onLoad: 'login-required', checkLoginIframe: false }
+// TODO: use .env variables
+const config: KeycloakConfig = {
+  url: 'http://localhost:8002',
+  realm: 'meldingen',
+  clientId: `meldingen`,
+}
+
+export const useAuthProvider = () => {
+  const [keycloak, setKeycloak] = useState<Keycloak | undefined>(undefined)
+  const authProvider = useRef<AuthProvider | undefined>(undefined)
+  const dataProviderRef = useRef<DataProvider | undefined>()
+
+  useEffect(() => {
+    // setTimeout is needed to prevent an infinite refresh loop caused by keyCloakClient.init()
+    const timerId = setTimeout(() => {
+      const initKeyCloakClient = async () => {
+        const keycloakClient = new Keycloak(config)
+
+        keycloakClient.onTokenExpired = () => {
+          keycloakClient.updateToken().catch(() => {
+            // eslint-disable-next-line no-console
+            console.error('Failed to refresh token')
+            keycloakClient.login()
+          })
+        }
+
+        await keycloakClient.init(initOptions)
+
+        authProvider.current = keycloakAuthProvider(keycloakClient)
+        dataProviderRef.current = dataProvider(httpClient(keycloakClient))
+
+        setKeycloak(keycloakClient)
+      }
+      if (!keycloak) {
+        initKeyCloakClient()
+      }
+    }, 0)
+
+    return () => clearTimeout(timerId)
+  }, [keycloak])
+
+  return { keycloak, dataProviderRef, authProvider }
+}
