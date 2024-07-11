0dd5d2f14c0d73b1a112637b7ead53687d6e72ef
Vincent de Graaf <vdegraaf.vincent@gmail.com>
2024-07-11 13:46:15 +0200

    [5989]: change authentication (#264)

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,15 +1,12 @@
 'use client'
 
 import dynamic from 'next/dynamic'
-import { SessionProvider } from 'next-auth/react'
 
 const Admin = dynamic(() => import('../components/admin/Admin').then((mod) => mod.Admin), { ssr: false })
 
 const Home = () => (
   <main>
-    <SessionProvider>
-      <Admin />
-    </SessionProvider>
+    <Admin />
   </main>
 )
 
--- apps/admin/src/components/admin/Admin/CustomAppBar.tsx
+++ apps/admin/src/components/admin/Admin/CustomAppBar.tsx
@@ -1,10 +1,7 @@
 import { AppBar, TitlePortal } from 'react-admin'
 
-import { LogoutButton } from './LogoutButton'
-
 export const CustomAppBar = () => (
   <AppBar>
     <TitlePortal />
-    <LogoutButton />
   </AppBar>
 )
