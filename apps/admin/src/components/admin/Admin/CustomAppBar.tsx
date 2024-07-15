de36158ea60dfdfaffac4761501ce3661870d1ea
Vincent <vdegraaf.vincent@gmail.com>
2024-07-15 11:36:07 +0200

    Remove next admin application

--- apps/admin/src/components/admin/Admin/CustomAppBar.tsx
+++
@@ -1,7 +0,0 @@
-import { AppBar, TitlePortal } from 'react-admin'
-
-export const CustomAppBar = () => (
-  <AppBar>
-    <TitlePortal />
-  </AppBar>
-)
--- apps/admin/src/components/form/Builder/translations/README.md
+++
@@ -1,3 +0,0 @@
-# Translations
-
-Sourced from <https://github.com/open-formulieren/open-forms/blob/master/src/openforms/js/lang/formio/nl.json>
