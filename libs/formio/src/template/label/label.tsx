ab35845ab2f03477ccecdecdfb19a94371f7a27b
alimpens <a.limpens@amsterdam.nl>
2024-07-16 13:57:10 +0200

    Only inputs should have labels

--- libs/formio/src/template/label/label.tsx
+++ libs/formio/src/template/label/label.tsx
@@ -1,11 +1,11 @@
-export const label = (ctx: any) =>
-  ctx.component.label &&
-  `
-  <label
-    ref="label"
-    class="${ctx.label.className} ams-label"
-    for="${ctx.instance.id}-${ctx.component.key}"
-  >
-    ${ctx.t(ctx.component.label, { _userInput: true })}
-  </label>
+const labelMarkup = (ctx: any) => `
+<label
+  ref="label"
+  class="ams-label"
+  for="${ctx.instance.id}-${ctx.component.key}"
+>
+  ${ctx.t(ctx.component.label, { _userInput: true })}
+</label>
 `
+
+export const label = (ctx: any) => (ctx.component.input ? labelMarkup(ctx) : '')
