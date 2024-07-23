82e878441b206cb1cb2bf8ed952200d5b3d39d6d
alimpens <a.limpens@amsterdam.nl>
2024-07-23 16:28:26 +0200

    Filter new components

--- apps/admin/src/utils/filterFormResponse.ts
+++ apps/admin/src/utils/filterFormResponse.ts
@@ -1,6 +1,9 @@
 const allowLists: Record<string, string[]> = {
   panel: ['label', 'key', 'type', 'input', 'components'],
+  radio: ['label', 'description', 'key', 'type', 'input', 'values'],
+  selectboxes: ['label', 'description', 'key', 'type', 'input', 'inputType', 'values'],
   textarea: ['label', 'description', 'key', 'type', 'input', 'autoExpand', 'showCharCount'],
+  textfield: ['label', 'description', 'key', 'type', 'input'],
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
