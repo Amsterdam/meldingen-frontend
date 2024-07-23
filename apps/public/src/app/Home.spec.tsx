7919564fd1a17a5d7f9e7e61f7427083f908f276
Aram <37216945+alimpens@users.noreply.github.com>
2024-07-23 09:56:53 +0200

    Implement MSW (#318)

--- apps/public/src/app/Home.spec.tsx
+++ apps/public/src/app/Home.spec.tsx
@@ -1,30 +1,34 @@
 import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { http, HttpResponse } from 'msw'
+import { setupServer } from 'msw/node'
 
 import { MeldingContextProvider } from '../context/MeldingContextProvider'
 import { NextRouterContextProviderMock } from '../mocks/NextRouterContextProviderMock'
 
 import { Home } from './Home'
 
-const mockInput = 'This is a test input'
-const mockLabel = 'Waar gaat het om?'
+const mockInput = 'This is test user input'
+const mockQuestionText = 'What is it about?'
 
-jest.mock('@meldingen/api-client', () => ({
-  __esModule: true,
-  ...jest.requireActual('@meldingen/api-client'),
-  postMelding: () =>
-    Promise.resolve({
-      id: 35,
-      created_at: '2024-07-01T08:54:59.731119',
-      updated_at: '2024-07-01T08:54:59.884144',
-      text: mockInput,
-      state: 'new',
-      classification: null,
-      token: 'qHaOTJy4j_wwIw_pHT1xqTEmpUO3tHkQYLXtLC6Gp58',
-    }),
-}))
+const server = setupServer(
+  http.post('http://localhost:8000/melding', async ({ request }) => {
+    const data = (await request.json()) as { text: string }
+
+    // Check if request payload equals input. If not, throw an error.
+    if (data?.text !== mockInput) {
+      return new HttpResponse('Incorrect body text', { status: 400 })
+    }
+
+    return new HttpResponse('Succesful response')
+  }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 const mockFormData = {
-  title: 'Hoofd formulier',
+  title: 'Hoofdformulier',
   display: 'form',
   created_at: '2024-07-15T14:00:56.112771',
   updated_at: '2024-07-15T14:00:56.112771',
@@ -32,8 +36,8 @@ const mockFormData = {
   components: [
     {
       type: 'textarea',
-      key: 'waar-gaat-het-om',
-      label: mockLabel,
+      key: 'what',
+      label: mockQuestionText,
       input: true,
       inputType: 'text',
       showCharCount: false,
@@ -44,7 +48,7 @@ const mockFormData = {
 }
 
 const push = jest.fn()
-const renderComponent = () => {
+const renderPage = () => {
   render(
     <MeldingContextProvider>
       <NextRouterContextProviderMock router={{ push }}>
@@ -56,25 +60,21 @@ const renderComponent = () => {
 
 describe('Page', () => {
   it('should render a form', async () => {
-    renderComponent()
+    renderPage()
 
     await waitFor(
       () => {
-        expect(screen.getByRole('textbox', { name: mockLabel })).toBeInTheDocument()
+        expect(screen.getByRole('textbox', { name: mockQuestionText })).toBeInTheDocument()
         expect(screen.getByRole('button')).toBeInTheDocument()
       },
       { timeout: 2000 },
     )
   })
 
-  it('should send a filled form', async () => {
-    renderComponent()
+  it('should send a filled form and navigate to /aanvullende-vragen', async () => {
+    renderPage()
 
-    await waitFor(() => {
-      screen.getByRole('textbox', { name: 'Waar gaat het om?' })
-    })
-
-    const input = screen.getByRole('textbox', { name: 'Waar gaat het om?' })
+    const input = screen.getByRole('textbox', { name: mockQuestionText })
 
     act(() => {
       // TODO: try to find a more realistic way to input a value in FormIO
@@ -92,4 +92,6 @@ describe('Page', () => {
       expect(push).toHaveBeenCalledWith('/aanvullende-vragen')
     })
   })
+
+  it.skip('should set context with the right values', () => {})
 })
--- apps/public/src/app/aanvullende-vragen/aanvullende-vragen.spec.tsx
+++ apps/public/src/app/aanvullende-vragen/aanvullende-vragen.spec.tsx
@@ -1,15 +1,44 @@
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { http, HttpResponse } from 'msw'
+import { setupServer } from 'msw/node'
 
+import { MeldingContext } from '../../context/MeldingContextProvider'
+import mockFormData from '../../mocks/mockFormData.json'
 import { NextRouterContextProviderMock } from '../../mocks/NextRouterContextProviderMock'
 
 import Page from './page'
 
+const mockFirstQuestionText = 'First question'
+const mockSecondQuestionText = 'Second question'
+const mockInput = 'This is test user input'
+const mockContextValue = {
+  data: {
+    id: 2,
+    token: 'test',
+    classification: 1,
+  },
+  setData: () => {},
+}
+
+const server = setupServer(
+  http.get('http://localhost:8000/form/classification/1', () => HttpResponse.json(mockFormData)),
+  http.post('http://localhost:8000/melding/2/question/1', () => new HttpResponse()),
+  http.put('http://localhost:8000/melding/2/answer_questions', () => new HttpResponse()),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
 const push = jest.fn()
 const renderPage = () => {
   render(
-    <NextRouterContextProviderMock router={{ push }}>
-      <Page />
-    </NextRouterContextProviderMock>,
+    <MeldingContext.Provider value={mockContextValue}>
+      <NextRouterContextProviderMock router={{ push }}>
+        <Page />
+      </NextRouterContextProviderMock>
+    </MeldingContext.Provider>,
   )
 }
 
@@ -18,7 +47,7 @@ describe('AanvullendeVragen', () => {
     renderPage()
 
     await waitFor(() => {
-      expect(screen.getByRole('textbox', { name: 'Waar is het?' })).toBeInTheDocument()
+      expect(screen.getByRole('textbox', { name: mockFirstQuestionText })).toBeInTheDocument()
     })
   })
 
@@ -37,20 +66,20 @@ describe('AanvullendeVragen', () => {
   })
 
   it('should navigate to home page after click on back link on first page', async () => {
+    const user = userEvent.setup()
+
     renderPage()
 
     const backLink = screen.getByRole('link', { name: 'Vorige vraag' })
 
-    act(() => {
-      fireEvent.click(backLink)
-    })
+    await user.click(backLink)
 
-    await waitFor(() => {
-      expect(push).toHaveBeenCalledWith('/', { scroll: true })
-    })
+    expect(push).toHaveBeenCalledWith('/', { scroll: true })
   })
 
   it('should navigate to second page after click on submit button on first page', async () => {
+    const user = userEvent.setup()
+
     renderPage()
 
     await waitFor(() => {
@@ -59,16 +88,14 @@ describe('AanvullendeVragen', () => {
 
     const submitButton = screen.getByRole('button', { name: 'Volgende vraag' })
 
-    act(() => {
-      fireEvent.click(submitButton)
-    })
+    await user.click(submitButton)
 
-    await waitFor(() => {
-      expect(screen.getByRole('textbox', { name: 'Wanneer was het?' })).toBeInTheDocument()
-    })
+    expect(screen.getByRole('textbox', { name: mockSecondQuestionText })).toBeInTheDocument()
   })
 
   it('should navigate to first page after click on back link on second page', async () => {
+    const user = userEvent.setup()
+
     renderPage()
 
     await waitFor(() => {
@@ -77,22 +104,84 @@ describe('AanvullendeVragen', () => {
 
     const submitButton = screen.getByRole('button', { name: 'Volgende vraag' })
 
-    act(() => {
-      fireEvent.click(submitButton)
-    })
+    await user.click(submitButton)
 
-    await waitFor(() => {
-      expect(screen.getByRole('textbox', { name: 'Wanneer was het?' })).toBeInTheDocument()
-    })
+    expect(screen.getByRole('textbox', { name: mockSecondQuestionText })).toBeInTheDocument()
 
     const backLink = screen.getByRole('link', { name: 'Vorige vraag' })
 
-    act(() => {
-      fireEvent.click(backLink)
-    })
+    await user.click(backLink)
+
+    expect(screen.getByRole('textbox', { name: mockFirstQuestionText })).toBeInTheDocument()
+  })
+
+  it.skip('should fall back on the default form when melding is not classified', () => {})
+
+  it('submits answer to first question', async () => {
+    server.use(
+      http.post('http://localhost:8000/melding/2/question/1', async ({ request }) => {
+        const data = (await request.json()) as { text: string }
+
+        // Check if request payload equals input. If not, throw an error.
+        if (data?.text !== mockInput) {
+          return new HttpResponse('Incorrect body text', { status: 400 })
+        }
+
+        return new HttpResponse('Succesful response')
+      }),
+    )
+
+    const user = userEvent.setup()
+
+    renderPage()
 
     await waitFor(() => {
-      expect(screen.getByRole('textbox', { name: 'Waar is het?' })).toBeInTheDocument()
+      expect(screen.getByRole('textbox', { name: mockFirstQuestionText })).toBeInTheDocument()
     })
+
+    const input = screen.getByRole('textbox', { name: mockFirstQuestionText })
+
+    await user.type(input, mockInput)
+
+    const nextButton = screen.getByRole('button', { name: 'Volgende vraag' })
+
+    await user.click(nextButton)
+  })
+
+  it('submits answer to last question', async () => {
+    server.use(
+      http.post('http://localhost:8000/melding/2/question/2', async ({ request }) => {
+        const data = (await request.json()) as { text: string }
+        // Check if request payload equals input. If not, throw an error.
+        if (data?.text !== mockInput) {
+          return new HttpResponse('Incorrect body text', { status: 400 })
+        }
+        return new HttpResponse('Succesful response')
+      }),
+    )
+
+    const user = userEvent.setup()
+
+    renderPage()
+
+    await waitFor(() => {
+      expect(screen.getByRole('button', { name: 'Volgende vraag' })).toBeInTheDocument()
+    })
+
+    const nextButton = screen.getByRole('button', { name: 'Volgende vraag' })
+
+    await user.click(nextButton)
+
+    expect(screen.getByRole('textbox', { name: mockSecondQuestionText })).toBeInTheDocument()
+
+    const input = screen.getByRole('textbox', { name: mockSecondQuestionText })
+
+    await user.type(input, mockInput)
+
+    const submitButton = screen.getByRole('button', { name: 'Volgende vraag' })
+
+    await user.click(submitButton)
+
+    expect(push).toHaveBeenCalledWith('/bedankt')
   })
 })
