d1538eb22a143d4a788035c39cf7d33e23af0fe3
Vincent <vdegraaf.vincent@gmail.com>
2024-08-26 16:04:04 +0200

    Add editForm to textField

--- libs/formio/src/components/Textfield/Textfield.tsx
+++ libs/formio/src/components/Textfield/Textfield.tsx
@@ -11,4 +11,63 @@ export class Textfield extends FormioTextfield {
 
     return inputInfo
   }
+
+  static editForm() {
+    return {
+      components: [
+        {
+          type: 'tabs',
+          keys: 'tabs',
+          components: [
+            {
+              key: 'display',
+              label: 'Display',
+              components: [
+                {
+                  weight: 0,
+                  type: 'textfield',
+                  input: true,
+                  key: 'label',
+                  label: 'Label',
+
+                  validate: {
+                    required: true,
+                  },
+                  autofocus: true,
+                },
+              ],
+            },
+            {
+              key: 'validation',
+              label: 'Validation',
+              components: [
+                {
+                  type: 'panel',
+                  title: 'JSONLogic Validation',
+                  key: 'json-validation-json',
+                  weight: 400,
+                  components: [
+                    {
+                      type: 'htmlelement',
+                      tag: 'div',
+                      content: '<h6>MinLength:</h6><pre>{"<": [{"var": "value"}, minLength]}</pre>',
+                    },
+                    {
+                      type: 'textarea',
+                      key: 'validate.json',
+                      hideLabel: true,
+                      rows: 5,
+                      editor: 'ace',
+                      as: 'json',
+                      input: true,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    }
+  }
 }
