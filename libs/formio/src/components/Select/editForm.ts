47ffa7b6b5cd86cf4858cff344d00914fbd3ce59
alimpens <a.limpens@amsterdam.nl>
2024-09-02 10:58:15 +0200

    Add Select edit form config

---
+++ libs/formio/src/components/Select/editForm.ts
@@ -0,0 +1,136 @@
+// Sourced from:
+// - https://github.com/formio/formio.js/blob/master/src/components/_classes/component/editForm/
+// - https://github.com/formio/formio.js/blob/master/src/components/select/editForm/
+
+export const editForm = () => ({
+  components: [
+    {
+      type: 'tabs',
+      keys: 'tabs',
+      weight: 0,
+      components: [
+        {
+          key: 'display',
+          label: 'Display',
+          components: [
+            {
+              weight: 0,
+              type: 'textfield',
+              input: true,
+              key: 'label',
+              label: 'Label',
+              placeholder: 'Field Label',
+              tooltip: 'The label for this field that will appear next to it.',
+              validate: {
+                required: true,
+              },
+              autofocus: true,
+            },
+            {
+              type: 'select',
+              input: true,
+              weight: 20,
+              tooltip: "Select the type of widget you'd like to use.",
+              key: 'widget',
+              defaultValue: 'html5',
+              label: 'Widget Type',
+              dataSrc: 'values',
+              data: {
+                values: [{ label: 'HTML 5', value: 'html5' }],
+              },
+            },
+            {
+              weight: 100,
+              type: 'textfield',
+              input: true,
+              key: 'placeholder',
+              label: 'Placeholder',
+              placeholder: 'Placeholder',
+              tooltip: 'The placeholder text that will appear when this field is empty.',
+            },
+            {
+              weight: 200,
+              type: 'textarea',
+              input: true,
+              key: 'description',
+              label: 'Description',
+              placeholder: 'Description for this field.',
+              tooltip: 'The description is text that will appear below the input field.',
+              editor: 'ace',
+              as: 'html',
+              wysiwyg: {
+                minLines: 3,
+                isUseWorkerDisabled: true,
+              },
+            },
+          ],
+        },
+        {
+          label: 'Data',
+          key: 'data',
+          components: [
+            {
+              type: 'datagrid',
+              input: true,
+              label: 'Data Source Values',
+              key: 'data.values',
+              weight: 10,
+              reorder: true,
+              defaultValue: [{ label: '', value: '' }],
+              validate: {
+                required: true,
+              },
+              components: [
+                {
+                  label: 'Label',
+                  key: 'label',
+                  input: true,
+                  type: 'textfield',
+                },
+                {
+                  label: 'Value',
+                  key: 'value',
+                  input: true,
+                  type: 'textfield',
+                  allowCalculateOverride: true,
+                  calculateValue: 'value = _.camelCase(row.label);',
+                },
+              ],
+              conditional: {
+                json: { '===': [{ var: 'data.dataSrc' }, 'values'] },
+              },
+            },
+          ],
+        },
+        {
+          key: 'validation',
+          label: 'Validation',
+          components: [
+            {
+              type: 'panel',
+              title: 'JSONLogic Validation',
+              key: 'json-validation-json',
+              weight: 400,
+              components: [
+                {
+                  type: 'htmlelement',
+                  tag: 'div',
+                  content: '<h6>MinLength:</h6><pre>{"<": [{"var": "value"}, minLength]}</pre>',
+                },
+                {
+                  type: 'textarea',
+                  key: 'validate.json',
+                  hideLabel: true,
+                  rows: 5,
+                  editor: 'ace',
+                  as: 'json',
+                  input: true,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+})
--- libs/formio/src/components/SelectBoxes/SelectBoxes.tsx
+++ libs/formio/src/components/SelectBoxes/SelectBoxes.tsx
@@ -5,14 +5,5 @@ import { SelectBoxesEditForm } from './editForm'
 const FormioSelectBoxes = (Components as any).components.selectboxes
 
 export class SelectBoxes extends FormioSelectBoxes {
-  get inputInfo() {
-    const inputInfo = super.inputInfo
-
-    inputInfo.attr.class = 'ams-checkbox'
-    inputInfo.attr.dir = 'auto'
-
-    return inputInfo
-  }
-
   static editForm = SelectBoxesEditForm
 }
