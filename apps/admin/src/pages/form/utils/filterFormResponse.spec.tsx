ee84ce7760801b66fdc0f187502c387212a8b6b8
alimpens <a.limpens@amsterdam.nl>
2024-10-14 09:44:02 +0200

    Update mock data, update snapshot

--- apps/admin/src/pages/form/utils/filterFormResponse.spec.tsx
+++ apps/admin/src/pages/form/utils/filterFormResponse.spec.tsx
@@ -1,5 +1,5 @@
 import { filterFormResponse } from './filterFormResponse'
-import { mockData } from './filterFormResponseMocks'
+import { mockData } from './filterFormResponseMock'
 
 // This test compares the output of the function to a snapshot.
 // To update the snapshot, run the following command:
