84aabc9d542e1dfa28e1a87ca5ad2af0b87e0a4a
Vincent <vdegraaf.vincent@gmail.com>
2024-10-18 14:31:06 +0200

    Make accessible

--- apps/public/src/app/bijlagen/_components/file-upload.tsx
+++ apps/public/src/app/bijlagen/_components/file-upload.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@amsterdam/design-system-react'
 import { useRef } from 'react'
 
 import styles from './file-upload.module.css'
@@ -9,21 +10,23 @@ export const FileUpload = () => {
   }
 
   const inputRef = useRef<HTMLInputElement>(null)
+
   return (
     <div className={styles['file-upload-wrapper']}>
       <label htmlFor="file-input">
-        <span className={styles['file-upload-button']}>Selecteer bestanden</span>
+        <Button className={styles['file-upload-button']} variant="secondary" onClick={() => inputRef.current?.click()}>
+          Selecteer bestanden
+        </Button>
         <span className={styles['file-upload-drop-area-text']}>Of sleep de bestanden in dit vak.</span>
 
-        <span className={styles['file-upload-input']}>
-          <input
-            ref={inputRef}
-            type="file"
-            id="file-input"
-            className={styles['file-upload-input']}
-            onChange={handleOnChange}
-          />
-        </span>
+        <input
+          ref={inputRef}
+          type="file"
+          id="file-input"
+          className={styles['file-upload-input']}
+          onChange={handleOnChange}
+          tabIndex={-1}
+        />
       </label>
     </div>
   )
