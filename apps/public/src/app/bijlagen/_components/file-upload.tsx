421dffb4987d506b1a0310e479ea39d28e6bdaa8
Vincent <vdegraaf.vincent@gmail.com>
2024-10-18 12:06:14 +0200

    Update styling

--- apps/public/src/app/bijlagen/_components/file-upload.tsx
+++ apps/public/src/app/bijlagen/_components/file-upload.tsx
@@ -1,6 +1,5 @@
-import { Button } from '@amsterdam/design-system-react'
+import { useRef } from 'react'
 
-// @ts-expect-error: Should expect string
 import styles from './file-upload.module.css'
 
 export const FileUpload = () => {
@@ -9,15 +8,22 @@ export const FileUpload = () => {
     console.log('Accepted files: ', acceptedFiles.target.files)
   }
 
+  const inputRef = useRef<HTMLInputElement>(null)
   return (
     <div className={styles['file-upload-wrapper']}>
       <label htmlFor="file-input">
-        <Button variant="secondary" className={styles['file-upload-button']}>
-          Selecteer bestanden
-        </Button>
-        <span className={styles['file-upload-drop-area']}>Of sleep de bestanden in dit vak.</span>
+        <span className={styles['file-upload-button']}>Selecteer bestanden</span>
+        <span className={styles['file-upload-drop-area-text']}>Of sleep de bestanden in dit vak.</span>
 
-        <input type="file" id="file-input" className={styles['file-upload-input']} onChange={handleOnChange} />
+        <span className={styles['file-upload-input']}>
+          <input
+            ref={inputRef}
+            type="file"
+            id="file-input"
+            className={styles['file-upload-input']}
+            onChange={handleOnChange}
+          />
+        </span>
       </label>
     </div>
   )
