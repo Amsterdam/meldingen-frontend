0e3a785464c3072716fb8e1532c3c28f22e5d8bf
Vincent <vdegraaf.vincent@gmail.com>
2024-10-21 15:46:26 +0200

    Cleanup code

--- apps/public/src/app/bijlagen/_components/file-upload.spec.tsx
+++ apps/public/src/app/bijlagen/_components/file-upload.spec.tsx
@@ -9,6 +9,7 @@ describe('FileUpload Component', () => {
 
     const buttonText = screen.getByText('Selecteer bestanden')
     const dropAreaText = screen.getByText('Of sleep de bestanden in dit vak.')
+
     expect(buttonText).toBeInTheDocument()
     expect(dropAreaText).toBeInTheDocument()
   })
--- apps/public/src/app/bijlagen/_components/file-upload.tsx
+++ apps/public/src/app/bijlagen/_components/file-upload.tsx
@@ -8,12 +8,13 @@ export const FileUpload = () => {
 
   return (
     <div className={styles['file-upload-wrapper']}>
-      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
       <label htmlFor="file-input" className={styles['file-upload-label']}>
-        <span className={styles['file-upload-button']}>Selecteer bestanden</span>
+        <span id="label" className={styles['file-upload-button']}>
+          Selecteer bestanden
+        </span>
         <span className={styles['file-upload-drop-area-text']}>Of sleep de bestanden in dit vak.</span>
+        <input type="file" id="file-input" className={styles['file-upload-input']} onChange={handleOnChange} />
       </label>
-      <input type="file" id="file-input" className={styles['file-upload-input']} onChange={handleOnChange} />
     </div>
   )
 }
