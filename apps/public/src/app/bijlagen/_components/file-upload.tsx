146c357fcf7dd72270df09471b6b0fd69fc7ece4
alimpens <a.limpens@amsterdam.nl>
2024-10-22 18:04:33 +0200

    Add focus outline fallback

--- apps/public/src/app/bijlagen/_components/file-upload.tsx
+++ apps/public/src/app/bijlagen/_components/file-upload.tsx
@@ -8,6 +8,7 @@ export const FileUpload = () => {
 
   return (
     <div className={styles.wrapper}>
+      <input type="file" id="file-input" className={styles.input} onChange={handleOnChange} />
       {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
       <label htmlFor="file-input" className={styles.label}>
         <span id="label" className={styles.button}>
@@ -15,7 +16,6 @@ export const FileUpload = () => {
         </span>
         <span className={styles['drop-area-text']}>Of sleep de bestanden in dit vak.</span>
       </label>
-      <input type="file" id="file-input" className={styles.input} onChange={handleOnChange} />
     </div>
   )
 }
