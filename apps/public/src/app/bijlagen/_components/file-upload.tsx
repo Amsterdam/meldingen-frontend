815a397bc076fb9ab52b2da0eab90ecfbccff6bb
alimpens <a.limpens@amsterdam.nl>
2024-10-22 17:45:36 +0200

    Get rid of double class names

--- apps/public/src/app/bijlagen/_components/file-upload.tsx
+++ apps/public/src/app/bijlagen/_components/file-upload.tsx
@@ -7,15 +7,15 @@ export const FileUpload = () => {
   }
 
   return (
-    <div className={styles['file-upload-wrapper']}>
+    <div className={styles.wrapper}>
       {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-      <label htmlFor="file-input" className={styles['file-upload-label']}>
-        <span id="label" className={styles['file-upload-button']}>
+      <label htmlFor="file-input" className={styles.label}>
+        <span id="label" className={styles.button}>
           Selecteer bestanden
         </span>
-        <span className={styles['file-upload-drop-area-text']}>Of sleep de bestanden in dit vak.</span>
+        <span className={styles['drop-area-text']}>Of sleep de bestanden in dit vak.</span>
       </label>
-      <input type="file" id="file-input" className={styles['file-upload-input']} onChange={handleOnChange} />
+      <input type="file" id="file-input" className={styles.input} onChange={handleOnChange} />
     </div>
   )
 }
