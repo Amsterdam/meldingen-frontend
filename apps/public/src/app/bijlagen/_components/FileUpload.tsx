c226b028f80ea29caee7e21fd21d16d43d0b1e45
Vincent <vdegraaf.vincent@gmail.com>
2024-10-29 13:05:43 +0100

    Add api call to FileUpload

--- apps/public/src/app/bijlagen/_components/FileUpload.tsx
+++ apps/public/src/app/bijlagen/_components/FileUpload.tsx
@@ -1,21 +1,63 @@
+import { useState } from 'react'
+
+import type { AttachmentOutput } from '@meldingen/api-client'
+import { postMeldingByMeldingIdAttachment } from '@meldingen/api-client'
+
+import { useMeldingContext } from '../../../context/MeldingContextProvider'
+
 import styles from './FileUpload.module.css'
 
 export const FileUpload = () => {
-  const handleOnChange = (acceptedFiles: any) => {
-    // Do something with the files
-    console.log('Accepted files: ', acceptedFiles.target.files)
+  const [uploadedFiles, setUploadedFiles] = useState<AttachmentOutput[]>([])
+  const { data } = useMeldingContext()
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (data && event.currentTarget.files) {
+      const filesArray = Array.from(event.currentTarget.files)
+
+      filesArray.forEach(async (file: File) => {
+        try {
+          const result = await postMeldingByMeldingIdAttachment({
+            formData: { file },
+            meldingId: data.id,
+            token: data.token,
+          })
+
+          setUploadedFiles((currentFiles) => [...currentFiles, result])
+        } catch (error) {
+          console.error('Error uploading file: ', error)
+        }
+      })
+    }
   }
 
   return (
-    <div className={styles.wrapper}>
-      <input type="file" id="file-input" className={styles.input} onChange={handleOnChange} />
-      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-      <label htmlFor="file-input" className={styles.label}>
-        <span id="label" className={styles.button}>
-          Selecteer bestanden
-        </span>
-        <span className={styles['drop-area-text']}>Of sleep de bestanden in dit vak.</span>
-      </label>
+    <div>
+      <div className={styles.wrapper}>
+        <input
+          multiple
+          type="file"
+          id="file-input"
+          aria-label="file-input"
+          className={styles.input}
+          onChange={handleOnChange}
+        />
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label htmlFor="file-input" className={styles.label}>
+          <span id="label" className={styles.button}>
+            Selecteer bestanden
+          </span>
+          <span className={styles['drop-area-text']}>Of sleep de bestanden in dit vak.</span>
+        </label>
+      </div>
+      <div>
+        Dummy List
+        {uploadedFiles.map((file) => (
+          <div key={file.id}>
+            <span>{file.original_filename}</span>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
