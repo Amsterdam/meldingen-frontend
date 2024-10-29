dadd8b25c0b544063dade213eb6f23af513e4568
Vincent <vdegraaf.vincent@gmail.com>
2024-10-29 13:06:01 +0100

    Add test case

--- apps/public/src/app/bijlagen/_components/FileUpload.spec.tsx
+++ apps/public/src/app/bijlagen/_components/FileUpload.spec.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
-
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+
 import { FileUpload } from './FileUpload'
 
 describe('FileUpload Component', () => {
@@ -12,5 +13,28 @@ describe('FileUpload Component', () => {
 
     expect(buttonText).toBeInTheDocument()
     expect(dropAreaText).toBeInTheDocument()
+  })
+
+  it('uploads a file ', async () => {
+    render(<FileUpload />)
+
+    const fileInput = screen.getByLabelText('file-input') as HTMLInputElement
+    const file = new File(['dummy content'], 'example.png', { type: 'image/png' })
+
+    await userEvent.upload(fileInput, file)
+
+    expect(fileInput.files).toHaveLength(1)
+  })
+
+  it('uploads multiple files', async () => {
+    render(<FileUpload />)
+
+    const fileInput = screen.getByLabelText('file-input') as HTMLInputElement
+    const file = new File(['dummy content'], 'example.png', { type: 'image/png' })
+    const file2 = new File(['dummy content two'], 'hoi.png', { type: 'image/png' })
+
+    await userEvent.upload(fileInput, [file, file2])
+
+    expect(fileInput.files).toHaveLength(2)
   })
 })
