f46a875c02c8afc2eeaeb973c904ed96e8bb0188
alimpens <a.limpens@amsterdam.nl>
2024-11-05 11:03:03 +0100

    Show coverage on test run

--- libs/form-renderer/vite.config.ts
+++ libs/form-renderer/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
+    coverage: {
+      enabled: true,
+    },
     globals: true,
     environment: 'jsdom',
     include: ['src/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
