81bae30041abc5d4def9115fc6ac24c7db41163a
alimpens <a.limpens@amsterdam.nl>
2024-11-25 17:34:31 +0100

    Setup root scripts

--- apps/public/src/app/Home.tsx
+++ apps/public/src/app/Home.tsx
@@ -28,6 +28,11 @@ export const Home = ({ formData }: { formData: StaticFormWithSubmit }) => {
     })
   }
 
+  // TODO: ESLint test, remove when ESlint works
+  // var hello = (name: any) => {
+  //   return 'Hello, ' + name + '!'
+  // }
+
   return (
     <Grid paddingBottom="large" paddingTop="medium">
       <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 2 }}>
