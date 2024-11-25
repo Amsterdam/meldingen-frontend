609f800dfa756b30dcabb6ac08d2fdbac0e58c2c
alimpens <a.limpens@amsterdam.nl>
2024-11-25 13:57:57 +0100

    Setup static form edit page

--- apps/admin/src/pages/static-form/StaticFormEdit.tsx
+++ apps/admin/src/pages/static-form/StaticFormEdit.tsx
@@ -1,18 +1,31 @@
-import { DeleteWithConfirmButton, Edit, SaveButton, SimpleForm, TextInput, Toolbar, ToolbarClasses } from 'react-admin'
+import { FormioSchema } from '@meldingen/formio'
+import { Edit, SimpleForm, TextInput } from 'react-admin'
+import filter from 'uber-json-schema-filter'
+import { FormTextAreaComponentInputSchema } from '@meldingen/api-client'
 
-export const StaticFormEdit = () => (
-  <Edit>
-    <SimpleForm
-      toolbar={
-        <Toolbar>
-          <div className={ToolbarClasses.defaultToolbar}>
-            <SaveButton alwaysEnable />
-            <DeleteWithConfirmButton />
-          </div>
-        </Toolbar>
-      }
-    >
-      <TextInput source="title" readOnly />
-    </SimpleForm>
-  </Edit>
-)
+export const StaticFormEdit = () => {
+  // todo: geen disable button
+  // labels van velden mooier maken
+
+  // The data is filtered here before being passed to the API
+  // It's filtered the same way as in the filterFormResponse function
+  const transform = (data: FormioSchema) => {
+    const component = data.components[0]
+    const filteredComponent = filter(FormTextAreaComponentInputSchema, component)
+
+    return {
+      ...data,
+      components: [filteredComponent],
+    }
+  }
+
+  return (
+    <Edit transform={transform}>
+      <SimpleForm>
+        <TextInput source="title" readOnly />
+        <TextInput source="components[0].label" />
+        <TextInput source="components[0].description" />
+      </SimpleForm>
+    </Edit>
+  )
+}
