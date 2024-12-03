441b66f2ae18aa2af5e32fab22b9d2bc610d3ce2
alimpens <a.limpens@amsterdam.nl>
2024-12-03 11:44:47 +0100

    Post answers aanvullende vragen

--- apps/public/src/app/nieuwe-renderer/_components/AanvullendeVragenRenderer.tsx
+++ apps/public/src/app/nieuwe-renderer/_components/AanvullendeVragenRenderer.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import { Grid, Heading, Link } from '@amsterdam/design-system-react'
+import { postMeldingByMeldingIdQuestionByQuestionId } from '@meldingen/api-client'
 import { FormRenderer } from '@meldingen/form-renderer'
 import NextLink from 'next/link'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import type { FormEvent } from 'react'
 
 // TODO: fix formData type
@@ -13,20 +14,59 @@ type Props = {
   previousPanelPath: string
 }
 
+const transformAnswer = (answer: string | { [key: string]: string }) => {
+  if (typeof answer === 'object') {
+    const checkedAnswers = Object.keys(answer).filter((key) => answer[key])
+
+    return checkedAnswers.join(', ')
+  }
+
+  return answer
+}
+
 export const AanvullendeVragenRenderer = ({ formData, nextPanelPath, previousPanelPath }: Props) => {
   const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const meldingId = searchParams.get('id')
+  const token = searchParams.get('token')
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
 
-    router.push(nextPanelPath)
+    const data = new FormData(e.target as HTMLFormElement)
+    const answerObject = Object.fromEntries(data)
+    const answers = Object.entries(answerObject)
+
+    answers.forEach(([key, value]) => {
+      if (value instanceof File) return undefined
+
+      // Transform all answers to strings (including checkbox objects)
+      const answerString = transformAnswer(value)
+
+      // // Filter out empty answers
+      if (answerString.length === 0) return undefined
+
+      const questionId = formData.find((component) => component.key === key)?.question
+
+      if (!meldingId || !questionId || !token) return undefined
+
+      return postMeldingByMeldingIdQuestionByQuestionId({
+        meldingId: parseInt(meldingId, 10),
+        questionId,
+        token,
+        requestBody: { text: answerString },
+      })
+    })
+
+    router.push(`${nextPanelPath}?token=${token}&id=${meldingId}`)
   }
 
   return (
     <Grid paddingBottom="large" paddingTop="medium">
       <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 2 }}>
-        <NextLink href={previousPanelPath} legacyBehavior passHref>
-          <Link className="ams-mb--xs" href={previousPanelPath}>
+        <NextLink href={`${previousPanelPath}?token=${token}&id=${meldingId}`} legacyBehavior passHref>
+          <Link className="ams-mb--xs" href="dummy-href">
             Vorige vraag
           </Link>
         </NextLink>
