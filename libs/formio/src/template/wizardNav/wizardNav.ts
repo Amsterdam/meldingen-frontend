de192b74fa3da5547dad88d95761980a353d602c
alimpens <a.limpens@amsterdam.nl>
2024-12-03 15:15:11 +0100

    Cleanup

--- libs/formio/src/template/wizardNav/wizardNav.ts
+++
@@ -1,6 +0,0 @@
-export const wizardNav = (ctx: any) => `
-<div>
-  ${ctx.buttons.next ? `<button class="ams-button ams-button--primary" type="submit" ref="${ctx.wizardKey}-next">${ctx.t('next')}</button>` : ''}
-  ${ctx.buttons.submit ? `<button class="ams-button ams-button--primary" type="submit" ref="${ctx.wizardKey}-submit">${ctx.t('submit')}</button>` : ''}
-</div>
-`
