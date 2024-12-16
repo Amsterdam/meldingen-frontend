725895d66f943837200cac8fcafcfeea3f7e15e7
alimpens <a.limpens@amsterdam.nl>
2024-12-16 10:49:56 +0100

    Wrap searchParams calls in Suspense

--- apps/public/src/app/(general)/bijlage/page.tsx
+++ apps/public/src/app/(general)/bijlage/page.tsx
@@ -3,13 +3,13 @@
 import { Column, Field, Heading, Label, Paragraph, UnorderedList } from '@amsterdam/design-system-react'
 import { Grid, SubmitButton } from '@meldingen/ui'
 import { useRouter, useSearchParams } from 'next/navigation'
-import type { FormEvent } from 'react'
+import { Suspense, type FormEvent } from 'react'
 
 import { BackLink } from '../_components/BackLink'
 
 import { FileUpload } from './_components/FileUpload'
 
-const Bijlagen = () => {
+const Form = () => {
   const router = useRouter()
   const searchParams = useSearchParams()
 
@@ -23,32 +23,40 @@ const Bijlagen = () => {
   }
 
   return (
-    <Grid paddingBottom="large" paddingTop="medium">
-      <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 2 }}>
-        <BackLink href={`/locatie?token=${token}&id=${meldingId}`}>Vorige vraag</BackLink>
-        <Heading className="ams-mb--sm">Foto’s</Heading>
-        <form onSubmit={handleSubmit}>
-          <Field>
-            <Label htmlFor="file-upload" optional>
-              Heeft u een foto om toe te voegen?
-            </Label>
-            <Column id="file-upload-description" className="ams-mb--sm">
-              <Paragraph>
-                Voeg een foto toe om de situatie te verduidelijken. Verwijder alle persoonsgegevens van u en derden.
-              </Paragraph>
-              <UnorderedList>
-                <UnorderedList.Item>U kunt maximaal drie bestanden tegelijk toevoegen.</UnorderedList.Item>
-                <UnorderedList.Item>Toegestane bestandtypes: jpg, jpeg en png.</UnorderedList.Item>
-                <UnorderedList.Item>Een bestand mag maximaal 20 MB groot zijn.</UnorderedList.Item>
-              </UnorderedList>
-            </Column>
-            <FileUpload id="file-upload" aria-describedby="file-upload-description" />
-          </Field>
-          <SubmitButton>Volgende vraag</SubmitButton>
-        </form>
-      </Grid.Cell>
-    </Grid>
+    <>
+      <BackLink href={`/locatie?token=${token}&id=${meldingId}`}>Vorige vraag</BackLink>
+      <Heading className="ams-mb--sm">Foto’s</Heading>
+      <form onSubmit={handleSubmit}>
+        <Field>
+          <Label htmlFor="file-upload" optional>
+            Heeft u een foto om toe te voegen?
+          </Label>
+          <Column id="file-upload-description" className="ams-mb--sm">
+            <Paragraph>
+              Voeg een foto toe om de situatie te verduidelijken. Verwijder alle persoonsgegevens van u en derden.
+            </Paragraph>
+            <UnorderedList>
+              <UnorderedList.Item>U kunt maximaal drie bestanden tegelijk toevoegen.</UnorderedList.Item>
+              <UnorderedList.Item>Toegestane bestandtypes: jpg, jpeg en png.</UnorderedList.Item>
+              <UnorderedList.Item>Een bestand mag maximaal 20 MB groot zijn.</UnorderedList.Item>
+            </UnorderedList>
+          </Column>
+          <FileUpload id="file-upload" aria-describedby="file-upload-description" />
+        </Field>
+        <SubmitButton>Volgende vraag</SubmitButton>
+      </form>
+    </>
   )
 }
+
+const Bijlagen = () => (
+  <Grid paddingBottom="large" paddingTop="medium">
+    <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 2 }}>
+      <Suspense>
+        <Form />
+      </Suspense>
+    </Grid.Cell>
+  </Grid>
+)
 
 export default Bijlagen
--- apps/public/src/app/(general)/locatie/page.tsx
+++ apps/public/src/app/(general)/locatie/page.tsx
@@ -4,11 +4,11 @@ import { Heading, Link, Paragraph } from '@amsterdam/design-system-react'
 import { Grid, SubmitButton } from '@meldingen/ui'
 import NextLink from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
-import type { FormEvent } from 'react'
+import { Suspense, type FormEvent } from 'react'
 
 import { BackLink } from '../_components/BackLink'
 
-const Locatie = () => {
+const Form = () => {
   const router = useRouter()
   const searchParams = useSearchParams()
 
@@ -24,32 +24,38 @@ const Locatie = () => {
   }
 
   return (
-    <Grid paddingBottom="large" paddingTop="medium">
-      <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 2 }}>
-        {/* TODO: get href of last aanvullende vragen page */}
-        <BackLink href="/">Vorige vraag</BackLink>
-        <Heading className="ams-mb--sm">Locatie</Heading>
-
-        {/* TODO: text should come from api */}
-        <Heading level={2} size="level-4">
-          Waar staat de container?
-        </Heading>
-        <Paragraph className="ams-mb--xs">
-          In het volgende scherm kunt u op de kaart een adres of container opzoeken.
-        </Paragraph>
-
-        <NextLink href="/locatie/kies" legacyBehavior passHref>
-          <Link variant="standalone" href="dummy-href" className="ams-mb--md">
-            Selecteer de locatie
-          </Link>
-        </NextLink>
-        <form onSubmit={handleSubmit}>
-          <input type="hidden" />
-          <SubmitButton>Volgende vraag</SubmitButton>
-        </form>
-      </Grid.Cell>
-    </Grid>
+    <form onSubmit={handleSubmit}>
+      <input type="hidden" />
+      <SubmitButton>Volgende vraag</SubmitButton>
+    </form>
   )
 }
+
+const Locatie = () => (
+  <Grid paddingBottom="large" paddingTop="medium">
+    <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 2 }}>
+      {/* TODO: get href of last aanvullende vragen page */}
+      <BackLink href="/">Vorige vraag</BackLink>
+      <Heading className="ams-mb--sm">Locatie</Heading>
+
+      {/* TODO: text should come from api */}
+      <Heading level={2} size="level-4">
+        Waar staat de container?
+      </Heading>
+      <Paragraph className="ams-mb--xs">
+        In het volgende scherm kunt u op de kaart een adres of container opzoeken.
+      </Paragraph>
+
+      <NextLink href="/locatie/kies" legacyBehavior passHref>
+        <Link variant="standalone" href="dummy-href" className="ams-mb--md">
+          Selecteer de locatie
+        </Link>
+      </NextLink>
+      <Suspense>
+        <Form />
+      </Suspense>
+    </Grid.Cell>
+  </Grid>
+)
 
 export default Locatie
