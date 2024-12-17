0074ac4225ad0be9c26ebca90203a5c1c9a91ca1
alimpens <a.limpens@amsterdam.nl>
2024-12-17 11:46:14 +0100

    Delete session cookies at end of session

--- apps/public/src/app/(general)/bijlage/actions.ts
+++ apps/public/src/app/(general)/bijlage/actions.ts
@@ -1,5 +1,13 @@
 'use server'
 
+import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-export const postAttachmentForm = async () => redirect('/bedankt')
+export const postAttachmentForm = async () => {
+  // Delete session cookies
+  const cookieStore = await cookies()
+  cookieStore.delete('id')
+  cookieStore.delete('token')
+
+  return redirect('/bedankt')
+}
