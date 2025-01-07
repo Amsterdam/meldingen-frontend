8a71f30e8195e7d7ffdafb34f63d9c0885a901e8
Vincent <vdegraaf.vincent@gmail.com>
2025-01-07 21:53:13 +0100

    Styling and cleanup

---
+++ apps/public/src/app/(map)/locatie/kies/_components/AddressMenu/AddressMenu.tsx
@@ -0,0 +1,32 @@
+import { Heading, Icon, Paragraph } from '@amsterdam/design-system-react'
+import { ChevronLeftIcon } from '@amsterdam/design-system-react-icons'
+import Link from 'next/link'
+
+import styles from './AddressMenu.module.css'
+
+type Props = {
+  address: string | null
+}
+
+export const AddressMenu = ({ address }: Props) => (
+  <div className={styles.container}>
+    <Link href="/locatie">
+      <Icon svg={ChevronLeftIcon} size="level-4" />
+      {/* TODO: Icon becomes red when clicking it.  */}
+    </Link>
+    <span>
+      <Heading level={1} size="level-4">
+        Selecteer de locatie
+      </Heading>
+      <Paragraph size="small">
+        Typ het dichtstbijzijnde adres, klik de locatie aan op de kaart of gebruik &quot;Mijn locatie&quot;
+      </Paragraph>
+    </span>
+    <span>
+      <Heading level={2} size="level-4">
+        Zoek op adres
+      </Heading>
+      {address && <span>{address}</span>}
+    </span>
+  </div>
+)
--- apps/public/src/app/(map)/locatie/kies/page.tsx
+++ apps/public/src/app/(map)/locatie/kies/page.tsx
@@ -4,25 +4,28 @@ import { Grid } from '@amsterdam/design-system-react'
 import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
 
-import { SideMenu } from './_components/SideMenu'
+import { AddressMenu } from './_components/AddressMenu/AddressMenu'
+
+type Location = { lat: number; lon: number }
 
 const Map = dynamic(() => import('./_components/BaseLayer').then((module) => module.BaseLayer), {
   loading: () => <p>Loading...</p>, // TODO: improve loading state
   ssr: false,
 })
 
-const reverseGeocodeService = async ({ lat, lon }: { lat: number; lon: number }) =>
+const reverseGeocodeService = async ({ lat, lon }: Location) =>
   fetch(`https://api.pdok.nl/bzk/locatieserver/search/v3_1/reverse?lat=${lat}&lon=${lon}&rows=1`)
     .then((res) => res.json())
     // eslint-disable-next-line no-console
     .catch((error) => console.error('Error:', error))
 
 const KiesLocatie = () => {
-  const [location, setLocation] = useState({ lat: 0, lon: 0 })
-  const [address, setAddress] = useState()
+  const [location, setLocation] = useState<Location | null>(null)
+  const [address, setAddress] = useState<string | null>(null)
 
   useEffect(() => {
     const getAddress = async () => {
+      if (!location) return
       const result = await reverseGeocodeService({ lat: location.lat, lon: location.lon })
 
       setAddress(result.response.docs[0].weergavenaam)
@@ -31,9 +34,10 @@ const KiesLocatie = () => {
   }, [location])
 
   return (
-    <Grid paddingVertical="small">
+    // TODO: Create a compact grid with padding 24px and the map that overflows it. https://designsystem.amsterdam/?path=/docs/brand-design-tokens-grid--docs
+    <Grid>
       <Grid.Cell span={4}>
-        <SideMenu address={address} />
+        <AddressMenu address={address} />
       </Grid.Cell>
       <Grid.Cell span={8}>
         <Map setLocation={setLocation} />
