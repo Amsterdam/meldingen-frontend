77e66cefcc11e9a33dd9a0d6a557ebd81f6eebd2
alimpens <a.limpens@amsterdam.nl>
2025-01-09 16:16:06 +0100

    Set backend base url with env var

--- apps/public/src/apiClientProxy.ts
+++ apps/public/src/apiClientProxy.ts
@@ -1,6 +1,8 @@
 import * as apiClientProxy from '@meldingen/api-client'
 
 // Set base url for the API client
-apiClientProxy.OpenAPI.BASE = 'http://localhost:8000'
+if (process.env.NEXT_BACKEND_BASE_URL) {
+  apiClientProxy.OpenAPI.BASE = process.env.NEXT_BACKEND_BASE_URL
+}
 
 export * from '@meldingen/api-client'
