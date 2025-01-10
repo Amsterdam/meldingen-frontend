1222e92d550a110508088d9442c68aa05859be86
Vincent <vdegraaf.vincent@gmail.com>
2025-01-10 13:22:15 +0100

    Update ra-keycloak and keycloak-js

--- apps/admin/src/app/providers/useAuthProvider.ts
+++ apps/admin/src/app/providers/useAuthProvider.ts
@@ -1,12 +1,16 @@
 import type { KeycloakConfig, KeycloakInitOptions } from 'keycloak-js'
 import Keycloak from 'keycloak-js'
 import { keycloakAuthProvider, httpClient } from 'ra-keycloak'
-import { useEffect, useRef, useState } from 'react'
+import { useRef } from 'react'
 import type { AuthProvider, DataProvider } from 'react-admin'
 
 import { dataProvider } from './dataProvider'
 
-const initOptions: KeycloakInitOptions = { onLoad: 'login-required', checkLoginIframe: false }
+const initOptions: KeycloakInitOptions = {
+  onLoad: 'login-required',
+  checkLoginIframe: false,
+  responseMode: 'query',
+}
 
 const config: KeycloakConfig = {
   url: import.meta.env.VITE_KEYCLOAK_BASE_URL,
@@ -15,36 +19,24 @@ const config: KeycloakConfig = {
 }
 
 export const useAuthProvider = () => {
-  const [keycloak, setKeycloak] = useState<Keycloak | undefined>()
   const authProvider = useRef<AuthProvider | undefined>()
   const dataProviderRef = useRef<DataProvider | undefined>()
 
-  useEffect(() => {
-    // setTimeout is needed to prevent an infinite refresh loop caused by keyCloakClient.init()
-    const timerId = setTimeout(() => {
-      const initKeyCloakClient = async () => {
-        const keycloakClient = new Keycloak(config)
+  const keycloakClient = new Keycloak(config)
 
-        keycloakClient.onTokenExpired = () => {
-          keycloakClient.updateToken().catch(() => {
-            keycloakClient.login()
-          })
-        }
+  keycloakClient.onTokenExpired = () => {
+    keycloakClient.updateToken().catch(() => {
+      keycloakClient.login()
+    })
+  }
 
-        await keycloakClient.init(initOptions)
+  authProvider.current = keycloakAuthProvider(keycloakClient, {
+    initOptions,
+    loginRedirectUri: `/#/auth-callback`,
+    logoutRedirectUri: `/#/login`,
+  })
 
-        authProvider.current = keycloakAuthProvider(keycloakClient)
-        dataProviderRef.current = dataProvider(import.meta.env.VITE_BACKEND_BASE_URL, httpClient(keycloakClient))
+  dataProviderRef.current = dataProvider(import.meta.env.VITE_BACKEND_BASE_URL, httpClient(keycloakClient))
 
-        setKeycloak(keycloakClient)
-      }
-      if (!keycloak) {
-        initKeyCloakClient()
-      }
-    }, 0)
-
-    return () => clearTimeout(timerId)
-  }, [keycloak])
-
-  return { keycloak, dataProviderRef, authProvider }
+  return { keycloakClient, dataProviderRef, authProvider }
 }
