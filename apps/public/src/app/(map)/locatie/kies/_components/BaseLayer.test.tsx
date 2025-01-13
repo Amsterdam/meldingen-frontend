b367b6d89c9ce01980cdccdd74ada75057f4ea69
Vincent <vdegraaf.vincent@gmail.com>
2025-01-13 12:41:54 +0100

    Pr feedback fix

--- apps/public/src/app/(map)/locatie/kies/_components/BaseLayer.test.tsx
+++ apps/public/src/app/(map)/locatie/kies/_components/BaseLayer.test.tsx
@@ -4,7 +4,7 @@ import { BaseLayer } from './BaseLayer'
 
 describe('BaseLayer', () => {
   it('renders the component', () => {
-    const { container } = render(<BaseLayer />)
+    const { container } = render(<BaseLayer setCoordinates={() => {}} />)
     expect(container.firstChild).toBeInTheDocument()
   })
 })
--- apps/public/src/app/(map)/locatie/kies/page.tsx
+++ apps/public/src/app/(map)/locatie/kies/page.tsx
@@ -2,10 +2,9 @@
 
 import { Grid } from '@amsterdam/design-system-react'
 import dynamic from 'next/dynamic'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import { SideBar } from './_components/SideBar/SideBar'
-import { reverseGeocodeService } from './utils.ts/reverse-geocode-service'
 
 export type Coordinates = { lat: number; lon: number }
 
@@ -16,23 +15,12 @@ const Map = dynamic(() => import('./_components/BaseLayer').then((module) => mod
 
 const KiesLocatie = () => {
   const [coordinates, setCoordinates] = useState<Coordinates | null>(null)
-  const [address, setAddress] = useState<string | null>(null)
-
-  useEffect(() => {
-    const getAddress = async () => {
-      if (!coordinates) return
-      const result = await reverseGeocodeService({ lat: coordinates.lat, lon: coordinates.lon })
-
-      setAddress(result.response.docs[0].weergavenaam)
-    }
-    getAddress()
-  }, [coordinates])
 
   return (
     // TODO: Create a compact grid with padding 24px and the map that overflows it. https://designsystem.amsterdam/?path=/docs/brand-design-tokens-grid--docs
     <Grid>
       <Grid.Cell span={4}>
-        <SideBar address={address} />
+        <SideBar coordinates={coordinates} />
       </Grid.Cell>
       <Grid.Cell span={8}>
         <Map setCoordinates={setCoordinates} />
