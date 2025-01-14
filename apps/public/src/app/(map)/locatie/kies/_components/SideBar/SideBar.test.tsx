ee01e4bc1de312b8f852d6da3fbe170f29d6f8ce
alimpens <a.limpens@amsterdam.nl>
2025-01-14 10:34:09 +0100

    Don't run async test for sync state

--- apps/public/src/app/(map)/locatie/kies/_components/SideBar/SideBar.test.tsx
+++ apps/public/src/app/(map)/locatie/kies/_components/SideBar/SideBar.test.tsx
@@ -8,16 +8,14 @@ vi.mock('../../utils', () => ({
 }))
 
 describe('SideBar', () => {
-  it('should render correctly', async () => {
-    render(<SideBar coordinates={{ lat: 52.37239126063553, lon: 4.900905743712159 }} />)
+  it('should render correctly', () => {
+    render(<SideBar coordinates={null} />)
 
     const heading = screen.getByRole('heading', { name: 'Selecteer de locatie' })
     const paragraph = screen.getByRole('paragraph')
 
-    await waitFor(() => {
-      expect(heading).toBeInTheDocument()
-      expect(paragraph).toBeInTheDocument()
-    })
+    expect(heading).toBeInTheDocument()
+    expect(paragraph).toBeInTheDocument()
   })
 
   it('should render render address based on provided coordinates ', async () => {
