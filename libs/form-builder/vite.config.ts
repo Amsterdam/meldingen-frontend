234fab5ea34ffa44fe20aba6684bfb5f024b00d5
alimpens <a.limpens@amsterdam.nl>
2025-01-14 12:11:04 +0100

    Remove vite from lib

--- libs/form-builder/vite.config.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-
-export default defineConfig({
-  plugins: [tsconfigPaths()],
-})
