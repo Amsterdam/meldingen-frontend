ec455b59f08075897e556bf74d19eba2b2a8a2aa
Aram <37216945+alimpens@users.noreply.github.com>
2025-01-14 09:23:18 +0100

    Make API client generic, use env var to set backend base url (#655)
    Co-authored-by: ramonavic <ramon.hofmeister@gmail.com>

--- apps/public/src/app/(general)/layout.tsx
+++ apps/public/src/app/(general)/layout.tsx
@@ -1,30 +1,15 @@
 import { Footer, Header, Screen } from '@meldingen/ui'
-import type { Metadata } from 'next'
 
 import { MeldingContextProvider } from '../../context/MeldingContextProvider'
 
-import '@amsterdam/design-system-tokens/dist/index.css'
-import '@amsterdam/design-system-assets/font/index.css'
-import '@amsterdam/design-system-css/dist/index.css'
-
-import '../global.css'
-
-export const metadata: Metadata = {
-  title: 'Meldingen',
-}
-
 const GeneralLayout = ({ children }: { children: React.ReactNode }) => (
-  <html lang="nl">
-    <body>
-      <MeldingContextProvider>
-        <Screen maxWidth="wide">
-          <Header />
-          <main id="main">{children}</main>
-          <Footer />
-        </Screen>
-      </MeldingContextProvider>
-    </body>
-  </html>
+  <MeldingContextProvider>
+    <Screen maxWidth="wide">
+      <Header />
+      <main id="main">{children}</main>
+      <Footer />
+    </Screen>
+  </MeldingContextProvider>
 )
 
 export default GeneralLayout
