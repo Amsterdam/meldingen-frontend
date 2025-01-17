184799aab819dc394a7db0ca44550d87d38b762e
Vincent <vdegraaf.vincent@gmail.com>
2025-01-17 14:01:22 +0100

    Disable no-default-export for vite.config.ts

--- libs/form-renderer/vite.config.ts
+++ libs/form-renderer/vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
