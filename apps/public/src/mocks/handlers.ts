492d359c96cb6346a9bf784ea7bcc0c0051dc906
alimpens <a.limpens@amsterdam.nl>
2025-01-27 15:22:36 +0100

    Simplify PDOK mocks

--- apps/public/src/mocks/handlers.ts
+++ apps/public/src/mocks/handlers.ts
@@ -20,17 +20,11 @@ export const handlers = [
   http.get(ENDPOINTS.PDOK_REVERSE, () =>
     HttpResponse.json({
       response: {
-        numFound: 1,
-        start: 0,
-        maxScore: 7.2280917,
-        numFoundExact: true,
         docs: [
           {
             type: 'adres',
             weergavenaam: 'Nieuwmarkt 15, 1011JR Amsterdam',
             id: 'adr-758e934b8651217819abcf0e60a45b39',
-            score: 7.2280917,
-            afstand: 1.8,
           },
         ],
       },
@@ -40,10 +34,6 @@ export const handlers = [
   http.get(ENDPOINTS.PDOK_SUGGEST, () =>
     HttpResponse.json({
       response: {
-        numFound: 566689,
-        start: 0,
-        maxScore: 1.4264281,
-        numFoundExact: true,
         docs: [
           {
             weergavenaam: 'Amsteldijk 152A-H, 1079LG Amsterdam',
