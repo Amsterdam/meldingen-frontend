2c725447e1740fb63ca417a04d89b8d54eef2460
Vincent <vdegraaf.vincent@gmail.com>
2025-02-03 20:27:48 +0100

    Refactor contact page

--- apps/public/src/app/(general)/contact/page.tsx
+++ apps/public/src/app/(general)/contact/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Column, Field, FieldSet, Heading, Label, Paragraph, TextInput } from '@amsterdam/design-system-react'
+import { Heading, Label, Paragraph, TextInput } from '@amsterdam/design-system-react'
 import { Grid, SubmitButton } from '@meldingen/ui'
 
 import { BackLink } from '../_components/BackLink'
@@ -11,39 +11,36 @@ const Contact = () => (
   <Grid paddingBottom="large" paddingTop="medium">
     <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 2 }}>
       <BackLink href="/bijlage">Vorige vraag</BackLink>
-      <Heading className="ams-mb--sm">Contact</Heading>
+      <Heading level={1} className="ams-mb--sm">
+        Contact
+      </Heading>
       <form action={postContactForm}>
-        <FieldSet
-          aria-labelledby="contact-details-description"
-          legend="Mogen we u bellen voor vragen? En op de hoogte houden via e-mail?"
-        >
-          <Column id="contact-details-description" className="ams-mb--sm">
-            <Paragraph>
-              Vaak hebben we nog een vraag. Daarmee kunnen we het probleem sneller of beter oplossen. Of we willen iets
-              uitleggen. Wij willen u dan graag even bellen. Of anders e-mailen wij u.
-            </Paragraph>
-            <Paragraph>Wij gebruiken uw telefoonnummer en e-mailadres alléén voor deze melding.</Paragraph>
-          </Column>
-          <Field className="ams-mb--sm">
-            <Label htmlFor="email-input" optional>
-              Wat is uw e-mailadres?
-            </Label>
-            <TextInput
-              name="email"
-              id="email-input"
-              type="email"
-              autoComplete="email"
-              autoCorrect="off"
-              spellCheck="false"
-            />
-          </Field>
-          <Field className="ams-mb--sm">
-            <Label htmlFor="tel-input" optional>
-              Wat is uw telefoonnummer?
-            </Label>
-            <TextInput name="phone" id="tel-input" type="tel" autoComplete="tel" />
-          </Field>
-        </FieldSet>
+        <Heading level={2} size="level-4" className="ams-mb--xs">
+          Mogen we u bellen voor vragen? En op de hoogte houden via e-mail?
+        </Heading>
+        <Paragraph className="ams-mb--xs">
+          Vaak hebben we nog een vraag. Daarmee kunnen we het probleem sneller of beter oplossen. Of we willen iets
+          uitleggen. Wij willen u dan graag even bellen. Of anders e-mailen wij u.
+        </Paragraph>
+        <Paragraph className="ams-mb--sm">
+          Wij gebruiken uw telefoonnummer en e-mailadres alléén voor deze melding.
+        </Paragraph>
+        <Label htmlFor="email-input" optional className="ams-mb--sm">
+          Wat is uw e-mailadres?
+        </Label>
+        <TextInput
+          name="email"
+          id="email-input"
+          type="email"
+          autoComplete="email"
+          autoCorrect="off"
+          spellCheck="false"
+          className="ams-mb--sm"
+        />
+        <Label htmlFor="tel-input" optional className="ams-mb--sm">
+          Wat is uw telefoonnummer?
+        </Label>
+        <TextInput name="phone" id="tel-input" type="tel" autoComplete="tel" className="ams-mb--sm" />
         <SubmitButton>Volgende vraag</SubmitButton>
       </form>
     </Grid.Cell>
