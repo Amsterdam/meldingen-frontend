f8780f3200034e147892662405b9ca285c598b7b
Vincent <vdegraaf.vincent@gmail.com>
2025-02-11 13:33:33 +0100

    PR feedback

--- apps/public/src/app/(map)/locatie/kies/_utils/getAddressFromCoordinates.ts
+++ apps/public/src/app/(map)/locatie/kies/_utils/getAddressFromCoordinates.ts
@@ -4,15 +4,17 @@ export const getAddressFromCoordinates = async ({ lat, lng }: Coordinates) =>
   fetch(`https://api.pdok.nl/bzk/locatieserver/search/v3_1/reverse?lat=${lat}&lon=${lng}&rows=1&distance=30`)
     .then((res) => res.json())
     .then((result) => {
-      if (result.response.numFound) {
+      // If there is no address nearby (>30m) the location that is clicked (e.g. water or middle of a park),
+      // PDOK does not return an address. Therefore we use "Pinned on map".
+      if (result.response.numFound === 0) {
         return {
-          id: result.response.docs[0].id,
-          weergave_naam: result.response.docs[0].weergavenaam,
+          id: undefined,
+          weergave_naam: 'Locatie pin op kaart',
         }
       }
       return {
-        id: undefined,
-        weergave_naam: 'Locatie pin op kaart',
+        id: result.response.docs[0].id,
+        weergave_naam: result.response.docs[0].weergavenaam,
       }
     })
     // TODO: add generic error handling
