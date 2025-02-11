0b0712bc11a4b76581790fedde6b0731c4259cc4
Vincent <vdegraaf.vincent@gmail.com>
2025-02-11 12:58:57 +0100

    Add test case

--- apps/public/src/app/(map)/locatie/kies/_utils/getAddressFromCoordinates.test.ts
+++ apps/public/src/app/(map)/locatie/kies/_utils/getAddressFromCoordinates.test.ts
@@ -1,3 +1,8 @@
+import { http, HttpResponse } from 'msw'
+
+import { ENDPOINTS } from 'apps/public/src/mocks/endpoints'
+import { server } from 'apps/public/src/mocks/node'
+
 import { getAddressFromCoordinates } from './getAddressFromCoordinates'
 
 describe('getAddressFromCoordinates', () => {
@@ -12,7 +17,18 @@ describe('getAddressFromCoordinates', () => {
   })
 
   it('should return "Locatie pin op kaart" when PDOK cannot find a address (e.g. when clicked on water)', async () => {
-    const coordinates = { lat: 52.37239126063553, lng: 4.900905743712159 }
+    server.use(
+      http.get(ENDPOINTS.PDOK_REVERSE, () =>
+        HttpResponse.json({
+          response: {
+            numFound: 0,
+            docs: [],
+          },
+        }),
+      ),
+    )
+
+    const coordinates = { lat: 52.381387987672596, lng: 4.931926012164198 }
     const result = await getAddressFromCoordinates(coordinates)
 
     expect(result).toEqual({
