6619b1a30f1b1a2df7d60dcc86a17f2778c4f1bd
Vincent <vdegraaf.vincent@gmail.com>
2025-02-11 12:41:19 +0100

    Add Locatie pin op kaart

--- apps/public/src/app/(map)/locatie/kies/_utils/getAddressFromCoordinates.test.ts
+++ apps/public/src/app/(map)/locatie/kies/_utils/getAddressFromCoordinates.test.ts
@@ -10,4 +10,14 @@ describe('getAddressFromCoordinates', () => {
       weergave_naam: 'Nieuwmarkt 15, 1011JR Amsterdam',
     })
   })
+
+  it('should return "Locatie pin op kaart" when PDOK cannot find a address (e.g. when clicked on water)', async () => {
+    const coordinates = { lat: 52.37239126063553, lng: 4.900905743712159 }
+    const result = await getAddressFromCoordinates(coordinates)
+
+    expect(result).toEqual({
+      id: undefined,
+      weergave_naam: 'Locatie pin op kaart',
+    })
+  })
 })
--- apps/public/src/app/(map)/locatie/kies/_utils/getAddressFromCoordinates.ts
+++ apps/public/src/app/(map)/locatie/kies/_utils/getAddressFromCoordinates.ts
@@ -1,12 +1,20 @@
 import type { Coordinates } from 'apps/public/src/types'
 
 export const getAddressFromCoordinates = async ({ lat, lng }: Coordinates) =>
-  fetch(`https://api.pdok.nl/bzk/locatieserver/search/v3_1/reverse?lat=${lat}&lon=${lng}&rows=1`)
+  fetch(`https://api.pdok.nl/bzk/locatieserver/search/v3_1/reverse?lat=${lat}&lon=${lng}&rows=1&distance=30`)
     .then((res) => res.json())
-    .then((result) => ({
-      id: result.response.docs[0].id,
-      weergave_naam: result.response.docs[0].weergavenaam,
-    }))
+    .then((result) => {
+      if (result.response.numFound) {
+        return {
+          id: result.response.docs[0].id,
+          weergave_naam: result.response.docs[0].weergavenaam,
+        }
+      }
+      return {
+        id: undefined,
+        weergave_naam: 'Locatie pin op kaart',
+      }
+    })
     // TODO: add generic error handling
     // eslint-disable-next-line no-console
     .catch((error) => console.error('Error:', error))
