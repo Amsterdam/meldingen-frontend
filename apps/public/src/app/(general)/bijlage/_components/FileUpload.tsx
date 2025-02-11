54ba0a7f645ef2d38060cf21d0aeb6ca6bf9a09b
Vincent <vdegraaf.vincent@gmail.com>
2025-02-11 20:59:06 +0100

    Fix file upload

--- apps/public/src/app/(general)/bijlage/_components/FileUpload.tsx
+++ apps/public/src/app/(general)/bijlage/_components/FileUpload.tsx
@@ -1,59 +1,32 @@
-import { useState } from 'react'
-import type { ChangeEvent, InputHTMLAttributes } from 'react'
-
-import type { AttachmentOutput } from 'apps/public/src/apiClientProxy'
-import { postMeldingByMeldingIdAttachment } from 'apps/public/src/apiClientProxy'
-
-import { useMeldingContext } from '../../../../context/MeldingContextProvider'
+import type { AttachmentOutput } from '@meldingen/api-client'
+import type { InputHTMLAttributes } from 'react'
 
 import styles from './FileUpload.module.css'
 
 type Props = {
   id: string
+  handleOnChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  uploadedFiles: AttachmentOutput[]
 } & InputHTMLAttributes<HTMLInputElement>
 
-export const FileUpload = ({ id, ...restProps }: Props) => {
-  const [uploadedFiles, setUploadedFiles] = useState<AttachmentOutput[]>([])
-
-  const { data } = useMeldingContext()
-
-  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (data && event.currentTarget.files) {
-      const filesArray = Array.from(event.currentTarget.files)
-
-      filesArray.forEach(async (file: File) => {
-        try {
-          const result = await postMeldingByMeldingIdAttachment({
-            formData: { file },
-            meldingId: data.id,
-            token: data.token,
-          })
-          setUploadedFiles((currentFiles) => [...currentFiles, result])
-        } catch (error) {
-          console.error('Error uploading file: ', error)
-        }
-      })
-    }
-  }
-
-  return (
-    <>
-      <div className={styles.wrapper}>
-        <input {...restProps} multiple type="file" id={id} className={styles.input} onChange={handleOnChange} />
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label htmlFor={id} className={styles.label}>
-          <span className={styles.button}>Selecteer bestanden</span>
-          <span className={styles['drop-area-text']}>Of sleep de bestanden in dit vak.</span>
-        </label>
-      </div>
+export const FileUpload = ({ id, handleOnChange, uploadedFiles, ...restProps }: Props) => (
+  <>
+    <div className={styles.wrapper}>
+      <input {...restProps} multiple type="file" id={id} className={styles.input} onChange={handleOnChange} />
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label htmlFor={id} className={styles.label}>
+        <span className={styles.button}>Selecteer bestanden</span>
+        <span className={styles['drop-area-text']}>Of sleep de bestanden in dit vak.</span>
+      </label>
+    </div>
+    {uploadedFiles.length > 0 && (
       <div className="ams-mb--sm">
-        Dummy List
         {uploadedFiles.map((file) => (
           <div key={file.id}>
             <span>{file.original_filename}</span>
           </div>
         ))}
       </div>
-    </>
-  )
-}
+    )}
+  </>
+)
--- apps/public/src/app/(general)/bijlage/actions.ts
+++ apps/public/src/app/(general)/bijlage/actions.ts
@@ -1,5 +1,31 @@
 'use server'
 
+import { postMeldingByMeldingIdAttachment } from '@meldingen/api-client'
+import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-export const postAttachmentForm = async () => redirect('/contact')
+export const postAttachmentForm = async (fileList: FileList) => {
+  const cookieStore = await cookies()
+  const meldingId = cookieStore.get('id')
+  const token = cookieStore.get('token')
+
+  if (!meldingId || !token) return undefined
+
+  const files = Array.from(fileList)
+
+  try {
+    return await Promise.all(
+      files.map((file) =>
+        postMeldingByMeldingIdAttachment({
+          formData: { file },
+          meldingId: Number(meldingId.value),
+          token: token.value,
+        }),
+      ),
+    )
+  } catch (error) {
+    console.error('Error uploading file: ', error)
+  }
+}
+
+export const redirectToNextPage = async () => redirect('/contact')
