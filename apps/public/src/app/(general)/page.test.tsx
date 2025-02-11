e28b7866f4fb565b2fc42581304f91db3009964f
alimpens <a.limpens@amsterdam.nl>
2025-02-11 17:59:56 +0100

    Add tests

---
+++ apps/public/src/app/(general)/page.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react'
+import { http, HttpResponse } from 'msw'
+import { vi } from 'vitest'
+
+import { ENDPOINTS } from 'apps/public/src/mocks/endpoints'
+import mockFormData from 'apps/public/src/mocks/mockFormData.json'
+import { server } from 'apps/public/src/mocks/node'
+
+import { Home } from './Home'
+import Page from './page'
+
+vi.mock('./Home', () => ({
+  Home: vi.fn(() => <div>Home Component</div>),
+}))
+
+describe('Page', () => {
+  it('renders the Home component with form data', async () => {
+    const PageComponent = await Page()
+
+    render(PageComponent)
+
+    expect(screen.getByText('Home Component')).toBeInTheDocument()
+    expect(Home).toHaveBeenCalledWith({ formData: mockFormData.components }, {})
+  })
+
+  it('returns undefined if no primary form is found', async () => {
+    server.use(
+      http.get(ENDPOINTS.STATIC_FORM, () =>
+        HttpResponse.json([
+          {
+            id: '123',
+            type: 'not-primary',
+          },
+        ]),
+      ),
+    )
+
+    const PageComponent = await Page()
+
+    expect(PageComponent).toBeUndefined()
+  })
+})
--- apps/public/src/mocks/endpoints.ts
+++ apps/public/src/mocks/endpoints.ts
@@ -3,4 +3,6 @@ export const ENDPOINTS = {
   PDOK_REVERSE: 'https://api.pdok.nl/bzk/locatieserver/search/v3_1/reverse',
   PDOK_SUGGEST: 'https://api.pdok.nl/bzk/locatieserver/search/v3_1/suggest',
   MELDING_ATTACHMENT_BY_ID: '/melding/:id/attachment',
+  STATIC_FORM: '/static-form',
+  STATIC_FORM_BY_STATIC_FORM_ID: '/static-form/:staticFormId',
 }
