91302a2febeeb5d436af6f8f6f7a09e9bf6ddfec
alimpens <a.limpens@amsterdam.nl>
2025-02-13 17:16:04 +0100

    Update ADS and layout

--- apps/public/src/app/(general)/locatie/Locatie.tsx
+++ apps/public/src/app/(general)/locatie/Locatie.tsx
@@ -26,7 +26,7 @@ export const Locatie = ({ prevPage, locationData }: Props) => {
 
   return (
     <Grid paddingBottom="large" paddingTop="medium">
-      <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 2 }}>
+      <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
         <BackLink href={prevPage} className="ams-mb--xs">
           Vorige vraag
         </BackLink>
