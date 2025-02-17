941656ae071030780e52123ead1ba8e8dec23fc5
alimpens <a.limpens@amsterdam.nl>
2025-02-17 16:55:49 +0100

    Get primary form label from BE

--- apps/public/src/app/(general)/samenvatting/getSummaryData.ts
+++ apps/public/src/app/(general)/samenvatting/getSummaryData.ts
@@ -4,6 +4,7 @@ import type { Coordinates } from 'apps/public/src/types'
 
 type Props = {
   melding: MeldingOutput
+  primaryFormLabel: string
   additionalQuestionsAnswers: GetMeldingByMeldingIdAnswersResponse
   location: {
     name: string
@@ -11,7 +12,7 @@ type Props = {
   }
 }
 
-export const getSummaryData = ({ melding, additionalQuestionsAnswers, location }: Props) => {
+export const getSummaryData = ({ melding, primaryFormLabel, additionalQuestionsAnswers, location }: Props) => {
   const additionalQuestionsSummary = additionalQuestionsAnswers.map((answer) => ({
     key: answer.question.id,
     term: answer.question.text,
@@ -21,18 +22,18 @@ export const getSummaryData = ({ melding, additionalQuestionsAnswers, location }
   return [
     {
       key: 'primary',
-      term: 'Wat wilt u melden?', // TODO: moet uit de API komen
+      term: primaryFormLabel,
       description: melding.text,
     },
     ...additionalQuestionsSummary,
     location && {
       key: 'location',
-      term: 'Waar is het?', // TODO
+      term: 'Waar is het?', // TODO: use i18n slug here
       description: location.name,
     },
     (melding.email || melding.phone) && {
       key: 'contact',
-      term: 'Wat zijn uw contactgegevens?', // TODO
+      term: 'Wat zijn uw contactgegevens?', // TODO: use i18n slug here
       description: [melding.email, melding.phone],
     },
   ].filter((item) => item) // Filter out undefined items
--- apps/public/src/app/(general)/samenvatting/page.tsx
+++ apps/public/src/app/(general)/samenvatting/page.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from 'next'
 import { cookies } from 'next/headers'
 
-import { getMeldingByMeldingIdAnswers, getMeldingByMeldingIdMelder } from 'apps/public/src/apiClientProxy'
+import {
+  getMeldingByMeldingIdAnswers,
+  getMeldingByMeldingIdMelder,
+  getStaticForm,
+  getStaticFormByStaticFormId,
+} from 'apps/public/src/apiClientProxy'
 
 import { getSummaryData } from './getSummaryData'
 import { Summary } from './Summary'
@@ -25,12 +30,19 @@ export default async () => {
 
   if (!meldingId || !token) return undefined
 
+  const primaryFormId = await getStaticForm().then((response) => response.find((form) => form.type === 'primary')?.id)
+
+  if (!primaryFormId) return undefined
+
+  const primaryForm = (await getStaticFormByStaticFormId({ staticFormId: primaryFormId }))?.components[0]
+
   const melding = await getMeldingByMeldingIdMelder({ meldingId: parseInt(meldingId, 10), token })
 
   const additionalQuestionsAnswers = await getMeldingByMeldingIdAnswers({ meldingId: parseInt(meldingId, 10), token })
 
   const data = getSummaryData({
     melding,
+    primaryFormLabel: primaryForm?.label,
     additionalQuestionsAnswers,
     location: location ? JSON.parse(location) : undefined,
   }) as SummaryData
