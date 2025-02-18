6ed239caaa13f23a2e4e0d74cedca1b08714f925
alimpens <a.limpens@amsterdam.nl>
2025-02-18 16:41:39 +0100

    Make sure Alert is read by screen readers

--- apps/public/src/app/(map)/locatie/kies/_components/Notification/Notification.tsx
+++ apps/public/src/app/(map)/locatie/kies/_components/Notification/Notification.tsx
@@ -1,6 +1,19 @@
 import type { AlertProps } from '@amsterdam/design-system-react'
 import { Alert } from '@amsterdam/design-system-react'
+import { useEffect, useRef } from 'react'
 
 import styles from './Notification.module.css'
 
-export const Notification = (props: AlertProps) => <Alert {...props} className={styles.notification} severity="error" />
+export const Notification = (props: AlertProps) => {
+  const ref = useRef<HTMLDivElement>(null)
+
+  // Set focus on the notification when it is rendered,
+  // to make sure screen readers read the entire message
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.focus()
+    }
+  }, [ref])
+
+  return <Alert {...props} className={styles.notification} ref={ref} severity="error" tabIndex={-1} />
+}
