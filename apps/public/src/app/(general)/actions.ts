c6510f73ad080cb4f46fefd1c39e4e83ed785d8e
Vincent <vdegraaf.vincent@gmail.com>
2025-02-20 15:59:46 +0100

    Put state machine in transition without aanvullende vragen

--- apps/public/src/app/(general)/actions.ts
+++ apps/public/src/app/(general)/actions.ts
@@ -3,31 +3,37 @@
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-import { getFormClassificationByClassificationId, postMelding } from 'apps/public/src/apiClientProxy'
+import {
+  getFormClassificationByClassificationId,
+  postMelding,
+  putMeldingByMeldingIdAnswerQuestions,
+} from 'apps/public/src/apiClientProxy'
 
 export const postPrimaryForm = async (_: unknown, formData: FormData) => {
   const formDataObj = Object.fromEntries(formData)
 
   let nextPage = '/locatie'
 
+  const { classification, id, token } = await postMelding({ requestBody: { text: formDataObj.primary.toString() } })
+
+  // Set session variables in cookies
+  const cookieStore = await cookies()
+  cookieStore.set('id', id.toString())
+  cookieStore.set('token', token)
+
   try {
-    const { classification, id, token } = await postMelding({ requestBody: { text: formDataObj.primary.toString() } })
-
     if (classification) {
-      // Set session variables in cookies
-      const cookieStore = await cookies()
-      cookieStore.set('id', id.toString())
-      cookieStore.set('token', token)
-
       // Get entire form, in order to redirect to its first panel
       const nextFormData = await getFormClassificationByClassificationId({ classificationId: classification })
       const nextFormFirstKey = nextFormData.components && nextFormData.components[0].key
 
       nextPage = `/aanvullende-vragen/${classification}/${nextFormFirstKey}`
+    } else {
+      putMeldingByMeldingIdAnswerQuestions({ meldingId: id, token })
     }
   } catch (error) {
     return { message: (error as Error).message }
   }
 
-  return redirect(nextPage)
+  redirect(nextPage)
 }
