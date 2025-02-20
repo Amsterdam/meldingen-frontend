09ffa3b69400d34f31bb560f0b7c4c36f59103cd
Vincent de Graaf <vdegraaf.vincent@gmail.com>
2025-02-20 13:30:55 +0100

    Fix file upload (#724)

--- apps/public/src/app/(general)/bijlage/page.test.tsx
+++ apps/public/src/app/(general)/bijlage/page.test.tsx
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react'
+import { cookies } from 'next/headers'
+import type { Mock } from 'vitest'
 
 import Page from './page'
 
@@ -6,12 +8,45 @@ vi.mock('./Bijlage', () => ({
   Bijlage: vi.fn(() => <div>Bijlage Component</div>),
 }))
 
+vi.mock('next/headers', () => ({
+  cookies: vi.fn(),
+}))
+
 describe('Page', () => {
+  const mockCookies = {
+    get: vi.fn(),
+  }
+
+  beforeEach(() => {
+    vi.clearAllMocks()
+    ;(cookies as Mock).mockReturnValue(mockCookies)
+  })
+
   it('renders the Bijlage component', async () => {
+    mockCookies.get.mockImplementation((name) => {
+      if (name === 'id') {
+        return { value: '21' }
+      }
+      if (name === 'token') {
+        return { value: 'z123890' }
+      }
+      return undefined
+    })
+
     const PageComponent = await Page()
 
     render(PageComponent)
 
     expect(screen.getByText('Bijlage Component')).toBeInTheDocument()
+  })
+
+  it('should render undefined when there is no meldingId or token', async () => {
+    mockCookies.get.mockImplementation(() => {})
+
+    const PageComponent = await Page()
+
+    render(PageComponent)
+
+    expect(PageComponent).toBeUndefined()
   })
 })
