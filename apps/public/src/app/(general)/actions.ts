49ed4481fabd3282c5f76d3ad0115ed4160ebd01
Vincent <vdegraaf.vincent@gmail.com>
2025-02-21 10:52:10 +0100

    Adjust primary form action

--- apps/public/src/app/(general)/actions.ts
+++ apps/public/src/app/(general)/actions.ts
@@ -3,36 +3,43 @@
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-import {
-  getFormClassificationByClassificationId,
-  postMelding,
-  putMeldingByMeldingIdAnswerQuestions,
-} from 'apps/public/src/apiClientProxy'
+import type { ApiError } from 'apps/public/src/apiClientProxy'
+import { getFormClassificationByClassificationId, postMelding } from 'apps/public/src/apiClientProxy'
+
+const isApiError = (error: unknown): error is ApiError =>
+  typeof error === 'object' && error !== null && 'name' in error && error.name === 'ApiError'
 
 export const postPrimaryForm = async (_: unknown, formData: FormData) => {
   const formDataObj = Object.fromEntries(formData)
 
   let nextPage = '/locatie'
 
-  const { classification, id, token } = await postMelding({ requestBody: { text: formDataObj.primary.toString() } })
-
-  // Set session variables in cookies
-  const cookieStore = await cookies()
-  cookieStore.set('id', id.toString())
-  cookieStore.set('token', token)
-
   try {
+    const { classification, id, token } = await postMelding({ requestBody: { text: formDataObj.primary.toString() } })
+
+    // Set session variables in cookies
+    const cookieStore = await cookies()
+    cookieStore.set('id', id.toString())
+    cookieStore.set('token', token)
+
     if (classification) {
       // Get entire form, in order to redirect to its first panel
       const nextFormData = await getFormClassificationByClassificationId({ classificationId: classification })
       const nextFormFirstKey = nextFormData.components && nextFormData.components[0].key
 
       nextPage = `/aanvullende-vragen/${classification}/${nextFormFirstKey}`
-    } else {
-      putMeldingByMeldingIdAnswerQuestions({ meldingId: id, token })
     }
   } catch (error) {
-    return { message: (error as Error).message }
+    if (isApiError(error)) {
+      if (error.statusText === 'Not Found') {
+        // When a classification does not have additional question go to next page
+        redirect(nextPage)
+      }
+
+      return { message: error.message }
+    }
+    // eslint-disable-next-line no-console
+    console.error('Unexpected error caught:', error)
   }
 
   return redirect(nextPage)
