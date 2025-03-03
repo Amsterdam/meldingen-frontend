ad07fc389d99af4afb008aef22ae405bde49eb4b
alimpens <a.limpens@amsterdam.nl>
2025-03-03 18:03:18 +0100

    Fix build

--- apps/public/src/app/(general)/contact/page.tsx
+++ apps/public/src/app/(general)/contact/page.tsx
@@ -5,6 +5,10 @@ import { getStaticForm, getStaticFormByStaticFormId } from 'apps/public/src/apiC
 
 import { Contact } from './Contact'
 
+// TODO: Force dynamic rendering for now, because the api isn't accessible in the pipeline yet.
+// We can remove this when the api is deployed.
+export const dynamic = 'force-dynamic'
+
 export const metadata: Metadata = {
   title: 'Stap 3 van 4 - Gegevens - Gemeente Amsterdam',
 }
