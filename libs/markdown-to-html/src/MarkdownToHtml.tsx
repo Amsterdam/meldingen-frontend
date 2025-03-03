c89353aa77fd2343fbc86f070a7c1c7eb880d2f5
alimpens <a.limpens@amsterdam.nl>
2025-03-03 13:49:04 +0100

    Support both internal and external links

--- libs/markdown-to-html/src/MarkdownToHtml.tsx
+++ libs/markdown-to-html/src/MarkdownToHtml.tsx
@@ -3,18 +3,22 @@ import type { PropsWithChildren } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 const markdownToHtmlMap = {
-  a: ({ children, href }: PropsWithChildren<{ href?: string }>) => (
-    <Link href={href} target="_blank" rel="noopener noreferrer">
-      {children}
-    </Link>
-  ),
+  a: ({ children, href }: PropsWithChildren<{ href?: string }>) => {
+    const isExternal = href?.startsWith('http')
+
+    return (
+      <Link href={href} rel={isExternal ? 'external' : undefined}>
+        {children}
+      </Link>
+    )
+  },
   h2: ({ children }: PropsWithChildren) => <Heading level={2}>{children}</Heading>,
   h3: ({ children }: PropsWithChildren) => <Heading level={3}>{children}</Heading>,
   h4: ({ children }: PropsWithChildren) => <Heading level={4}>{children}</Heading>,
+  li: ({ children }: PropsWithChildren) => <li>{children}</li>,
+  ol: ({ children }: PropsWithChildren) => <OrderedList>{children}</OrderedList>,
   p: ({ children }: PropsWithChildren) => <Paragraph>{children}</Paragraph>,
   ul: ({ children }: PropsWithChildren) => <UnorderedList>{children}</UnorderedList>,
-  ol: ({ children }: PropsWithChildren) => <OrderedList>{children}</OrderedList>,
-  li: ({ children }: PropsWithChildren) => <li>{children}</li>,
 }
 
 const disallowedElements = ['h1', 'h5', 'h6', 'img']
