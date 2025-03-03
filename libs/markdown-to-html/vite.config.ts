8681117d962b80df87a7f0c366da9d706831757f
alimpens <a.limpens@amsterdam.nl>
2025-03-03 16:00:45 +0100

    Add tests

---
+++ libs/markdown-to-html/vite.config.ts
@@ -0,0 +1,18 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      enabled: true,
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: ['src/index.ts'],
+    },
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    setupFiles: ['./vitest.setup.ts'],
+    watch: false,
+  },
+})
