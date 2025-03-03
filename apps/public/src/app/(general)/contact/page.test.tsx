47eae02d92571b690fd9c3d7a84f662557400278
alimpens <a.limpens@amsterdam.nl>
2025-03-03 17:59:31 +0100

    Fix tests

--- apps/public/src/app/(general)/contact/page.test.tsx
+++ apps/public/src/app/(general)/contact/page.test.tsx
@@ -1,4 +1,8 @@
 import { render, screen } from '@testing-library/react'
+import { http, HttpResponse } from 'msw'
+
+import { ENDPOINTS } from 'apps/public/src/mocks/endpoints'
+import { server } from 'apps/public/src/mocks/node'
 
 import Page from './page'
 
@@ -8,10 +12,38 @@ vi.mock('./Contact', () => ({
 
 describe('Page', () => {
   it('renders the Contact component', async () => {
+    server.use(
+      http.get(ENDPOINTS.STATIC_FORM, () =>
+        HttpResponse.json([
+          {
+            id: '123',
+            type: 'contact',
+          },
+        ]),
+      ),
+    )
+
     const PageComponent = await Page()
 
     render(PageComponent)
 
     expect(screen.getByText('Contact Component')).toBeInTheDocument()
+  })
+
+  it('returns undefined if no contact form is found', async () => {
+    server.use(
+      http.get(ENDPOINTS.STATIC_FORM, () =>
+        HttpResponse.json([
+          {
+            id: '123',
+            type: 'not-contact',
+          },
+        ]),
+      ),
+    )
+
+    const PageComponent = await Page()
+
+    expect(PageComponent).toBeUndefined()
   })
 })
