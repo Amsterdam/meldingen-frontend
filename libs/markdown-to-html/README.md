77d5b8c3fa2d50bf6baddb360390ae7d4c6aa57a
alimpens <a.limpens@amsterdam.nl>
2025-03-03 15:30:39 +0100

    Replace children in lists instead of recreating CSS

--- libs/markdown-to-html/README.md
+++ libs/markdown-to-html/README.md
@@ -1,1 +1,4 @@
 # Markdown to HTML
+
+Converts Markdown strings to HTML elements.
+It uses [Amsterdam Design System](https://designsystem.amsterdam/) components for the correct markup and styling.
--- libs/markdown-to-html/src/MarkdownToHtml.tsx
+++ libs/markdown-to-html/src/MarkdownToHtml.tsx
@@ -1,8 +1,7 @@
 import { Column, Heading, Link, OrderedList, Paragraph, UnorderedList } from '@amsterdam/design-system-react'
-import { type PropsWithChildren } from 'react'
+import { Children, isValidElement } from 'react'
+import type { PropsWithChildren } from 'react'
 import ReactMarkdown from 'react-markdown'
-
-import styles from './MarkdownToHtml.module.css'
 
 const markdownToHtmlMap = {
   a: ({ children, href }: PropsWithChildren<{ href?: string }>) => {
@@ -17,16 +16,36 @@ const markdownToHtmlMap = {
   h2: ({ children }: PropsWithChildren) => <Heading level={2}>{children}</Heading>,
   h3: ({ children }: PropsWithChildren) => <Heading level={3}>{children}</Heading>,
   h4: ({ children }: PropsWithChildren) => <Heading level={4}>{children}</Heading>,
-  ol: ({ children }: PropsWithChildren) => <OrderedList>{children}</OrderedList>,
+  ol: ({ children }: PropsWithChildren) => {
+    const replacedChildren = Children.map(children, (child) => {
+      if (isValidElement(child) && child.type === 'li') {
+        return <OrderedList.Item {...child.props} />
+      }
+
+      return undefined
+    })
+
+    return <OrderedList>{replacedChildren}</OrderedList>
+  },
   p: ({ children }: PropsWithChildren) => <Paragraph>{children}</Paragraph>,
-  ul: ({ children }: PropsWithChildren) => <UnorderedList>{children}</UnorderedList>,
+  ul: ({ children }: PropsWithChildren) => {
+    const replacedChildren = Children.map(children, (child) => {
+      if (isValidElement(child) && child.type === 'li') {
+        return <UnorderedList.Item {...child.props} />
+      }
+
+      return undefined
+    })
+
+    return <UnorderedList>{replacedChildren}</UnorderedList>
+  },
 }
 
 const disallowedElements = ['blockquote', 'code', 'h1', 'h5', 'h6', 'hr', 'img']
 
 export const MarkdownToHtml = ({ children }: { children: string }) => (
-  <Column className={styles.container}>
-    <ReactMarkdown disallowedElements={disallowedElements} components={markdownToHtmlMap} skipHtml>
+  <Column>
+    <ReactMarkdown components={markdownToHtmlMap} disallowedElements={disallowedElements} skipHtml>
       {children}
     </ReactMarkdown>
   </Column>
