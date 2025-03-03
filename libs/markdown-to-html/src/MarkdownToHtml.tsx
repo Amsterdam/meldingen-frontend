0857e8251d08065a05ef6e2d7c9f2bb923813f80
alimpens <a.limpens@amsterdam.nl>
2025-03-03 14:59:21 +0100

    Add list styling, add space between components

--- libs/markdown-to-html/src/MarkdownToHtml.tsx
+++ libs/markdown-to-html/src/MarkdownToHtml.tsx
@@ -1,13 +1,15 @@
-import { Heading, Link, OrderedList, Paragraph, UnorderedList } from '@amsterdam/design-system-react'
-import type { PropsWithChildren } from 'react'
+import { Column, Heading, Link, OrderedList, Paragraph, UnorderedList } from '@amsterdam/design-system-react'
+import { type PropsWithChildren } from 'react'
 import ReactMarkdown from 'react-markdown'
+
+import styles from './MarkdownToHtml.module.css'
 
 const markdownToHtmlMap = {
   a: ({ children, href }: PropsWithChildren<{ href?: string }>) => {
     const isExternal = href?.startsWith('http')
 
     return (
-      <Link href={href} rel={isExternal ? 'external' : undefined}>
+      <Link {...(href ? { href } : {})} rel={isExternal ? 'external' : undefined} variant="inline">
         {children}
       </Link>
     )
@@ -15,16 +17,17 @@ const markdownToHtmlMap = {
   h2: ({ children }: PropsWithChildren) => <Heading level={2}>{children}</Heading>,
   h3: ({ children }: PropsWithChildren) => <Heading level={3}>{children}</Heading>,
   h4: ({ children }: PropsWithChildren) => <Heading level={4}>{children}</Heading>,
-  li: ({ children }: PropsWithChildren) => <li>{children}</li>,
   ol: ({ children }: PropsWithChildren) => <OrderedList>{children}</OrderedList>,
   p: ({ children }: PropsWithChildren) => <Paragraph>{children}</Paragraph>,
   ul: ({ children }: PropsWithChildren) => <UnorderedList>{children}</UnorderedList>,
 }
 
-const disallowedElements = ['h1', 'h5', 'h6', 'img']
+const disallowedElements = ['blockquote', 'code', 'h1', 'h5', 'h6', 'hr', 'img']
 
 export const MarkdownToHtml = ({ children }: { children: string }) => (
-  <ReactMarkdown disallowedElements={disallowedElements} components={markdownToHtmlMap} skipHtml>
-    {children}
-  </ReactMarkdown>
+  <Column className={styles.container}>
+    <ReactMarkdown disallowedElements={disallowedElements} components={markdownToHtmlMap} skipHtml>
+      {children}
+    </ReactMarkdown>
+  </Column>
 )
