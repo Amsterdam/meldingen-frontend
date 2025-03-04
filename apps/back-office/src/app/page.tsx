588614f564217a20845fcac74995413e3ecc8864
Vincent de Graaf <vdegraaf.vincent@gmail.com>
2025-03-04 08:53:46 +0100

    Update apps/back-office/src/app/page.tsx
    Co-authored-by: Aram <37216945+alimpens@users.noreply.github.com>

--- apps/back-office/src/app/page.tsx
+++ apps/back-office/src/app/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Overview } from './overzicht/Overview'
 
 export const metadata: Metadata = {
-  title: 'Overzicht | Meldingen',
+  title: 'Overzicht meldingen openbare ruimte - Gemeente Amsterdam',
 }
 
 export default async () => <Overview />
