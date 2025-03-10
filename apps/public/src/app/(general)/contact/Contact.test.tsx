bc029de536b4f2b5732cabb9d4732b0c3db2a34c
alimpens <a.limpens@amsterdam.nl>
2025-03-10 14:45:03 +0100

    Add tests

--- apps/public/src/app/(general)/contact/Contact.test.tsx
+++ apps/public/src/app/(general)/contact/Contact.test.tsx
@@ -42,11 +42,11 @@ describe('Contact', () => {
     render(<Contact formData={mockFormData} />)
 
     const emailInput = screen.getByRole('textbox', { name: 'Wat is uw e-mailadres? (niet verplicht)' })
-    const phoneInput = screen.getByRole('textbox', { name: 'Wat is uw telefoonnummer? (niet verplicht)' })
+    const telInput = screen.getByRole('textbox', { name: 'Wat is uw telefoonnummer? (niet verplicht)' })
     const submitButton = screen.getByRole('button')
 
     expect(emailInput).toBeInTheDocument()
-    expect(phoneInput).toBeInTheDocument()
+    expect(telInput).toBeInTheDocument()
     expect(submitButton).toBeInTheDocument()
   })
 
@@ -56,5 +56,38 @@ describe('Contact', () => {
     render(<Contact formData={mockFormData} />)
 
     expect(screen.queryByText('Test error message')).toBeInTheDocument()
+  })
+
+  it('should render descriptions that are connected the e-mail and tel inputs', () => {
+    render(<Contact formData={mockFormData} />)
+
+    const emailInput = screen.getByRole('textbox', { name: 'Wat is uw e-mailadres? (niet verplicht)' })
+    const telInput = screen.getByRole('textbox', { name: 'Wat is uw telefoonnummer? (niet verplicht)' })
+
+    expect(emailInput).toHaveAccessibleDescription('Test 1')
+    expect(telInput).toHaveAccessibleDescription('Test 2')
+  })
+
+  it('should not render descriptions that are connected the e-mail and tel inputs when they are not provided', () => {
+    const mockFormDataWithoutDescriptions = [
+      {
+        ...mockFormData[0],
+        description: '',
+      },
+      {
+        ...mockFormData[1],
+        description: '',
+      },
+    ]
+
+    render(<Contact formData={mockFormDataWithoutDescriptions} />)
+
+    const emailInput = screen.getByRole('textbox', { name: 'Wat is uw e-mailadres? (niet verplicht)' })
+    const telInput = screen.getByRole('textbox', { name: 'Wat is uw telefoonnummer? (niet verplicht)' })
+
+    expect(emailInput).not.toHaveAccessibleDescription()
+    expect(emailInput).not.toHaveAttribute('aria-describedby', 'email-input-description')
+    expect(telInput).not.toHaveAccessibleDescription()
+    expect(telInput).not.toHaveAttribute('aria-describedby', 'tel-input-description')
   })
 })
