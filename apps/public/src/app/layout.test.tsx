79d926299dc5cef9299c71d7f9b08818c2190841
alimpens <a.limpens@amsterdam.nl>
2025-03-10 10:50:33 +0100

    Use keys instead of translations

--- apps/public/src/app/layout.test.tsx
+++ apps/public/src/app/layout.test.tsx
@@ -2,6 +2,11 @@ import { render } from '@testing-library/react'
 
 import Layout from './layout'
 
+vi.mock('next-intl/server', () => ({
+  getLocale: vi.fn().mockReturnValue('nl'),
+  getMessages: vi.fn().mockReturnValue({}),
+}))
+
 describe('Layout', () => {
   it('should render', () => {
     const { container } = render(<Layout>Test</Layout>)
