9e20cf5998f6d8de3deffe8ac595c4e6d6e7cd6b
Aram <37216945+alimpens@users.noreply.github.com>
2025-03-11 09:31:41 +0100

    Use i18n for Attachments (#752)

--- libs/ui/src/FileInput/FileInput.tsx
+++ libs/ui/src/FileInput/FileInput.tsx
@@ -3,16 +3,17 @@ import type { InputHTMLAttributes } from 'react'
 import styles from './FileInput.module.css'
 
 export type Props = {
+  buttonText: string
+  dropAreaText: string
   id: string
 } & InputHTMLAttributes<HTMLInputElement>
 
-export const FileInput = ({ id, ...restProps }: Props) => (
+export const FileInput = ({ buttonText, dropAreaText, id, ...restProps }: Props) => (
   <div className={styles.wrapper}>
     <input id={id} type="file" className={styles.input} {...restProps} />
-    {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
     <label htmlFor={id} className={styles.label}>
-      <span className={styles.button}>Selecteer bestanden</span>
-      <span className={styles.dropAreaText}>Of sleep de bestanden in dit vak.</span>
+      <span className={styles.button}>{buttonText}</span>
+      <span className={styles.dropAreaText}>{dropAreaText}</span>
     </label>
   </div>
 )
