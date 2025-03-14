5ce3df324b2eb7291b621c11d2cac11e9024e084
Vincent <vdegraaf.vincent@gmail.com>
2025-03-14 14:57:13 +0100

    Secure application and wrap root in <Provider /> to access hooks

--- apps/back-office/src/app/Overview.tsx
+++ apps/back-office/src/app/Overview.tsx
@@ -2,11 +2,14 @@
 
 import { Paragraph } from '@amsterdam/design-system-react'
 import { Grid } from '@meldingen/ui'
+import { SessionProvider } from 'next-auth/react'
 
 export const Overview = () => (
-  <Grid paddingBottom="large" paddingTop="medium">
-    <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
-      <Paragraph>Back Office</Paragraph>
-    </Grid.Cell>
-  </Grid>
+  <SessionProvider>
+    <Grid paddingBottom="large" paddingTop="medium">
+      <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
+        <Paragraph>Back Office</Paragraph>
+      </Grid.Cell>
+    </Grid>
+  </SessionProvider>
 )
---
+++ apps/back-office/src/middleware.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/no-default-export, no-restricted-exports
+export { default } from 'next-auth/middleware'
