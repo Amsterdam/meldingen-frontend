8671de11a424e1b81daa170c22a7f0e3ab170143
alimpens <a.limpens@amsterdam.nl>
2025-03-17 13:28:53 +0100

    Update lint-staged config

--- apps/public/.lintstagedrc.mjs
+++ apps/public/.lintstagedrc.mjs
@@ -3,7 +3,7 @@ const path = require('path')
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
 
-module.exports = {
+export default {
   '*.{css,scss}': ['stylelint --allow-empty-input', 'prettier --check'],
   '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --check', 'tsc-files --noEmit'],
 }
