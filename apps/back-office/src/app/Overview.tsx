fdacffb68fb0120e2d2b5b7108de11698c8b188a
Vincent <vdegraaf.vincent@gmail.com>
2025-03-17 20:53:37 +0100

    Redirect to signin when RefreshAccessTokenError occurs

--- apps/back-office/src/app/Overview.tsx
+++ apps/back-office/src/app/Overview.tsx
@@ -2,17 +2,14 @@
 
 import { Paragraph } from '@amsterdam/design-system-react'
 import { Grid } from '@meldingen/ui'
-// import { signIn, useSession } from 'next-auth/'
 import { signIn, useSession } from 'next-auth/react'
 import { useEffect } from 'react'
 
 export const Overview = () => {
   const session = useSession()
-  console.log('--- session:', session)
-  // console.log('--- session:', session)
 
   useEffect(() => {
-    if (session?.error === 'RefreshAccessTokenError') {
+    if (session?.data?.error === 'RefreshAccessTokenError') {
       signIn() // Force sign in to hopefully resolve error
     }
   }, [session])
--- apps/back-office/src/app/api/auth/[...nextauth]/route.ts
+++ apps/back-office/src/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,7 @@ import KeycloakProvider from 'next-auth/providers/keycloak'
 
 /**
  * Takes a token, and returns a new token with updated
- * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * `accessToken`, `accessTokenExpiresAt`. The same for the refresh token If an error occurs,
  * returns the old token and an error property
  */
 const refreshAccessToken = async (token: JWT) => {
