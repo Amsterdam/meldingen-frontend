623701a8a41abca62e97138a05b6825de51f1b05
alimpens <a.limpens@amsterdam.nl>
2025-03-17 13:24:29 +0100

    Run `next lint` on staged files

---
+++ apps/public/.lintstagedrc.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+
+module.exports = {
+  '*.{css,scss}': ['stylelint --allow-empty-input', 'prettier --check'],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --check', 'tsc-files --noEmit'],
+}
