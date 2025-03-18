a5a7ac6b2d87bd354a1ae176907158e7d72518ab
Vincent <vdegraaf.vincent@gmail.com>
2025-03-18 08:02:29 +0100

    wip

--- apps/back-office/src/app/layout.tsx
+++ apps/back-office/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
+// import { getServerSession } from 'next-auth/next'
 import type { ReactNode } from 'react'
 
+import { auth } from './api/auth/[...nextauth]'
 import { NextAuthProvider } from './providers'
 
 import '@amsterdam/design-system-tokens/dist/index.css'
@@ -13,12 +15,18 @@ export const metadata: Metadata = {
   description: 'Beheer van meldingen over de openbare ruimte',
 }
 
-const RootLayout = ({ children }: { children: ReactNode }) => (
-  <html lang="nl">
-    <NextAuthProvider>
-      <body>{children}</body>
-    </NextAuthProvider>
-  </html>
-)
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  // const session = await getServerSession()
+  const session = await auth()
+  console.log('--- session SERVER:', session)
+
+  return (
+    <html lang="nl">
+      <NextAuthProvider>
+        <body>{children}</body>
+      </NextAuthProvider>
+    </html>
+  )
+}
 
 export default RootLayout
