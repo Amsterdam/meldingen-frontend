3f5095e9baaf85e986a371df7cb68ba788ea4840
Vincent <vdegraaf.vincent@gmail.com>
2025-03-21 09:52:51 +0100

    Move redirect on error to root layout

--- apps/back-office/src/app/Overview.tsx
+++ apps/back-office/src/app/Overview.tsx
@@ -1,20 +1,10 @@
 'use client'
 
 import { Paragraph } from '@amsterdam/design-system-react'
-import { signIn, useSession } from 'next-auth/react'
-import { useEffect } from 'react'
 
 import { Grid } from '@meldingen/ui'
 
 export const Overview = () => {
-  const session = useSession()
-
-  useEffect(() => {
-    if (session?.data?.error === 'RefreshAccessTokenError') {
-      signIn() // Force sign in to hopefully resolve error
-    }
-  }, [session])
-
   return (
     <Grid paddingBottom="large" paddingTop="medium">
       <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
--- apps/back-office/src/app/layout.tsx
+++ apps/back-office/src/app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from 'next'
+import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth'
 import type { ReactNode } from 'react'
 
+import { authOptions } from './authentication/authOptions'
 import { NextAuthProvider } from './providers'
 
 import '@amsterdam/design-system-tokens/dist/index.css'
 import '@amsterdam/design-system-assets/font/index.css'
+
 import '@amsterdam/design-system-css/dist/index.css'
 
 import './global.css'
@@ -13,12 +17,19 @@ export const metadata: Metadata = {
   description: 'Beheer van meldingen over de openbare ruimte',
 }
 
-const RootLayout = ({ children }: { children: ReactNode }) => (
-  <html lang="nl">
-    <NextAuthProvider>
-      <body>{children}</body>
-    </NextAuthProvider>
-  </html>
-)
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const session = await getServerSession(authOptions)
 
+  if (session?.error === 'RefreshAccessTokenError') {
+    redirect(`${process.env.NEXTAUTH_URL}/api/auth/signin`) // Force sign in to hopefully resolve error
+  }
+
+  return (
+    <html lang="nl">
+      <NextAuthProvider>
+        <body>{children}</body>
+      </NextAuthProvider>
+    </html>
+  )
+}
 export default RootLayout
