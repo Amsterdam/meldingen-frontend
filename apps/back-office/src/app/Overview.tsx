3db3dfaeeac660d5585969a1a96da2d6e8261eff
alimpens <a.limpens@amsterdam.nl>
2025-03-24 13:23:02 +0100

    Remove unnecessary change

--- apps/back-office/src/app/Overview.tsx
+++ apps/back-office/src/app/Overview.tsx
@@ -4,12 +4,10 @@ import { Paragraph } from '@amsterdam/design-system-react'
 
 import { Grid } from '@meldingen/ui'
 
-export const Overview = () => {
-  return (
-    <Grid paddingBottom="large" paddingTop="medium">
-      <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
-        <Paragraph>Back Office</Paragraph>
-      </Grid.Cell>
-    </Grid>
-  )
-}
+export const Overview = () => (
+  <Grid paddingBottom="large" paddingTop="medium">
+    <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
+      <Paragraph>Back Office</Paragraph>
+    </Grid.Cell>
+  </Grid>
+)
