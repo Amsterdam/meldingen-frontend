ace5023b69914777ef202e5c5a4ec2e372747a36
alimpens <a.limpens@amsterdam.nl>
2025-04-01 13:18:38 +0200

    Add token on page

--- apps/back-office/src/app/page.tsx
+++ apps/back-office/src/app/page.tsx
@@ -1,9 +1,21 @@
+import { client } from 'libs/api-client/src/client.gen'
 import type { Metadata } from 'next'
+import { getServerSession } from 'next-auth'
 
+import { authOptions } from './authentication/authOptions'
 import { Overview } from './Overview'
 
 export const metadata: Metadata = {
   title: 'Overzicht meldingen openbare ruimte - Gemeente Amsterdam',
 }
 
-export default async () => <Overview />
+export default async () => {
+  const session = await getServerSession(authOptions)
+
+  client.setConfig({
+    auth: () => session?.accessToken,
+    baseUrl: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
+  })
+
+  return <Overview />
+}
