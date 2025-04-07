9c4747d66cbe3943ecf29edcf9b3052ab0715834
alimpens <a.limpens@amsterdam.nl>
2025-04-07 14:05:05 +0200

    Revert unnecessary change

--- apps/back-office/src/app/page.tsx
+++ apps/back-office/src/app/page.tsx
@@ -6,6 +6,4 @@ export const metadata: Metadata = {
   title: 'Overzicht meldingen openbare ruimte - Gemeente Amsterdam',
 }
 
-export default async () => {
-  return <Overview />
-}
+export default async () => <Overview />
