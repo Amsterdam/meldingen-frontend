65b8f62d6508fb1891c70a0d9339bd32f53562d7
alimpens <a.limpens@amsterdam.nl>
2025-04-28 10:39:20 +0200

    Use defined mock data in tests

--- apps/public/src/app/(general)/samenvatting/page.test.tsx
+++ apps/public/src/app/(general)/samenvatting/page.test.tsx
@@ -4,6 +4,9 @@ import type { Mock } from 'vitest'
 
 import Page from './page'
 import { Summary } from './Summary'
+import mockAdditionalQuestionsAnswerData from 'apps/public/src/mocks/mockAdditionalQuestionsAnswerData.json'
+import mockFormData from 'apps/public/src/mocks/mockFormData.json'
+import mockMeldingData from 'apps/public/src/mocks/mockMeldingData.json'
 
 vi.mock('next/headers', () => ({
   cookies: vi.fn(),
@@ -43,24 +46,35 @@ describe('Page', () => {
 
     render(PageComponent)
 
+    const additionalQuestions = mockAdditionalQuestionsAnswerData.map((item) => ({
+      key: item.question.id,
+      term: item.question.text,
+      description: [item.text],
+    }))
+
+    const contact = {
+      key: 'contact',
+      term: 'summary.contact-label',
+      description: [mockMeldingData.email, mockMeldingData.phone],
+    }
+
+    const primaryForm = {
+      key: 'primary',
+      term: mockFormData.components[0].components[0].label,
+      description: [mockMeldingData.text],
+    }
+
     expect(screen.getByText('Summary Component')).toBeInTheDocument()
     expect(Summary).toHaveBeenCalledWith(
       {
-        additionalQuestions: [
-          { key: '35', term: 'Wat wilt u melden?', description: ['q1'] },
-          { key: '36', term: 'Text Field', description: ['q2'] },
-        ],
-        contact: {
-          key: 'contact',
-          term: 'summary.contact-label',
-          description: ['email@email.email', '0612345678'],
-        },
+        additionalQuestions: additionalQuestions,
+        contact: contact,
         location: {
           key: 'location',
           term: 'location.title',
           description: ['Test address'],
         },
-        primaryForm: { key: 'primary', term: 'First question', description: ['Alles'] },
+        primaryForm: primaryForm,
       },
       {},
     )
