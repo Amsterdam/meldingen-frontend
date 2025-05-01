309b91b3e3c5c8ffb21967089ed4b1dd349d2c34
alimpens <a.limpens@amsterdam.nl>
2025-05-01 13:26:34 +0200

    Fix tests

--- apps/public/src/mocks/endpoints.ts
+++ apps/public/src/mocks/endpoints.ts
@@ -3,7 +3,7 @@ export const ENDPOINTS = {
   PDOK_REVERSE: 'https://api.pdok.nl/bzk/locatieserver/search/v3_1/reverse',
   PDOK_SUGGEST: 'https://api.pdok.nl/bzk/locatieserver/search/v3_1/suggest',
   MELDING_BY_ID: 'http://localhost:8000/melding/:id/melder',
-  MELDING_ANSWERS_BY_ID: 'http://localhost:8000/melding/:id/answers',
+  MELDING_ANSWERS_BY_ID_MELDER: 'http://localhost:8000/melding/:id/answers/melder',
   MELDING_ATTACHMENT_BY_ID: 'http://localhost:8000/melding/:id/attachment',
   MELDING_ATTACHMENT_DELETE_BY_ID: 'http://localhost:8000/melding/:id/attachment/:attachmentId',
   MELDING_CONTACT_BY_ID: 'http://localhost:8000/melding/:id/contact',
