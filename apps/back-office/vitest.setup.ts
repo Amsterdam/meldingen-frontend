e55fc0c7510d9225e4727dad3b864f7648eb9265
Vincent <vdegraaf.vincent@gmail.com>
2025-05-02 14:11:39 +0200

    Install next-intl

--- apps/back-office/vitest.setup.ts
+++ apps/back-office/vitest.setup.ts
@@ -5,6 +5,26 @@ import { afterAll, afterEach, beforeAll } from 'vitest'
 
 import { server } from './src/mocks/node'
 
+// In our unit tests, we use the translation key instead of the real translation.
+// We mock useTranslations and getTranslations to return a function (the 't' function in our code)
+// that just returns the key
+vi.mock('next-intl', async () => {
+  const actual = (await vi.importActual('next-intl')) as any
+
+  return {
+    ...actual,
+    useTranslations: () => {
+      const t = (key: string) => key
+      t.rich = (key: string) => key
+      return t
+    },
+  }
+})
+
+vi.mock('next-intl/server', async () => ({
+  getTranslations: () => (key: string) => key,
+}))
+
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
