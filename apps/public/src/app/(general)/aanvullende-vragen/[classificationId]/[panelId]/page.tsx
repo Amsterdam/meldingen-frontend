4557ec41fbc74cce40ae858501b369fcbeddb536
alimpens <a.limpens@amsterdam.nl>
2025-05-19 16:40:04 +0200

    Remove unnecessary comments

--- apps/public/src/app/(general)/aanvullende-vragen/[classificationId]/[panelId]/page.tsx
+++ apps/public/src/app/(general)/aanvullende-vragen/[classificationId]/[panelId]/page.tsx
@@ -8,37 +8,7 @@ import { postForm } from './actions'
 import { AdditionalQuestions } from './AdditionalQuestions'
 import { handleApiError } from 'apps/public/src/handleApiError'
 
-// TODO: pagina's die niet bestaan moeten redirect krijgen
-// TODO: pagina's die wel bestaan maar geen token in url param moeten redirect krijgen
-
-// TODO: Force dynamic rendering for now, because the api isn't accessible in the pipeline yet.
-// We can remove this and uncomment generateStaticParams when the api is deployed.
 export const dynamic = 'force-dynamic'
-
-// export const generateStaticParams = async () => {
-//   // Fetch a list of all forms
-//   const forms = await getForm()
-
-//   // Use classification id to get the panel ids per form
-//   const panelIdsByForm = await Promise.all(
-//     forms
-//       .filter((form) => form.classification)
-//       .map(async (form) => {
-//         const formByClassification = await getFormClassificationByClassificationId({
-//           classificationId: form.classification as number,
-//         })
-
-//         const panelIds = formByClassification?.components.map((panel) => ({
-//           classification: `${form.classification}`,
-//           panelId: panel.key,
-//         }))
-
-//         return panelIds
-//       }),
-//   )
-
-//   return panelIdsByForm.flat()
-// }
 
 export const generateMetadata = async () => {
   const t = await getTranslations('additional-questions')
