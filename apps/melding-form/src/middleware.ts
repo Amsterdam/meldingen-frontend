fa6995c3eb2e9d3ca0bec7b4ff699c2a4ab65306
Vincent <vdegraaf.vincent@gmail.com>
2025-06-06 14:56:56 +0200

    Redirect to home when right cookies are missing

---
+++ apps/melding-form/src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export const middleware = (request: NextRequest) => {
+  const token = request.cookies.get('token')
+  const id = request.cookies.get('id')
+
+  if (!token || !id) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/aanvullende-vragen/:path*', '/locatie/:path*', '/bijlage', '/contact', '/samenvatting'],
+}
