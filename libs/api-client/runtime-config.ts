cb74fc2b20c3506185a090c6c2885d9a3c81c94b
ramonavic <r.hofmeister@amsterdam.nl>
2025-06-30 15:48:27 +0200

    adds alternative url for unit tests

--- libs/api-client/runtime-config.ts
+++ libs/api-client/runtime-config.ts
@@ -3,12 +3,10 @@ import type { CreateClientConfig } from './src/client.gen';
 export const createClientConfig: CreateClientConfig = (config) => {
   const isServer = typeof window === 'undefined';
 
-  console.log('calling create client config', process.env.NEXT_BACKEND_BASE_URL, process.env.NEXT_PUBLIC_BACKEND_BASE_URL)
-
   return {
     ...config,
     baseUrl: isServer
       ? process.env.NEXT_BACKEND_BASE_URL
-      : process.env.NEXT_PUBLIC_BACKEND_BASE_URL
+      : process.env.NEXT_PUBLIC_BACKEND_BASE_URL || "http://localhost:8000"
   };
 };
