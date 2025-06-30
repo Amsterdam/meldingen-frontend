380158bf6e5ab713ab25741557674c4e54615c01
ramonavic <r.hofmeister@amsterdam.nl>
2025-06-30 12:02:11 +0200

    initial commit that uses runTimeConfig and works in builds and dev mode

--- apps/melding-form/next.config.ts
+++ apps/melding-form/next.config.ts
@@ -3,6 +3,9 @@ import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  compiler: {
+    removeConsole: false,
+  },
 }
 
 const withNextIntl = createNextIntlPlugin()
