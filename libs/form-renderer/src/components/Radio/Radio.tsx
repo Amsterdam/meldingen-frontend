0ba48cca5310e33653044b8cc79fb83688db8690
Vincent <vdegraaf.vincent@gmail.com>
2025-07-04 11:18:01 +0200

    Store radio checkboxes in localStorage

--- libs/form-renderer/src/components/Radio/Radio.tsx
+++ libs/form-renderer/src/components/Radio/Radio.tsx
@@ -1,4 +1,5 @@
 import { Column, FieldSet, Radio as ADSRadio } from '@amsterdam/design-system-react'
+import { ChangeEvent, useEffect, useState } from 'react'
 
 import { MarkdownToHtml } from '@meldingen/markdown-to-html'
 
@@ -11,27 +12,61 @@ type Props = {
     label: string
     value: string
   }[]
+  onChange: (values: {
+    target: {
+      value: string[]
+      name: string
+    }
+  }) => void
+  defaultValue: string[]
 }
 
-export const Radio = ({ description, id, label, validate, values }: Props) => (
-  <FieldSet
-    aria-describedby={description ? `${id}-description` : undefined}
-    aria-required={validate?.required ? 'true' : undefined}
-    legend={label}
-    optional={!validate?.required}
-    role="radiogroup"
-  >
-    {description && (
-      <MarkdownToHtml id={`${id}-description`} type="description">
-        {description}
-      </MarkdownToHtml>
-    )}
-    <Column gap="x-small">
-      {values.map(({ label: radioLabel, value }) => (
-        <ADSRadio key={value} aria-required={validate?.required ? 'true' : undefined} name={id} value={value}>
-          {radioLabel}
-        </ADSRadio>
-      ))}
-    </Column>
-  </FieldSet>
-)
+export const Radio = ({ description, id, label, validate, values, onChange, defaultValue }: Props) => {
+  const [checkedValues, setCheckedValues] = useState<string[]>([])
+
+  useEffect(() => {
+    if (defaultValue) setCheckedValues(defaultValue)
+  }, [defaultValue])
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>, value: string) => {
+    const updated = event.target.checked ? [value] : checkedValues.filter((v) => v !== value)
+
+    setCheckedValues(updated)
+    onChange({
+      target: {
+        value: updated,
+        name: id,
+      },
+    })
+  }
+
+  return (
+    <FieldSet
+      aria-describedby={description ? `${id}-description` : undefined}
+      aria-required={validate?.required ? 'true' : undefined}
+      legend={label}
+      optional={!validate?.required}
+      role="radiogroup"
+    >
+      {description && (
+        <MarkdownToHtml id={`${id}-description`} type="description">
+          {description}
+        </MarkdownToHtml>
+      )}
+      <Column gap="x-small">
+        {values.map(({ label: radioLabel, value }) => (
+          <ADSRadio
+            key={value}
+            aria-required={validate?.required ? 'true' : undefined}
+            name={id}
+            value={value}
+            checked={checkedValues.includes(value)}
+            onChange={(e) => handleChange(e, value)}
+          >
+            {radioLabel}
+          </ADSRadio>
+        ))}
+      </Column>
+    </FieldSet>
+  )
+}
