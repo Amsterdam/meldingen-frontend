0f58956bb5af76f1d24a8a1a88a70b051b3d9b21
alimpens <a.limpens@amsterdam.nl>
2025-07-14 17:02:21 +0200

    Fix issue in summary

--- apps/melding-form/src/app/(general)/aanvullende-vragen/[classificationId]/[panelId]/_utils/mergeCheckboxAnswers.ts
+++ apps/melding-form/src/app/(general)/aanvullende-vragen/[classificationId]/[panelId]/_utils/mergeCheckboxAnswers.ts
@@ -9,15 +9,16 @@ type CheckboxAnswers = {
 export const mergeCheckboxAnswers = (answers: [string, string | File][]): Answer => {
   const checkboxAnswers = answers.filter(([key]) => key.startsWith('checkbox___'))
 
-  const groupedCheckboxAnswers = checkboxAnswers.reduce<CheckboxAnswers>((acc, [key, value]) => {
+  const groupedCheckboxAnswers = checkboxAnswers.reduce<CheckboxAnswers>((acc, [key]) => {
     const questionId = key.split('___')[1]
+    const value = key.split('___')[2]
 
     if (!acc[questionId]) {
       acc[questionId] = ''
     }
 
     if (acc[questionId].length === 0) {
-      acc[questionId] = value as string
+      acc[questionId] = value
     } else {
       acc[questionId] = `${acc[questionId]}, ${value}`
     }
