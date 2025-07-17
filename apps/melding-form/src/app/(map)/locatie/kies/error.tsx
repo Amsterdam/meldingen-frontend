200f55213938b2f29ddd128de87787925d247a11
alimpens <a.limpens@amsterdam.nl>
2025-07-17 17:56:09 +0200

    Copy error file to map

--- apps/melding-form/src/app/(map)/locatie/kies/error.tsx
+++ apps/melding-form/src/app/(map)/locatie/kies/error.tsx
@@ -1,9 +1,8 @@
 'use client'
 
-import { Grid, Heading, Page, Paragraph } from '@amsterdam/design-system-react'
+import { Button, Heading, Paragraph } from '@amsterdam/design-system-react'
+import { useTranslations } from 'next-intl'
 import { useEffect } from 'react'
-
-import { Footer, Header } from '@meldingen/ui'
 
 export default function Error({ error }: { error: Error & { digest?: string } }) {
   useEffect(() => {
@@ -12,23 +11,18 @@ export default function Error({ error }: { error: Error & { digest?: string } })
     console.error(error)
   }, [error])
 
+  const t = useTranslations('error')
+
   return (
-    <Page>
-      <title>Er is iets mis gegaan - Gemeente Amsterdam</title>
-      <Header />
-      <Grid paddingBottom="2x-large" paddingTop="x-large">
-        <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
-          <main>
-            <Heading level={1} className="ams-mb-l">
-              Er is iets mis gegaan
-            </Heading>
-            <Paragraph className="ams-mb-m" size="large">
-              De pagina die u probeert te bezoeken heeft een storing.
-            </Paragraph>
-          </main>
-        </Grid.Cell>
-      </Grid>
-      <Footer />
-    </Page>
+    <>
+      <title>{t('metadata.title')}</title>
+      <Heading className="ams-mb-m" level={1} size="level-2">
+        {t('title')}
+      </Heading>
+      <Button className="ams-mb-m" onClick={() => window.location.reload()}>
+        {t('button')}
+      </Button>
+      <Paragraph className="ams-mb-m">{t('description')}</Paragraph>
+    </>
   )
 }
