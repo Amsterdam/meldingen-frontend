b2a0e5fe156233997e5d99758b80293b073b4558
Vincent <vdegraaf.vincent@gmail.com>
2025-07-21 11:54:13 +0200

    Enlarge icons as designed

--- apps/melding-form/src/app/(map)/locatie/kies/_components/map-markers.ts
+++ apps/melding-form/src/app/(map)/locatie/kies/_components/map-markers.ts
@@ -2,36 +2,36 @@ import L from 'leaflet'
 
 export const gftAfvalIcon = L.icon({
   iconUrl: '/afval/gft.svg',
-  iconSize: [20, 20],
-  iconAnchor: [10, 19],
+  iconSize: [44, 44],
+  iconAnchor: [22, 44],
 })
 
 export const glasAfvalIcon = L.icon({
   iconUrl: '/afval/glas.svg',
-  iconSize: [20, 20],
-  iconAnchor: [10, 19],
+  iconSize: [44, 44],
+  iconAnchor: [22, 44],
 })
 
 export const papierAfvalIcon = L.icon({
   iconUrl: '/afval/papier.svg',
-  iconSize: [20, 20],
-  iconAnchor: [10, 19],
+  iconSize: [44, 44],
+  iconAnchor: [22, 44],
 })
 
 export const plasticAfvalIcon = L.icon({
   iconUrl: '/afval/plastic.svg',
-  iconSize: [20, 20],
-  iconAnchor: [10, 19],
+  iconSize: [44, 44],
+  iconAnchor: [22, 44],
 })
 
 export const restAfvalIcon = L.icon({
   iconUrl: '/afval/restafval.svg',
-  iconSize: [20, 20],
-  iconAnchor: [10, 19],
+  iconSize: [44, 44],
+  iconAnchor: [22, 44],
 })
 
 export const textielAfvalIcon = L.icon({
   iconUrl: '/afval/textiel.svg',
-  iconSize: [20, 20],
-  iconAnchor: [10, 19],
+  iconSize: [44, 44],
+  iconAnchor: [22, 44],
 })
