88e7a0b77b60b7ef35431decc7649d4377a79ac8
Aram <37216945+alimpens@users.noreply.github.com>
2025-07-21 13:59:47 +0200

    Add 404 page (#892)

---
+++ apps/melding-form/src/app/not-found.tsx
@@ -0,0 +1,37 @@
+import NextLink from 'next/link'
+import { getTranslations } from 'next-intl/server'
+
+import { MarkdownToHtml } from '@meldingen/markdown-to-html'
+import { Footer, Grid, Header, Heading, Page, StandaloneLink } from '@meldingen/ui'
+
+export const generateMetadata = async () => {
+  const t = await getTranslations('not-found')
+
+  return {
+    title: t('metadata.title'),
+  }
+}
+
+export default async function NotFound() {
+  const t = await getTranslations('not-found')
+
+  return (
+    <Page>
+      <Header />
+      <Grid paddingBottom="x-large">
+        <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
+          <main>
+            <Heading className="ams-mb-m" level={1} size="level-2">
+              {t('title')}
+            </Heading>
+            <MarkdownToHtml className="ams-mb-s">{t('description')}</MarkdownToHtml>
+            <NextLink href="/" legacyBehavior passHref>
+              <StandaloneLink>{t('link')}</StandaloneLink>
+            </NextLink>
+          </main>
+        </Grid.Cell>
+      </Grid>
+      <Footer />
+    </Page>
+  )
+}
