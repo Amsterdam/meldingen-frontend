c22afe798d7897f3c93d1752b71c51526bdc179c
alimpens <a.limpens@amsterdam.nl>
2025-07-24 13:51:13 +0200

    Use page pattern for id and token cookies

--- apps/melding-form/src/app/(map)/locatie/kies/page.tsx
+++ apps/melding-form/src/app/(map)/locatie/kies/page.tsx
@@ -16,10 +16,9 @@ export const generateMetadata = async () => {
 
 export default async () => {
   const cookieStore = await cookies()
-  const meldingId = cookieStore.get('id')?.value
-  const token = cookieStore.get('token')?.value
-
-  if (!meldingId || !token) throw new Error('Primary form id or token not found.')
+  // We check for the existence of these cookies in our middleware, so non-null assertion is safe here.
+  const meldingId = cookieStore.get('id')!.value
+  const token = cookieStore.get('token')!.value
 
   const { data, error } = await getMeldingByMeldingIdMelder({
     path: {
