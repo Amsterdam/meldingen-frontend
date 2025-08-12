a23ea90a4869ee9ed92e44777ec50097b49e8c4a
alimpens <a.limpens@amsterdam.nl>
2025-08-12 11:57:47 +0200

    Align error pages

--- apps/melding-form/src/app/(general)/cookie-storing/page.tsx
+++ apps/melding-form/src/app/(general)/cookie-storing/page.tsx
@@ -4,7 +4,7 @@ import { getTranslations } from 'next-intl/server'
 import { Heading, Paragraph, StandaloneLink } from '@meldingen/ui'
 
 export const generateMetadata = async () => {
-  const t = await getTranslations('cookies-storing')
+  const t = await getTranslations('cookie-storing')
 
   return {
     title: t('metadata.title'),
@@ -12,18 +12,16 @@ export const generateMetadata = async () => {
 }
 
 export default async () => {
-  const t = await getTranslations('cookies-storing')
+  const t = await getTranslations('cookie-storing')
 
   return (
     <>
-      <Heading level={1} className="ams-mb-l">
+      <Heading className="ams-mb-m" level={1} size="level-2">
         {t('title')}
       </Heading>
-      <Paragraph className="ams-mb-m" size="large">
-        {t('description')}
-      </Paragraph>
+      <Paragraph className="ams-mb-s">{t('description')}</Paragraph>
       <NextLink href="/" legacyBehavior passHref>
-        <StandaloneLink className="ams-mb-m">{t('link')}</StandaloneLink>
+        <StandaloneLink>{t('link')}</StandaloneLink>
       </NextLink>
     </>
   )
