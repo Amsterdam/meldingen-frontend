29a0d310b7ac4813fa23803f47158f3d5f04fcd3
ramonavic <r.hofmeister@amsterdam.nl>
2025-08-25 11:18:05 +0200

    add logging

--- libs/api-client/runtime-config.ts
+++ libs/api-client/runtime-config.ts
@@ -3,6 +3,8 @@ import type { CreateClientConfig } from './src/client.gen';
 export const createClientConfig: CreateClientConfig = (config) => {
   const isServer = typeof window === 'undefined';
 
+  console.log(isServer, process.env.NEXT_INTERNAL_BACKEND_BASE_URL, process.env.NEXT_PUBLIC_BACKEND_BASE_UR)
+
   return {
     ...config,
     baseUrl: isServer
