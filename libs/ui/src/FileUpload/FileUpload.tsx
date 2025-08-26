33939a2d5e3fcf9ff076691d9d8ad7035bab2ed1
alimpens <a.limpens@amsterdam.nl>
2025-08-26 17:49:07 +0200

    Add translatable labels, open file upload dialog on click

--- libs/ui/src/FileUpload/FileUpload.tsx
+++ libs/ui/src/FileUpload/FileUpload.tsx
@@ -1,9 +1,28 @@
 import styles from './FileUpload.module.css'
 
-export const FileUpload = () => {
+export const FileUpload = ({
+  buttonText = 'Bestanden kiezen',
+  dropAreaText = 'Sleep bestanden in dit vak of',
+  onChange,
+}: {
+  buttonText?: string
+  dropAreaText?: string
+  onChange: (files: FileList | null) => void
+}) => {
+  const handleClick = () => {
+    // Create a temporary input in order to open the file upload dialog
+    const input = document.createElement('input')
+    input.type = 'file'
+    input.multiple = true
+    input.onchange = () => {
+      onChange(input.files)
+    }
+    input.click()
+  }
+
   return (
-    <button className={styles.upload} type="button">
-      <span>Sleep bestanden in dit vak of</span> <span className={styles.button}>Bestanden kiezen</span>
+    <button className={styles.upload} type="button" onClick={handleClick}>
+      <span>{dropAreaText}</span> <span className={styles.button}>{buttonText}</span>
     </button>
   )
 }
