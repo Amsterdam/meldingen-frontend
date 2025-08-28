c70dfabe0d566b2b1c747988c22529b08a0d2340
alimpens <a.limpens@amsterdam.nl>
2025-08-28 16:07:15 +0200

    Refactor getFiles

--- libs/ui/src/FileUpload/FileUpload.tsx
+++ libs/ui/src/FileUpload/FileUpload.tsx
@@ -4,31 +4,26 @@ import styles from './FileUpload.module.css'
 
 // Based on https://design-system.service.gov.uk/components/file-upload/#using-the-improved-file-upload-component
 
+// Some browsers support dataTransfer.files and some support dataTransfer.items
+// For that reason, we handle both here.
+// See also https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop#process_the_drop
 const getFiles = (dataTransfer: DataTransfer): FileList => {
-  // Some browsers support dataTransfer.files and some support dataTransfer.items
-  // For that reason, we handle both here.
-  // See also https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop#process_the_drop
-
   // If dataTransfer.files is present and has files, return it directly
   if (dataTransfer.files && dataTransfer.files.length > 0) {
     return dataTransfer.files
   }
 
   // Otherwise, collect files from items and construct a FileList
-  const files: File[] = []
+  const dt = new DataTransfer()
   if (dataTransfer.items) {
-    for (let i = 0; i < dataTransfer.items.length; i++) {
-      const item = dataTransfer.items[i]
+    ;[...dataTransfer.items].forEach((item) => {
       if (item.kind === 'file') {
         const file = item.getAsFile()
-        if (file) files.push(file)
+        if (file) dt.items.add(file)
       }
-    }
+    })
   }
 
-  // Create a FileList from the array of files using a DataTransfer
-  const dt = new DataTransfer()
-  files.forEach((file) => dt.items.add(file))
   return dt.files
 }
 
