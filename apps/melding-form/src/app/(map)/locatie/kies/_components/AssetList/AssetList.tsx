39effbc1dbc0c7d5bdd6ef5c4cf15f1c3ad0666f
Vincent <vdegraaf.vincent@gmail.com>
2025-09-05 15:22:08 +0200

    Update address when changing asset selection

--- apps/melding-form/src/app/(map)/locatie/kies/_components/AssetList/AssetList.tsx
+++ apps/melding-form/src/app/(map)/locatie/kies/_components/AssetList/AssetList.tsx
@@ -2,7 +2,7 @@ import { Checkbox } from '@amsterdam/design-system-react'
 import clsx from 'clsx'
 import { Dispatch, SetStateAction } from 'react'
 
-import { Feature } from '@meldingen/api-client'
+import { Feature, Point } from '@meldingen/api-client'
 
 import { MAX_ASSETS } from '../../_utils/addAssetLayerToMap'
 import type { Coordinates } from 'apps/melding-form/src/types'
@@ -12,16 +12,40 @@ import styles from './AssetList.module.css'
 export type Props = {
   assetList: Feature[]
   selectedAssets: Feature[]
-  setCoordinates: (coordinates: Coordinates) => void
+  setCoordinates: (coordinates?: Coordinates) => void
   setSelectedAssets: Dispatch<SetStateAction<Feature[]>>
 }
 
-export const AssetList = ({ assetList, selectedAssets, setSelectedAssets }: Props) => {
+const isPoint = (geometry: Feature['geometry']): geometry is Point => geometry?.type === 'Point'
+
+export const AssetList = ({ assetList, selectedAssets, setCoordinates, setSelectedAssets }: Props) => {
   if (assetList.length === 0 && selectedAssets.length === 0) return
 
   const filteredList = assetList.filter(
     (asset) => !selectedAssets.some((selectedAsset) => selectedAsset.id === asset.id),
   )
+
+  const updateSelectedAsset = (asset: Feature) => {
+    if (selectedAssets.length > 1) {
+      const previousSelectedAssetCoordinates = selectedAssets[1].geometry.coordinates
+      const [y, x] = previousSelectedAssetCoordinates
+      setCoordinates({ lat: x, lng: y })
+    } else {
+      setCoordinates()
+    }
+
+    setSelectedAssets((assetList) => {
+      return assetList.filter((a) => a.id !== asset.id)
+    })
+  }
+
+  const updateUnselectedAsset = (asset: Feature) => {
+    if (isPoint(asset.geometry)) {
+      const [y, x] = asset.geometry.coordinates
+      setCoordinates({ lat: x, lng: y })
+    }
+    setSelectedAssets((assetList) => [asset, ...assetList])
+  }
 
   return (
     <div className={clsx(styles.container, 'ams-mb-m')}>
@@ -30,11 +54,7 @@ export const AssetList = ({ assetList, selectedAssets, setSelectedAssets }: Prop
         const idNummer = asset.properties.id_nummer as string
 
         return (
-          <Checkbox
-            key={idNummer}
-            onChange={() => setSelectedAssets((assetList) => assetList.filter((a) => a !== asset))}
-            checked
-          >
+          <Checkbox key={idNummer} onChange={() => updateSelectedAsset(asset)} checked>
             {idNummer}
           </Checkbox>
         )
@@ -46,7 +66,7 @@ export const AssetList = ({ assetList, selectedAssets, setSelectedAssets }: Prop
         return (
           <Checkbox
             key={idNummer}
-            onChange={() => setSelectedAssets((assetList) => [asset, ...assetList])}
+            onChange={() => updateUnselectedAsset(asset)}
             disabled={selectedAssets.length === MAX_ASSETS}
           >
             {idNummer}
