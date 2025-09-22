8148a9e6882ebc79743d8f25daba81be2a59d5d6
Vincent <vdegraaf.vincent@gmail.com>
2025-09-22 14:53:51 +0200

    Remove accidental code

--- libs/form-builder/src/components/Textarea/SelectBoxes.tsx
+++
@@ -1,9 +0,0 @@
-import { Components } from '@formio/react'
-
-import { editForm } from '../SelectBoxes/editForm'
-
-const FormioSelectBoxes = (Components as any).components.selectboxes
-
-export class SelectBoxes extends FormioSelectBoxes {
-  static editForm = editForm
-}
