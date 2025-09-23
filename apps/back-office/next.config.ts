352e23412b06c002e7a3a9ea0a3442197496a11e
Vincent <vdegraaf.vincent@gmail.com>
2025-09-23 13:04:15 +0200

    Explicitly tell next which lock-file is the root

--- apps/back-office/next.config.ts
+++ apps/back-office/next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
+import path from 'path'
+
 const nextConfig: NextConfig = {
   output: 'standalone',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  outputFileTracingRoot: path.resolve(__dirname, '../../'),
 }
 
 const withNextIntl = createNextIntlPlugin()
--- apps/melding-form/next.config.ts
+++ apps/melding-form/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
+import path from 'path'
+
 const nextConfig: NextConfig = {
   output: 'standalone',
+  outputFileTracingRoot: path.resolve(__dirname, '../../'),
 }
 
 const withNextIntl = createNextIntlPlugin()
