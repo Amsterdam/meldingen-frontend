add4060cc7410141bf17a84aefaf182312a7e931
Vincent <vdegraaf.vincent@gmail.com>
2025-10-24 15:26:12 +0200

    Add error text message field to form builder components

--- libs/form-builder/src/components/SelectBoxes/editForm.ts
+++ libs/form-builder/src/components/SelectBoxes/editForm.ts
@@ -96,6 +96,13 @@ export const editForm = () => ({
               input: true,
             },
             {
+              weight: 11,
+              type: 'textfield',
+              label: 'Error handling text',
+              key: 'validate.required_error_message',
+              input: true,
+            },
+            {
               type: 'panel',
               title: 'JSONLogic Validation',
               key: 'json-validation-json',
