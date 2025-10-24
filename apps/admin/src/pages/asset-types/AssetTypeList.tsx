7c8e69ecbd490a31a60fbff6a934790b9d6d20bb
Vincent <vdegraaf.vincent@gmail.com>
2025-10-24 10:55:30 +0200

    Remove broken search bar

--- apps/admin/src/pages/asset-types/AssetTypeList.tsx
+++ apps/admin/src/pages/asset-types/AssetTypeList.tsx
@@ -1,9 +1,7 @@
-import { Datagrid, List, TextField, TextInput } from 'react-admin'
-
-const assetTypeFilters = [<TextInput label="ra.action.search" source="q" alwaysOn key="search" />]
+import { Datagrid, List, TextField } from 'react-admin'
 
 export const AssetTypeList = () => (
-  <List filters={assetTypeFilters}>
+  <List>
     <Datagrid rowClick="edit">
       <TextField source="name" />
       <TextField source="id" />
