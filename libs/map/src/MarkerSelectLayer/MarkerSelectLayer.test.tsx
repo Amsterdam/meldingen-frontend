5764cd3d83307be3b851f638fdd6f19739598171
alimpens <a.limpens@amsterdam.nl>
2025-11-03 17:12:18 +0100

    Add list of tests for MarkerSelectLayer

--- libs/map/src/MarkerSelectLayer/MarkerSelectLayer.test.tsx
+++ libs/map/src/MarkerSelectLayer/MarkerSelectLayer.test.tsx
@@ -21,252 +21,52 @@ describe('MarkerSelectLayer', () => {
   })
 })
 
-// describe('addAssetLayerToMap', () => {
-//   beforeEach(() => {
-//     const container = document.createElement('div')
-//     defaultProps.mapInstance = L.map(container)
-//     defaultProps.assetLayerRef = {
-//       current: { remove: removeMock } as unknown as L.Layer,
-//     } as RefObject<L.Layer | null>
-//   })
+// MARKER EFFECT
 
-//   it('returns early when mapInstance is not provided', () => {
-//     const result = addAssetLayerToMap({ ...defaultProps, mapInstance: null })
+// returns early when mapInstance is not provided
 
-//     expect(result).toBeUndefined()
-//     expect(removeMock).not.toBeCalled()
-//   })
+// returns early when there are no assets
 
-//   it('returns early when there are no assets', () => {
-//     const result = addAssetLayerToMap({ ...defaultProps, assetList: [] })
+// removes the previous asset layer if it exists
 
-//     expect(result).toBeUndefined()
-//     expect(removeMock).not.toBeCalled()
-//   })
+// adds a new asset layer if it doesn't exist
 
-//   it('removes the previous asset layer if it exists', () => {
-//     addAssetLayerToMap({ ...defaultProps })
+// calls getContainerFeatureIcon for each feature
 
-//     expect(removeMock).toHaveBeenCalledOnce()
-//   })
+// assigns marker to markersRef using feature id
 
-//   it('adds a new geoJSON layer to the map', () => {
-//     addAssetLayerToMap({ ...defaultProps })
+// creates cluster icons with correct properties and disables keyboard
 
-//     expect(defaultProps.mapInstance?.hasLayer(defaultProps.assetLayerRef.current!)).toBe(true)
-//   })
+// skips features without geometry or with non-Point geometry
 
-//   it('calls getContainerFeatureIcon for each feature', () => {
-//     addAssetLayerToMap({ ...defaultProps })
+// // CLICKED WHEN SELECTED
 
-//     expect(getContainerFeatureIcon).toHaveBeenCalledWith(containerAssets[0], false)
-//   })
+// // resets onMaxMarkersReached
 
-//   it('uses selectedAssetIcon for selected feature', () => {
-//     addAssetLayerToMap({ ...defaultProps, selectedAssets: [containerAssets[0]] })
+// // calls onSelectedMarkersChange with list of selected markers minus clicked marked
 
-//     expect(getContainerFeatureIcon).toHaveBeenCalledWith(containerAssets[0], true)
-//   })
+// // sets selected point as undefined if it is the last marker
 
-//   it('sets selectedAssets and coordinates when marker is clicked and not selected', () => {
-//     addAssetLayerToMap({ ...defaultProps })
+// // sets address to second last selected asset when last selected asset is deselected by clicking marker
 
-//     const marker = defaultProps.assetMarkersRef.current[containerAssets[0].id!]
-//     marker.fire('click')
+// // CLICKED WHEN NOT SELECTED
 
-//     expect(defaultProps.setSelectedAssets).toHaveBeenCalled()
-//     expect(defaultProps.setCoordinates).toHaveBeenCalledWith({
-//       lat: marker.getLatLng().lat,
-//       lng: marker.getLatLng().lng,
-//     })
-//   })
+// // does not add more than maxMarkers when marker is clicked
 
-//   it('does not add more than MAX_ASSETS when marker is clicked', () => {
-//     const assetList = Array(6)
-//       .fill(containerAssets[0])
-//       .map((asset, index) => ({ ...asset, id: (index + 1).toString() }))
-//     const maxAssets = Array(5)
-//       .fill(containerAssets[0])
-//       .map((asset, index) => ({ ...asset, id: (index + 1).toString() }))
+// calls onSelectedMarkersChange and updates selected point
 
-//     addAssetLayerToMap({ ...defaultProps, selectedAssets: maxAssets, assetList })
+// fetchMarkersOnMoveEnd
 
-//     const marker = defaultProps.assetMarkersRef.current['6']
+// // returns undefined if classification is undefined
 
-//     marker.fire('click')
+// // does not fetch assets if classification has no asset support
 
-//     expect(defaultProps.setSelectedAssets).not.toHaveBeenCalled()
-//     expect(defaultProps.setCoordinates).not.toHaveBeenCalled()
-//   })
+// // does not fetch assets if map is hidden
 
-//   it('resets notification and coordinates and removes asset from selectedAssets when a selected marker is clicked', () => {
-//     addAssetLayerToMap({ ...defaultProps, selectedAssets: [containerAssets[0]] })
+// // calls setAssetList with fetched assets
 
-//     const marker = defaultProps.assetMarkersRef.current[containerAssets[0].id!]
-//     marker.fire('click')
+// // calls onMarkersChange with fetched assets
 
-//     expect(defaultProps.setNotificationType).toBeCalledWith(null)
-//     expect(defaultProps.setSelectedAssets).toHaveBeenCalled()
-//     expect(defaultProps.setCoordinates).toHaveBeenCalledWith(undefined)
-//   })
+// // throws an error if the API call fails
 
-//   it('resets coordinates when last selected asset is deselected by clicking marker', () => {
-//     addAssetLayerToMap({ ...defaultProps, selectedAssets: [containerAssets[0]] })
-
-//     const marker = defaultProps.assetMarkersRef.current[containerAssets[0].id!]
-//     marker.fire('click')
-
-//     expect(defaultProps.setCoordinates).toHaveBeenCalledWith(undefined)
-//     expect(defaultProps.setSelectedAssets).toHaveBeenCalled()
-//   })
-
-//   it('sets address to second last selected asset when last selected asset is deselected by clicking marker', () => {
-//     addAssetLayerToMap({ ...defaultProps, selectedAssets: containerAssets })
-
-//     const marker = defaultProps.assetMarkersRef.current[containerAssets[0].id!]
-//     marker.fire('click')
-
-//     // @ts-expect-error an asset always has coordinates
-//     const [y, x] = containerAssets[1].geometry.coordinates
-
-//     expect(defaultProps.setCoordinates).toHaveBeenCalledWith({ lat: x, lng: y })
-//     expect(defaultProps.setSelectedAssets).toHaveBeenCalled()
-//   })
-
-//   it('assigns marker to assetMarkersRef using feature id', () => {
-//     addAssetLayerToMap({ ...defaultProps })
-
-//     expect(defaultProps.assetMarkersRef.current[containerAssets[0].id!]).toBeInstanceOf(L.Marker)
-//   })
-
-//   it('creates cluster icons with correct properties and disables keyboard', () => {
-//     // Create a mock cluster object
-//     const mockCluster = {
-//       getChildCount: () => 7,
-//       options: {},
-//     } as unknown as L.MarkerCluster
-
-//     const icon = createClusterIcon(mockCluster)
-
-//     expect(mockCluster.options.keyboard).toBe(false)
-//     expect(icon.options.html).toBe('7')
-//     expect(icon.options.className).toBe('meldingen-cluster')
-//     expect(icon.options.iconSize).toEqual([70, 70])
-//     expect(icon.options.iconAnchor).toEqual([35, 35])
-//   })
-
-//   it('skips features without geometry or with non-Point geometry', () => {
-//     const assetList = [
-//       { ...containerAssets[0], geometry: null },
-//       {
-//         ...containerAssets[0],
-//         geometry: {
-//           type: 'LineString',
-//           coordinates: [
-//             [0, 0],
-//             [1, 1],
-//           ],
-//         },
-//       },
-//       { ...containerAssets[0], geometry: { type: 'Point', coordinates: [1, 2] } },
-//     ] as Feature[]
-
-//     const assetMarkersRef = { current: {} } as RefObject<Record<string, L.Marker>>
-
-//     addAssetLayerToMap({
-//       ...defaultProps,
-//       assetList,
-//       assetMarkersRef,
-//     })
-
-//     // Only the Point geometry should result in a marker
-//     expect(Object.keys(assetMarkersRef.current)).toHaveLength(1)
-//     expect(assetMarkersRef.current[assetList[2].id!]).toBeInstanceOf(L.Marker)
-//   })
-// })
-
-// describe('fetchAssets', () => {
-//   it('returns undefined if classification is undefined', async () => {
-//     const result = await fetchAssets({ ...defaultProps, classification: undefined })
-
-//     expect(result).toBeUndefined()
-//     expect(defaultProps.mapInstance.getZoom).not.toHaveBeenCalled()
-//   })
-
-//   it('does not fetch assets if classification has no asset support', async () => {
-//     const result = await fetchAssets({ ...defaultProps, classification: 'invalid-classification' })
-
-//     expect(result).toBeUndefined()
-//     expect(defaultProps.mapInstance.getZoom).not.toHaveBeenCalled()
-//   })
-
-//   it('does not fetch assets if map is hidden', async () => {
-//     const hiddenMapInstance = {
-//       ...mockMapInstance,
-//       getSize: vi.fn(() => ({ x: 0, y: 0 })),
-//     } as unknown as L.Map
-
-//     const result = await fetchAssets({ ...defaultProps, mapInstance: hiddenMapInstance })
-
-//     expect(result).toBeUndefined()
-//     expect(defaultProps.mapInstance.getZoom).not.toHaveBeenCalled()
-//   })
-
-//   it('calls setAssetList with fetched assets', async () => {
-//     await fetchAssets(defaultProps)
-
-//     expect(defaultProps.setAssetList).toHaveBeenCalledWith(containerAssets)
-//   })
-
-//   it('throws an error if the API call fails', async () => {
-//     server.use(
-//       http.get(ENDPOINTS.GET_WFS_BY_NAME, () => HttpResponse.json({ detail: 'Error message' }, { status: 500 })),
-//     )
-
-//     await expect(fetchAssets(defaultProps)).rejects.toThrow('Error message')
-//   })
-
-//   it('calls setAssetsList with empty array and remove layer if zoom is below threshold', async () => {
-//     const lowZoomMapInstance = {
-//       ...mockMapInstance,
-//       getZoom: vi.fn(() => 15),
-//     } as unknown as L.Map
-
-//     const mockAssetLayerRef = { current: { remove: vi.fn() } as unknown as L.Layer }
-
-//     await fetchAssets({
-//       ...defaultProps,
-//       mapInstance: lowZoomMapInstance,
-//       assetLayerRef: mockAssetLayerRef,
-//     })
-
-//     expect(defaultProps.setAssetList).toHaveBeenCalledWith([])
-//     expect(mockAssetLayerRef.current?.remove).toBeCalled()
-//   })
-// })
-
-// describe('updateAssetMarkers', () => {
-//   it('does not update markers if mapInstance is null', () => {
-//     const result = updateAssetMarkers({ ...defaultProps, mapInstance: null })
-
-//     expect(result).toBeUndefined()
-//   })
-
-//   it('does not update markers if assetMarkersRef is empty', () => {
-//     const result = updateAssetMarkers({ ...defaultProps, assetMarkersRef: { current: {} } })
-
-//     expect(result).toBeUndefined()
-//   })
-
-//   it('sets selectedAssetsIcon for selected assets', () => {
-//     updateAssetMarkers(defaultProps)
-
-//     expect(defaultProps.assetMarkersRef.current['1'].setIcon).toHaveBeenCalledWith({ icon: 'container-selected' })
-//   })
-
-//   it('sets container icon for unselected assets', () => {
-//     updateAssetMarkers(defaultProps)
-
-//     expect(defaultProps.assetMarkersRef.current['2'].setIcon).toHaveBeenCalledWith({ icon: 'container' })
-//   })
-// })
+// // calls onMarkersChange with empty array and remove layer if zoom is below threshold
