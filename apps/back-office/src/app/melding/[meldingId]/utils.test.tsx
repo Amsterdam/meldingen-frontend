c01794b79f1326204490806c3ebb8e97b294fdb5
Vincent <vdegraaf.vincent@gmail.com>
2025-11-07 16:04:16 +0100

    Add test cases for attachment util function

--- apps/back-office/src/app/melding/[meldingId]/utils.test.tsx
+++ apps/back-office/src/app/melding/[meldingId]/utils.test.tsx
@@ -1,6 +1,12 @@
 import { http, HttpResponse } from 'msw'
 
-import { getAdditionalQuestionsData, getContactData, getLocationData, getMeldingData } from './utils'
+import {
+  getAdditionalQuestionsData,
+  getAttachmentsData,
+  getContactData,
+  getLocationData,
+  getMeldingData,
+} from './utils'
 import { getFullNLAddress } from '../../utils'
 import { melding } from 'apps/back-office/src/mocks/data'
 import { additionalQuestions } from 'apps/back-office/src/mocks/data'
@@ -170,17 +176,51 @@ describe('getMeldingData', () => {
   })
 })
 
-//  it('returns an attachment with error message when getAttachmentById returns an error', async () => {
-//     server.use(
-//       http.get(ENDPOINTS.GET_ATTACHMENT_BY_ID, () =>
-//         HttpResponse.json({ detail: 'Error message' }, { status: 500 }),
-//       ),
-//     )
+describe('getAttachmentsData', () => {
+  it('returns correct attachments data', async () => {
+    const result = await getAttachmentsData(mockMeldingId, (key: string) => key)
 
-//     const params = Promise.resolve({ meldingId: 123 })
-//     const result = await Page({ params })
+    expect(result).toMatchObject({
+      files: [
+        {
+          // TODO: Add blob to matching object
+          fileName: 'IMG_0815.jpg',
+        },
+      ],
+      key: 'attachments',
+      term: 'detail.attachments.title',
+    })
+  })
 
-//     const { getByText } = render(result)
+  it('returns an error message when getMeldingByMeldingIdAttachments returns an error', async () => {
+    server.use(
+      http.get(ENDPOINTS.GET_MELDING_BY_MELDING_ID_ATTACHMENTS, () =>
+        HttpResponse.json({ detail: 'Error message' }, { status: 500 }),
+      ),
+    )
 
-//     expect(getByText('Error message')).toBeInTheDocument()
-//   })
+    const result = await getAttachmentsData(mockMeldingId, (key: string) => key)
+
+    expect(result).toEqual({ error: 'Error message' })
+  })
+
+  it('returns an attachment with error message when getAttachmentById returns an error ', async () => {
+    server.use(
+      http.get(ENDPOINTS.GET_ATTACHMENT_BY_ID, () => HttpResponse.json({ detail: 'Error message' }, { status: 500 })),
+    )
+
+    const result = await getAttachmentsData(mockMeldingId, (key: string) => key)
+
+    expect(result).toMatchObject({
+      files: [
+        {
+          blob: null,
+          fileName: 'IMG_0815.jpg',
+          error: 'Error message',
+        },
+      ],
+      key: 'attachments',
+      term: 'detail.attachments.title',
+    })
+  })
+})
