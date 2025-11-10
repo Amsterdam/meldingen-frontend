cf40d7657614920fb75b6256351246bebc6f060c
alimpens <a.limpens@amsterdam.nl>
2025-11-10 18:25:21 +0100

    Add styling, add temp Loading component

--- apps/melding-form/src/app/(general)/samenvatting/_components/AttachmentImage.tsx
+++ apps/melding-form/src/app/(general)/samenvatting/_components/AttachmentImage.tsx
@@ -1,10 +1,14 @@
 import { Image, Paragraph } from '@amsterdam/design-system-react'
 import { useEffect, useState } from 'react'
 
+import styles from './AttachmentImage.module.css'
+
 type Props = {
   blob: Blob
   fileName: string
 }
+
+const Loading = () => <div className={styles.loading} />
 
 export const AttachmentImage = ({ blob, fileName }: Props) => {
   const [url, setUrl] = useState<string | null>(null)
@@ -18,11 +22,9 @@ export const AttachmentImage = ({ blob, fileName }: Props) => {
   }, [])
 
   return (
-    url && (
-      <>
-        <Image src={url} alt="" className="ams-mb-m" />
-        <Paragraph>{fileName}</Paragraph>
-      </>
-    )
+    <>
+      {url ? <Image src={url} alt="" className={styles.image} /> : <Loading />}
+      <Paragraph>{fileName}</Paragraph>
+    </>
   )
 }
