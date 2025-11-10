ef7d085cd11a31e00c9597d3cbe7232988b84416
alimpens <a.limpens@amsterdam.nl>
2025-11-10 18:43:25 +0100

    Fix tests and add new one

---
+++ apps/melding-form/src/app/(general)/samenvatting/_components/AttachmentImage.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+
+import { AttachmentImage } from './AttachmentImage'
+
+const createObjectURLMock = vi.fn().mockImplementation(() => 'test-url')
+
+global.URL.createObjectURL = createObjectURLMock
+
+const testBlob = new Blob(['test-blob'], { type: 'image/jpeg' }) as Blob
+
+describe('AttachmentImage', () => {
+  it('renders an image when a blob is provided', async () => {
+    render(<AttachmentImage blob={testBlob} fileName="IMG_0815.jpg" />)
+
+    expect(createObjectURLMock).toHaveBeenCalled()
+
+    const image = screen.getByRole('presentation')
+
+    expect(image).toHaveAttribute('src', 'test-url')
+  })
+})
