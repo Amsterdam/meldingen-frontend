e37f6d0d00b6f51c4217b967e3594fcad9ffe1e8
alimpens <a.limpens@amsterdam.nl>
2025-11-20 16:45:35 +0100

    Entra test in admin

--- apps/admin/src/app/Admin.tsx
+++ apps/admin/src/app/Admin.tsx
@@ -1,4 +1,6 @@
+import { LoginPage } from 'ra-auth-msal'
 import { Admin as ReactAdmin, Resource } from 'react-admin'
+import { BrowserRouter } from 'react-router-dom'
 
 import { AssetTypeCreate, AssetTypeEdit, AssetTypeList } from '../pages/asset-types'
 import { ClassificationCreate, ClassificationEdit, ClassificationList } from '../pages/classification'
@@ -8,27 +10,30 @@ import { CustomLayout } from './components'
 import { i18nProvider, useAuthProvider } from './providers'
 
 export const Admin = () => {
-  const { authProvider, dataProviderRef, keycloakClient } = useAuthProvider()
+  const { authProvider, dataProviderRef } = useAuthProvider() // keycloakClient
 
-  if (!keycloakClient) return <p>Loading...</p>
+  // if (!keycloakClient) return <p>Loading...</p>
 
   return (
-    <ReactAdmin
-      authProvider={authProvider.current}
-      dataProvider={dataProviderRef.current}
-      i18nProvider={i18nProvider}
-      layout={CustomLayout}
-    >
-      <Resource create={<FormCreate />} edit={<FormEdit />} list={<FormList />} name="form" />
-      <Resource
-        create={<ClassificationCreate />}
-        edit={<ClassificationEdit />}
-        list={<ClassificationList />}
-        name="classification"
-        recordRepresentation="name"
-      />
-      <Resource edit={<StaticFormEdit />} list={<StaticFormList />} name="static-form" recordRepresentation="name" />
-      <Resource create={<AssetTypeCreate />} edit={<AssetTypeEdit />} list={<AssetTypeList />} name="asset-type" />
-    </ReactAdmin>
+    <BrowserRouter>
+      <ReactAdmin
+        authProvider={authProvider.current}
+        dataProvider={dataProviderRef.current}
+        i18nProvider={i18nProvider}
+        layout={CustomLayout}
+        loginPage={LoginPage}
+      >
+        <Resource create={<FormCreate />} edit={<FormEdit />} list={<FormList />} name="form" />
+        <Resource
+          create={<ClassificationCreate />}
+          edit={<ClassificationEdit />}
+          list={<ClassificationList />}
+          name="classification"
+          recordRepresentation="name"
+        />
+        <Resource edit={<StaticFormEdit />} list={<StaticFormList />} name="static-form" recordRepresentation="name" />
+        <Resource create={<AssetTypeCreate />} edit={<AssetTypeEdit />} list={<AssetTypeList />} name="asset-type" />
+      </ReactAdmin>
+    </BrowserRouter>
   )
 }
--- apps/admin/src/app/providers/useAuthProvider.ts
+++ apps/admin/src/app/providers/useAuthProvider.ts
@@ -1,23 +1,38 @@
-import type { KeycloakConfig, KeycloakInitOptions } from 'keycloak-js'
+import type { KeycloakConfig } from 'keycloak-js' // KeycloakInitOptions
 import type { AuthProvider, DataProvider } from 'react-admin'
 
+import { PublicClientApplication } from '@azure/msal-browser'
 import Keycloak from 'keycloak-js'
-import { httpClient, keycloakAuthProvider } from 'ra-keycloak'
+import { msalAuthProvider } from 'ra-auth-msal'
+import { httpClient } from 'ra-keycloak' // keycloakAuthProvider
 import { useRef } from 'react'
 
 import { dataProvider } from './dataProvider'
 
-const initOptions: KeycloakInitOptions = {
-  checkLoginIframe: false,
-  onLoad: 'login-required',
-  responseMode: 'query',
-}
+// const initOptions: KeycloakInitOptions = {
+//   checkLoginIframe: false,
+//   onLoad: 'login-required',
+//   responseMode: 'query',
+// }
 
 const config: KeycloakConfig = {
   clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
   realm: import.meta.env.VITE_KEYCLOAK_REALM,
   url: import.meta.env.VITE_KEYCLOAK_BASE_URL,
 }
+
+const myMSALObj = new PublicClientApplication({
+  auth: {
+    authority: import.meta.env.ENTRA_MSAL_AUTHORITY,
+    clientId: import.meta.env.ENTRA_CLIENT_ID,
+    navigateToLoginRequestUrl: false,
+    redirectUri: `http://localhost:3001/auth-callback`,
+  },
+  cache: {
+    cacheLocation: 'sessionStorage',
+    storeAuthStateInCookie: false,
+  },
+})
 
 export const useAuthProvider = () => {
   const authProvider = useRef<AuthProvider | undefined>(undefined)
@@ -31,11 +46,18 @@ export const useAuthProvider = () => {
     })
   }
 
-  authProvider.current = keycloakAuthProvider(keycloakClient, {
-    initOptions,
-    loginRedirectUri: `/#/auth-callback`,
-    logoutRedirectUri: `/#/login`,
+  const test = msalAuthProvider({
+    // @ts-expect-error dunno
+    msalInstance: myMSALObj,
   })
+
+  // authProvider.current = keycloakAuthProvider(keycloakClient, {
+  //   initOptions,
+  //   loginRedirectUri: `/#/auth-callback`,
+  //   logoutRedirectUri: `/#/login`,
+  // })
+
+  authProvider.current = test
 
   dataProviderRef.current = dataProvider(import.meta.env.VITE_BACKEND_BASE_URL, httpClient(keycloakClient))
 
