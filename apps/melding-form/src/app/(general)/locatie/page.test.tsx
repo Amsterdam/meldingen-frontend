cbf35e8bb35b132db6987faa231b0b7515d842ad
Vincent <vdegraaf.vincent@gmail.com>
2025-12-09 09:16:13 +0100

    Add test case for /locatie page

--- apps/melding-form/src/app/(general)/locatie/page.test.tsx
+++ apps/melding-form/src/app/(general)/locatie/page.test.tsx
@@ -1,12 +1,16 @@
 import type { Mock } from 'vitest'
 
 import { render, screen } from '@testing-library/react'
+import { http, HttpResponse } from 'msw'
 import { cookies } from 'next/headers'
 
 import { Location } from './Location'
 import Page from './page'
 import { COOKIES } from 'apps/melding-form/src/constants'
-import { mockCookies } from 'apps/melding-form/src/mocks/utils'
+import { containerAssets } from 'apps/melding-form/src/mocks/data'
+import { ENDPOINTS } from 'apps/melding-form/src/mocks/endpoints'
+import { server } from 'apps/melding-form/src/mocks/node'
+import { mockCookies, mockIdAndTokenCookies } from 'apps/melding-form/src/mocks/utils'
 
 vi.mock('next/headers', () => ({ cookies: vi.fn() }))
 
@@ -15,9 +19,20 @@ vi.mock('./Location', () => ({
 }))
 
 describe('Page', () => {
+  beforeEach(() => {
+    mockIdAndTokenCookies()
+  })
   it('renders Location component with default props when cookies are not set', async () => {
     ;(cookies as Mock).mockReturnValue({
-      get: () => undefined,
+      get: (name: string) => {
+        if (name === COOKIES.ID) {
+          return { value: '123' }
+        }
+        if (name === COOKIES.TOKEN) {
+          return { value: 'test-token' }
+        }
+        return undefined
+      },
     })
 
     const PageComponent = await Page()
@@ -25,13 +40,15 @@ describe('Page', () => {
     render(PageComponent)
 
     expect(screen.getByText('Location Component')).toBeInTheDocument()
-    expect(Location).toHaveBeenCalledWith({ address: undefined, prevPage: '/' }, undefined)
+    expect(Location).toHaveBeenCalledWith(expect.objectContaining({ address: undefined, prevPage: '/' }), undefined)
   })
 
   it('renders Location component with props from cookies', async () => {
     mockCookies({
       [COOKIES.ADDRESS]: 'Oudezijds Voorburgwal 300, 1012GL Amsterdam',
+      [COOKIES.ID]: '123',
       [COOKIES.LAST_PANEL_PATH]: '/previous',
+      [COOKIES.TOKEN]: 'test-token',
     })
 
     const PageComponent = await Page()
@@ -40,7 +57,88 @@ describe('Page', () => {
 
     expect(screen.getByText('Location Component')).toBeInTheDocument()
     expect(Location).toHaveBeenCalledWith(
-      { address: 'Oudezijds Voorburgwal 300, 1012GL Amsterdam', prevPage: '/previous' },
+      expect.objectContaining({ address: 'Oudezijds Voorburgwal 300, 1012GL Amsterdam', prevPage: '/previous' }),
+      undefined,
+    )
+  })
+
+  it('fetches and passes saved assets to Location component', async () => {
+    let callCount = 0
+    server.use(
+      http.get(ENDPOINTS.GET_WFS_BY_NAME, () => {
+        callCount += 1
+
+        if (callCount === 1) {
+          return HttpResponse.json({
+            features: [containerAssets[0]],
+          })
+        } else {
+          return HttpResponse.json({
+            features: [containerAssets[1]],
+          })
+        }
+      }),
+    )
+
+    const PageComponent = await Page()
+
+    render(PageComponent)
+
+    expect(Location).toHaveBeenCalledWith(expect.objectContaining({ savedAssets: containerAssets }), undefined)
+  })
+
+  it('returns empty savedAssets when no assets are found', async () => {
+    server.use(http.get(ENDPOINTS.GET_WFS_BY_NAME, () => HttpResponse.json({ features: [] })))
+
+    const PageComponent = await Page()
+    render(PageComponent)
+
+    expect(Location).toHaveBeenCalledWith(
+      expect.objectContaining({
+        savedAssets: [],
+      }),
+      undefined,
+    )
+  })
+
+  it('logs an error when fetches assetIds from melding fails', async () => {
+    server.use(
+      http.get(ENDPOINTS.GET_MELDING_BY_MELDING_ID_ASSETS_MELDER, () =>
+        HttpResponse.json('Test error', { status: 500 }),
+      ),
+    )
+    const consoleSpy = vi.spyOn(console, 'error').mockImplementation(() => {})
+
+    const PageComponent = await Page()
+    render(PageComponent)
+
+    expect(consoleSpy).toHaveBeenCalledWith('Test error')
+
+    consoleSpy.mockRestore()
+
+    expect(Location).toHaveBeenCalledWith(
+      expect.objectContaining({
+        savedAssets: [],
+      }),
+      undefined,
+    )
+  })
+
+  it('logs an error when the wfs endpoint fails', async () => {
+    server.use(http.get(ENDPOINTS.GET_WFS_BY_NAME, () => HttpResponse.json('Test error', { status: 500 })))
+    const consoleSpy = vi.spyOn(console, 'error').mockImplementation(() => {})
+
+    const PageComponent = await Page()
+    render(PageComponent)
+
+    expect(consoleSpy).toHaveBeenCalledWith('Test error')
+
+    consoleSpy.mockRestore()
+
+    expect(Location).toHaveBeenCalledWith(
+      expect.objectContaining({
+        savedAssets: [],
+      }),
       undefined,
     )
   })
