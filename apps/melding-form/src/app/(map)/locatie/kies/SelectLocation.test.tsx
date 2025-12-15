38574311244970fb772bdf1643e31d3f4ee3844e
Vincent <vdegraaf.vincent@gmail.com>
2025-12-15 14:44:32 +0100

    Revert "Make sure map components are rendered in the test"
    This reverts commit 2f0ade92b8ff94a94a4a8f4c45bd8fe393dd6c1f.

--- apps/melding-form/src/app/(map)/locatie/kies/SelectLocation.test.tsx
+++ apps/melding-form/src/app/(map)/locatie/kies/SelectLocation.test.tsx
@@ -14,7 +14,7 @@ vi.mock('./_components/AssetList/AssetList', () => ({
 
 vi.mock('@meldingen/map', () => ({
   Controls: vi.fn(),
-  Map: vi.fn((props) => <div data-testid="map-mock">{props.children}</div>),
+  Map: vi.fn(),
   MarkerSelectLayer: vi.fn(),
   PointSelectLayer: vi.fn(),
 }))
