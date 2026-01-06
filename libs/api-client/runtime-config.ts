94686b7ece17f36747a0ed6d1eb7247646570507
alimpens <a.limpens@amsterdam.nl>
2026-01-06 15:46:37 +0100

    Re-export client from index file, and check for node env in runtime function

--- libs/api-client/runtime-config.ts
+++ libs/api-client/runtime-config.ts
@@ -1,7 +1,12 @@
 import type { CreateClientConfig } from './src/client.gen';
 
 export const createClientConfig: CreateClientConfig = (config) => {
+  const isNodeEnv = typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
   const isServer = typeof window === 'undefined';
+
+  // If this function does not run in a Node.js environment
+  // (like in our Admin Vite app), return the config as is.
+  if (!isNodeEnv) return { ...config }
 
   return {
     ...config,
