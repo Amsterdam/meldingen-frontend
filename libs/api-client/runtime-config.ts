15c0845a0aefb764ce468ac3d366829a2177a42e
Joey <joey.hendriks@amsterdam.nl>
2026-02-18 15:45:13 +0100

    see if we even reach this

--- libs/api-client/runtime-config.ts
+++ libs/api-client/runtime-config.ts
@@ -1,17 +1,12 @@
 import type { CreateClientConfig } from './src/client.gen';
 
 export const createClientConfig: CreateClientConfig = (config) => {
-  const isNodeEnv = typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
   const isServer = typeof window === 'undefined';
-
-  // If this function does not run in a Node.js environment
-  // (like in our Admin Vite app), return the config as is.
-  if (!isNodeEnv) return { ...config };
 
   return {
     ...config,
     baseUrl: isServer
       ? process.env.NEXT_INTERNAL_BACKEND_BASE_URL
-      : process.env.NEXT_PUBLIC_BACKEND_BASE_URL || "http://localhost:8000" // Fallback for automated tests
+      : process.env.NEXT_PUBLIC_BACKEND_BASE_URL || config?.baseUrl // Fallback for automated tests
   };
 };
