7c967be77d546ebb2044c975e4167fd23677a7f1
alimpens <a.limpens@amsterdam.nl>
2026-02-19 16:35:15 +0100

    Throw error if necessary env var is not set

--- libs/api-client/runtime-config.ts
+++ libs/api-client/runtime-config.ts
@@ -3,7 +3,13 @@ import type { CreateClientConfig } from './src/client.gen';
 export const createClientConfig: CreateClientConfig = (config) => {
   const isServer = typeof window === 'undefined';
 
-  // TODO: We should throw an error if the required environment variables are not set
+  if (!process.env.NEXT_PUBLIC_BACKEND_BASE_URL) {
+    throw new Error('NEXT_PUBLIC_BACKEND_BASE_URL environment variable must be set');
+  }
+
+  if (isServer && !process.env.NEXT_INTERNAL_BACKEND_BASE_URL) {
+    throw new Error('NEXT_INTERNAL_BACKEND_BASE_URL environment variable must be set');
+  }
 
   return {
     ...config,
