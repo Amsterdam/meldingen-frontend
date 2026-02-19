19c568609caf955d01052077f26c5bcc29de98ef
alimpens <a.limpens@amsterdam.nl>
2026-02-19 16:21:06 +0100

    Set correct env vars for tests

--- libs/api-client/runtime-config.ts
+++ libs/api-client/runtime-config.ts
@@ -3,10 +3,12 @@ import type { CreateClientConfig } from './src/client.gen';
 export const createClientConfig: CreateClientConfig = (config) => {
   const isServer = typeof window === 'undefined';
 
+  // TODO: We should throw an error if the required environment variables are not set
+
   return {
     ...config,
     baseUrl: isServer
       ? process.env.NEXT_INTERNAL_BACKEND_BASE_URL
-      : process.env.NEXT_PUBLIC_BACKEND_BASE_URL || config?.baseUrl // Fallback for automated tests
+      : process.env.NEXT_PUBLIC_BACKEND_BASE_URL
   };
 };
