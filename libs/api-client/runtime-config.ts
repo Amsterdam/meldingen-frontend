1fa544a2752e3758c24e678ef3b11baa228e8a47
alimpens <a.limpens@amsterdam.nl>
2026-02-19 17:17:59 +0100

    Check for Vite app in runtime config

--- libs/api-client/runtime-config.ts
+++ libs/api-client/runtime-config.ts
@@ -1,20 +1,24 @@
-import type { CreateClientConfig } from './src/client.gen';
+import type { CreateClientConfig } from './src/client.gen'
 
 export const createClientConfig: CreateClientConfig = (config) => {
-  const isServer = typeof window === 'undefined';
+  // @ts-expect-error
+  const isVite = Boolean(import.meta.env)
+  const isServer = typeof window === 'undefined'
+
+  if (isVite) {
+    return { ...config }
+  }
 
   if (!process.env.NEXT_PUBLIC_BACKEND_BASE_URL) {
-    throw new Error('NEXT_PUBLIC_BACKEND_BASE_URL environment variable must be set');
+    throw new Error('NEXT_PUBLIC_BACKEND_BASE_URL environment variable must be set')
   }
 
   if (isServer && !process.env.NEXT_INTERNAL_BACKEND_BASE_URL) {
-    throw new Error('NEXT_INTERNAL_BACKEND_BASE_URL environment variable must be set');
+    throw new Error('NEXT_INTERNAL_BACKEND_BASE_URL environment variable must be set')
   }
 
   return {
     ...config,
-    baseUrl: isServer
-      ? process.env.NEXT_INTERNAL_BACKEND_BASE_URL
-      : process.env.NEXT_PUBLIC_BACKEND_BASE_URL
-  };
-};
+    baseUrl: isServer ? process.env.NEXT_INTERNAL_BACKEND_BASE_URL : process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
+  }
+}
