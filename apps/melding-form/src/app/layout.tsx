75ccc903ae973bb30a3f88a66902bac170a2c26a
joeyamst <joey.hendriks@amsterdam.nl>
2026-02-23 12:56:25 +0100

    Move API client config from library to apps (#1139)
    Co-authored-by: Aram <37216945+alimpens@users.noreply.github.com>
    Co-authored-by: alimpens <a.limpens@amsterdam.nl>

--- apps/melding-form/src/app/layout.tsx
+++ apps/melding-form/src/app/layout.tsx
@@ -3,7 +3,10 @@ import type { ReactNode } from 'react'
 import { NextIntlClientProvider } from 'next-intl'
 import { getLocale, getTranslations } from 'next-intl/server'
 
+import { client } from '@meldingen/api-client'
+
 import './global.css'
+import { ApiClientInitializer } from './ApiClientInitializer'
 
 export const generateMetadata = async () => {
   const t = await getTranslations('metadata')
@@ -13,11 +16,18 @@ export const generateMetadata = async () => {
   }
 }
 
+// Configure the API client for server requests.
+// Client requests are configured in ApiClientInitializer.
+client.setConfig({
+  baseUrl: process.env.NEXT_INTERNAL_BACKEND_BASE_URL,
+})
+
 const RootLayout = async ({ children }: { children: ReactNode }) => {
   const locale = await getLocale()
 
   return (
     <html dir="ltr" lang={locale}>
+      <ApiClientInitializer />
       <NextIntlClientProvider>
         <body>{children}</body>
       </NextIntlClientProvider>
