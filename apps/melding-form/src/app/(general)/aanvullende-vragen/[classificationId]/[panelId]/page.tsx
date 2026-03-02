82a505f96bc659eef0e66a8dce3ea30e21fdbf96
Vincent <vdegraaf.vincent@gmail.com>
2026-03-02 16:58:18 +0100

    Set date options in melding-form

--- apps/melding-form/src/app/(general)/aanvullende-vragen/[classificationId]/[panelId]/page.tsx
+++ apps/melding-form/src/app/(general)/aanvullende-vragen/[classificationId]/[panelId]/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation'
 
 import type {
   FormCheckboxComponentOutput,
+  FormDateComponentOutput,
   FormOutput,
   FormPanelComponentOutput,
   FormRadioComponentOutput,
@@ -38,8 +39,44 @@ const stripPositionKey = <T extends { position?: unknown }>(obj: T): Omit<T, 'po
   return rest
 }
 
+const getDateOptions = (dayRange: number) => {
+  const today = new Date()
+
+  const dateEntries = Array.from({ length: dayRange }, (_, i) => {
+    const date = new Date(today)
+    date.setDate(today.getDate() - i)
+
+    const converted_date = date.toISOString().split('T')[0]
+
+    const dayName = date.toLocaleString('nl-NL', { weekday: 'long' })
+    const day = date.getDate()
+    const month = date.toLocaleString('nl-NL', { month: 'long' })
+
+    const capitalizedDayName = dayName.charAt(0).toUpperCase() + dayName.slice(1)
+
+    const value = i === 0 ? 'day' : `day - ${i}`
+
+    let label: string
+    if (i === 0) {
+      label = 'Vandaag'
+    } else if (i === 1) {
+      label = `Gisteren ${day} ${month}`
+    } else {
+      label = `${capitalizedDayName} ${day} ${month}`
+    }
+
+    return { converted_date, label, value }
+  })
+
+  return [...dateEntries, { converted_date: null, label: 'Weet ik niet', value: 'Unknown' }]
+}
+
+type FormDateComponentOutputWithValues = FormDateComponentOutput & { values: ReturnType<typeof getDateOptions> }
+
 const getValuesAndLabels = (component: FormOutputWithoutPanelComponents) => {
   switch (component.type) {
+    case 'date':
+      return (component as FormDateComponentOutputWithValues).values
     case 'radio':
       return (component as FormRadioComponentOutput).values.map(stripPositionKey)
     case 'select':
@@ -53,11 +90,23 @@ const getValuesAndLabels = (component: FormOutputWithoutPanelComponents) => {
 
 type FormOutputWithoutPanelComponents = Exclude<FormOutput['components'][number], FormPanelComponentOutput>
 
+const setComponentOptions = (components: FormOutputWithoutPanelComponents[]) =>
+  components.map((component) => {
+    switch (component.type) {
+      case 'date':
+        return { ...component, values: getDateOptions((component as FormDateComponentOutput).dayRange) }
+      default:
+        return component
+    }
+  })
+
 const getFormComponents = (
   components: FormOutputWithoutPanelComponents[],
   answers?: GetMeldingByMeldingIdAnswersMelderResponses['200'],
-) =>
-  components.map((component) => {
+) => {
+  const componentWithCustomOptions = setComponentOptions(components)
+
+  return componentWithCustomOptions.map((component) => {
     const answer = answers?.find((answer) => answer.question.id === component.question)
 
     // Prefill if answer exists, otherwise return component without defaultValue(s)
@@ -78,6 +127,7 @@ const getFormComponents = (
         return component
     }
   })
+}
 
 type Params = Promise<{
   classificationId: number
--- libs/form-renderer/src/components/DateInput/DateInput.tsx
+++ libs/form-renderer/src/components/DateInput/DateInput.tsx
@@ -5,40 +5,13 @@ import { FieldSet } from '@meldingen/ui'
 
 import { getAriaDescribedBy } from '../../utils'
 
-const getDateOptions = (dayRange: number) => {
-  const today = new Date()
-
-  const dateEntries = Array.from({ length: dayRange }, (_, i) => {
-    const date = new Date(today)
-    date.setDate(today.getDate() - i)
-
-    const converted_date = date.toISOString().split('T')[0]
-
-    const dayName = date.toLocaleString('nl-NL', { weekday: 'long' })
-    const day = date.getDate()
-    const month = date.toLocaleString('nl-NL', { month: 'long' })
-
-    const capitalizedDayName = dayName.charAt(0).toUpperCase() + dayName.slice(1)
-
-    const value = `day - ${i + 1}`
-
-    let label: string
-    if (i === 0) {
-      label = 'Vandaag'
-    } else if (i === 1) {
-      label = `Gisteren ${day} ${month}`
-    } else {
-      label = `${capitalizedDayName} ${day} ${month}`
-    }
-
-    return { converted_date, label, value }
-  })
-
-  return [...dateEntries, { converted_date: null, label: 'Weet ik niet', value: 'Unknown' }]
+type DateOption = {
+  converted_date: string | null
+  label: string
+  value: string
 }
 
 export type Props = {
-  dayRange: number
   defaultValue?: string
   description?: string
   errorMessage?: string
@@ -46,10 +19,10 @@ export type Props = {
   id: string
   label: string
   validate?: { required: boolean } | null
+  values: DateOption[]
 }
 
 export const DateInput = ({
-  dayRange,
   defaultValue,
   description,
   errorMessage,
@@ -57,40 +30,37 @@ export const DateInput = ({
   id,
   label,
   validate,
-}: Props) => {
-  const options = getDateOptions(dayRange)
-
-  return (
-    <FieldSet
-      aria-describedby={getAriaDescribedBy(id, description, errorMessage)}
-      aria-required={validate?.required ? 'true' : undefined}
-      hasHeading={hasHeading}
-      invalid={Boolean(errorMessage)}
-      legend={label}
-      optional={!validate?.required}
-      role="radiogroup"
-    >
-      {description && (
-        <MarkdownToHtml id={`${id}-description`} type="description">
-          {description}
-        </MarkdownToHtml>
-      )}
-      {errorMessage && <ErrorMessage id={`${id}-error`}>{errorMessage}</ErrorMessage>}
-      <Column gap="x-small">
-        {options.map(({ label: radioLabel, value }, index) => (
-          <ADSRadio
-            aria-required={validate?.required ? 'true' : undefined}
-            defaultChecked={value === defaultValue}
-            id={index === 0 ? id : undefined} // Use component id for first radio, to be able to link to it in the Invalid Form Alert
-            invalid={Boolean(errorMessage)}
-            key={value}
-            name={id}
-            value={value}
-          >
-            {radioLabel}
-          </ADSRadio>
-        ))}
-      </Column>
-    </FieldSet>
-  )
-}
+  values,
+}: Props) => (
+  <FieldSet
+    aria-describedby={getAriaDescribedBy(id, description, errorMessage)}
+    aria-required={validate?.required ? 'true' : undefined}
+    hasHeading={hasHeading}
+    invalid={Boolean(errorMessage)}
+    legend={label}
+    optional={!validate?.required}
+    role="radiogroup"
+  >
+    {description && (
+      <MarkdownToHtml id={`${id}-description`} type="description">
+        {description}
+      </MarkdownToHtml>
+    )}
+    {errorMessage && <ErrorMessage id={`${id}-error`}>{errorMessage}</ErrorMessage>}
+    <Column gap="x-small">
+      {values.map(({ label: radioLabel, value }, index) => (
+        <ADSRadio
+          aria-required={validate?.required ? 'true' : undefined}
+          defaultChecked={value === defaultValue}
+          id={index === 0 ? id : undefined} // Use component id for first radio, to be able to link to it in the Invalid Form Alert
+          invalid={Boolean(errorMessage)}
+          key={value}
+          name={id}
+          value={value}
+        >
+          {radioLabel}
+        </ADSRadio>
+      ))}
+    </Column>
+  </FieldSet>
+)
