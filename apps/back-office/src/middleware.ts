3b56aade7dae3888f35b554497b9faff2c20f40f
Joey <joey.hendriks@amsterdam.nl>
2026-03-04 15:40:48 +0100

    use proxy instead of middleware

--- apps/back-office/src/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export const middleware = () => NextResponse.next()
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|robots.txt|.*\\..*).*)'],
-}
