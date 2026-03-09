62e169f2cd12d16f9c34eea6843d709c5e499e70
Vincent <vdegraaf.vincent@gmail.com>
2026-03-09 16:44:51 +0100

    Add data to mobile view

--- apps/back-office/src/app/OverviewMobile.tsx
+++ apps/back-office/src/app/OverviewMobile.tsx
@@ -1,10 +1,46 @@
-import type { MeldingWithAddress } from './overviewFields'
+import { useTranslations } from 'next-intl'
+import NextLink from 'next/link'
+import { Fragment } from 'react'
+
+import { DescriptionList, Link } from '@meldingen/ui'
+
+import type { MeldingWithAddress, OverviewField } from './overviewFields'
+
+import { formatValue, getMeldingDetailHref, OVERVIEW_FIELDS } from './overviewFields'
 
 type Props = {
   meldingen: MeldingWithAddress[]
 }
 
-export const OverviewMobile = (_props: Props) => {
-  // Intentionally empty for now; this will become the card-based overview.
-  return null
+const renderValue = (melding: MeldingWithAddress, field: OverviewField, t: (key: string) => string) => {
+  if (field.key === 'public_id') {
+    return (
+      <NextLink href={getMeldingDetailHref(melding)} legacyBehavior passHref>
+        <Link>{melding.public_id}</Link>
+      </NextLink>
+    )
+  }
+
+  return formatValue(melding, field.key, t) ?? ''
+}
+
+export const OverviewMobile = ({ meldingen }: Props) => {
+  const t = useTranslations()
+
+  return (
+    <div>
+      {meldingen.map((melding) => (
+        <div key={melding.public_id}>
+          <DescriptionList>
+            {OVERVIEW_FIELDS.map((field) => (
+              <Fragment key={field.key}>
+                <DescriptionList.Term>{t(`overview.${field.labelKey}`)}</DescriptionList.Term>
+                <DescriptionList.Description>{renderValue(melding, field, t)}</DescriptionList.Description>
+              </Fragment>
+            ))}
+          </DescriptionList>
+        </div>
+      ))}
+    </div>
+  )
 }
