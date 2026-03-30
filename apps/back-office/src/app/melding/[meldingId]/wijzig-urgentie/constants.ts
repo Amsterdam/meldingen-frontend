ace60e38cfe08780902baeb3f0f49856968f1602
Vincent <vdegraaf.vincent@gmail.com>
2026-03-30 16:03:43 +0200

    Add extra typings urgency values

--- apps/back-office/src/app/melding/[meldingId]/wijzig-urgentie/constants.ts
+++ apps/back-office/src/app/melding/[meldingId]/wijzig-urgentie/constants.ts
@@ -1,3 +1,8 @@
 import type { MeldingOutput } from '@meldingen/api-client'
 
-export const URGENCY_VALUES: MeldingOutput['urgency'][] = [-1, 0, 1] as const
+type Urgency = MeldingOutput['urgency']
+type AllUrgencyValues<T extends readonly Urgency[]> = Urgency extends T[number] ? T : never
+
+const asUrgencyValues = <T extends readonly Urgency[]>(arr: AllUrgencyValues<T> & T): T => arr
+
+export const URGENCY_VALUES = asUrgencyValues([-1, 0, 1] as const)
