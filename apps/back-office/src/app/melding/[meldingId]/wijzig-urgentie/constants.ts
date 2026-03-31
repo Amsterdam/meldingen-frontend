654d395cca096b6a4f08020e7b3a34d77487ca2f
Vincent de Graaf <vdegraaf.vincent@gmail.com>
2026-03-31 20:50:49 +0200

    Update apps/back-office/src/app/melding/[meldingId]/wijzig-urgentie/constants.ts
    Co-authored-by: Aram <37216945+alimpens@users.noreply.github.com>

--- apps/back-office/src/app/melding/[meldingId]/wijzig-urgentie/constants.ts
+++ apps/back-office/src/app/melding/[meldingId]/wijzig-urgentie/constants.ts
@@ -1,8 +1,3 @@
 import type { MeldingOutput } from '@meldingen/api-client'
 
-type Urgency = MeldingOutput['urgency']
-type AllUrgencyValues<T extends readonly Urgency[]> = Urgency extends T[number] ? T : never
-
-const asUrgencyValues = <T extends readonly Urgency[]>(arr: AllUrgencyValues<T> & T): T => arr
-
-export const URGENCY_VALUES = asUrgencyValues([-1, 0, 1] as const)
+export const URGENCY_VALUES: readonly MeldingOutput['urgency'][] = [-1, 0, 1]
