9ccd37b853db315630c9d8289575d7a3b48ed394
alimpens <a.limpens@amsterdam.nl>
2026-04-20 15:41:15 +0200

    Add 'instructions' field to classifications in Admin

--- apps/admin/src/app/providers/i18nProvider.ts
+++ apps/admin/src/app/providers/i18nProvider.ts
@@ -232,6 +232,7 @@ const nl: TranslationMessages = {
     classification: {
       fields: {
         form: 'Vragenlijst',
+        instructions: 'Beschrijving',
         name: 'Naam',
       },
       name: 'Categorie |||| Categorieën',
