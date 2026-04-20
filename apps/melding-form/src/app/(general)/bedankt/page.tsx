f9a788f637170adfd4f5f8915ea41aa0d8f4b1d8
alimpens <a.limpens@amsterdam.nl>
2026-04-20 17:47:33 +0200

    Link back to Back Office from Melding Form

--- apps/melding-form/src/app/(general)/bedankt/page.tsx
+++ apps/melding-form/src/app/(general)/bedankt/page.tsx
@@ -22,11 +22,15 @@ export default async ({ searchParams }: { searchParams: Promise<{ [key: string]:
 
   const createdAt = (await searchParams).created_at
   const publicId = (await searchParams).public_id
+  const source = (await searchParams).source
 
   const date = createdAt ? new Date(createdAt).toLocaleDateString('nl-NL') : undefined
   const time = createdAt ? new Date(createdAt).toLocaleTimeString('nl-NL', { timeStyle: 'short' }) : undefined
 
   const description = t('description', { date, publicId, time })
+
+  const backOfficeBaseUrl = process.env.NEXT_PUBLIC_BACK_OFFICE_BASE_URL
+  const returnLink = source === 'back-office' ? `${backOfficeBaseUrl}/melden` : `/#${TOP_ANCHOR_ID}`
 
   return (
     <main>
@@ -34,7 +38,7 @@ export default async ({ searchParams }: { searchParams: Promise<{ [key: string]:
         {t('title')}
       </Heading>
       <MarkdownToHtml className="ams-mb-s">{description}</MarkdownToHtml>
-      <NextLink href={`/#${TOP_ANCHOR_ID}`} legacyBehavior passHref>
+      <NextLink href={returnLink} legacyBehavior passHref>
         <StandaloneLink href="dummy-href">{t('link')}</StandaloneLink>
       </NextLink>
     </main>
