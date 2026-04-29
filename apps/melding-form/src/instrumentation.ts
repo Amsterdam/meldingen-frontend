f139e652e7b2d9f475d72ca7ab76bf725779da5e
Aram <37216945+alimpens@users.noreply.github.com>
2026-04-29 10:14:46 +0200

    Revert "Set API client base url in instrumentation.ts (#1266)"
    This reverts commit ae5bcbbe666a16bf398d4d344574fb533918b868.

--- apps/melding-form/src/instrumentation.ts
+++
@@ -1,7 +0,0 @@
-import { client } from '@meldingen/api-client'
-
-export const register = () => {
-  // Configure the API client for server requests.
-  // Client requests are configured in ApiClientInitializer.
-  client.setConfig({ baseUrl: process.env.NEXT_INTERNAL_BACKEND_BASE_URL })
-}
