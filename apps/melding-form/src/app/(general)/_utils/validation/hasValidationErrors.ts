30a2e6d7c50079678e6d873f6141749268d6c903
alimpens <a.limpens@amsterdam.nl>
2026-05-01 18:12:40 +0200

    Avoid parameter shadowing

--- apps/melding-form/src/app/(general)/_utils/validation/hasValidationErrors.ts
+++ apps/melding-form/src/app/(general)/_utils/validation/hasValidationErrors.ts
@@ -1,4 +1,6 @@
 import { isApiErrorArray } from '~/handleApiError'
 
 export const hasValidationErrors = (response: Response, error: unknown) =>
-  response.status === 422 && isApiErrorArray(error) && error?.detail?.some((error) => error.type === 'value_error')
+  response.status === 422 &&
+  isApiErrorArray(error) &&
+  error?.detail?.some((detailError) => detailError.type === 'value_error')
