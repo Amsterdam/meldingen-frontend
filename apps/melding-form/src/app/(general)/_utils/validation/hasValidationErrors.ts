d21b69a1f791bafbefb23487f85fb2b651ff375b
alimpens <a.limpens@amsterdam.nl>
2026-05-18 11:27:24 +0200

    Handle undefined response

--- apps/melding-form/src/app/(general)/_utils/validation/hasValidationErrors.ts
+++ apps/melding-form/src/app/(general)/_utils/validation/hasValidationErrors.ts
@@ -1,6 +1,6 @@
 import { isApiErrorArray } from '~/handleApiError'
 
-export const hasValidationErrors = (response: Response, error: unknown) =>
-  response.status === 422 &&
+export const hasValidationErrors = (response?: Response, error?: unknown) =>
+  response?.status === 422 &&
   isApiErrorArray(error) &&
   error?.detail?.some((detailError) => detailError.type === 'value_error')
