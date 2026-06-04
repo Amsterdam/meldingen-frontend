bd4bdb1eb608be90d6fdaec1acdea0f9411d27e6
alimpens <a.limpens@amsterdam.nl>
2026-06-04 11:47:25 +0200

    Move Back Office Overview page utils to separate files as well

---
+++ apps/back-office/src/app/constants.ts
@@ -0,0 +1,9 @@
+export const OVERVIEW_FIELDS = [
+  { key: 'public_id', labelKey: 'column-header.public_id' },
+  { key: 'created_at', labelKey: 'column-header.created_at' },
+  { key: 'classification', labelKey: 'column-header.classification' },
+  { key: 'state', labelKey: 'column-header.state' },
+  { key: 'urgency', labelKey: 'column-header.urgency' },
+  { key: 'address', labelKey: 'column-header.address' },
+  { key: 'postal_code', labelKey: 'column-header.postal_code' },
+] as const
