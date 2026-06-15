1606f3d31278abceafeba2e483db5e4f519c90a7
Aram <a.limpens@amsterdam.nl>
2026-06-15 13:35:29 +0200

    Refactor

--- apps/melding-form/src/app/(map)/locatie/kies/page.tsx
+++ apps/melding-form/src/app/(map)/locatie/kies/page.tsx
@@ -1,6 +1,8 @@
 import { getTranslations } from 'next-intl/server'
 import { cookies } from 'next/headers'
 
+import type { AssetOutput, MeldingOutput } from '@meldingen/api-client'
+
 import {
   deleteMeldingByMeldingIdAssetByAssetId,
   getAssetTypeByAssetTypeIdWfs,
@@ -20,47 +22,29 @@ export const generateMetadata = async () => {
   }
 }
 
+const MAX_ASSETS_FALLBACK = 3
+
+const getAssetTypeConfig = (data?: MeldingOutput) => {
+  const args = data?.classification?.asset_type?.arguments
+
+  return {
+    filter: args?.filter as string | undefined,
+    iconEntry: args?.icon_entry as string | undefined,
+    iconFolder: args?.icon_folder as string | undefined,
+    label: args?.label as string | undefined,
+    maxAssets: data?.classification?.asset_type?.max_assets ?? MAX_ASSETS_FALLBACK,
+    srsName: args?.srs_name as string | undefined,
+    typeNames: args?.type_names as string | undefined,
+  }
+}
+
 const getFilter = (id: string) => `
   <Filter>
     <ResourceId rid="${id}" />
   </Filter>
 `
 
-const MAX_ASSETS_FALLBACK = 3
-
-const getAssetsFromMelding = async (meldingId: string, token: string, assetTypeId: number, typeNames: string) => {
-  // Get existing assets for this melding
-  const { data: assetIds, error } = await getMeldingByMeldingIdAssetsMelder({
-    path: { melding_id: parseInt(meldingId, 10) },
-    query: { token },
-  })
-
-  if (error) {
-    // TODO: Log the error to an error reporting service
-    // eslint-disable-next-line no-console
-    console.error(error)
-    return []
-  }
-
-  // Delete all assets to avoid conflicts with previously selected assets
-  await Promise.all(
-    assetIds.map(async (asset) => {
-      const { error } = await deleteMeldingByMeldingIdAssetByAssetId({
-        path: {
-          asset_id: asset.id,
-          melding_id: parseInt(meldingId, 10),
-        },
-        query: { token },
-      })
-
-      if (error) {
-        // TODO: Log the error to an error reporting service
-        // eslint-disable-next-line no-console
-        console.error(error)
-      }
-    }),
-  )
-
+const fetchAssetFeatures = async (assetTypeId: number, typeNames: string, assetIds: AssetOutput[]) => {
   const assets = await Promise.all(
     assetIds.map(async (asset) => {
       const filter = getFilter(asset.external_id)
@@ -84,16 +68,54 @@ const getAssetsFromMelding = async (meldingId: string, token: string, assetTypeI
   return assets.filter((asset) => asset !== null)
 }
 
+const deleteExistingAssets = async (meldingId: number, token: string, assetIds: AssetOutput[]) => {
+  await Promise.all(
+    assetIds.map(async (asset) => {
+      const { error } = await deleteMeldingByMeldingIdAssetByAssetId({
+        path: {
+          asset_id: asset.id,
+          melding_id: meldingId,
+        },
+        query: { token },
+      })
+
+      if (error) {
+        // TODO: Log the error to an error reporting service
+        // eslint-disable-next-line no-console
+        console.error(error)
+      }
+    }),
+  )
+}
+
+const getAssetsFromMelding = async (meldingId: number, token: string, assetTypeId: number, typeNames: string) => {
+  // Get existing assets for this melding
+  const { data: assetIds, error } = await getMeldingByMeldingIdAssetsMelder({
+    path: { melding_id: meldingId },
+    query: { token },
+  })
+
+  if (error) {
+    // TODO: Log the error to an error reporting service
+    // eslint-disable-next-line no-console
+    console.error(error)
+    return []
+  }
+
+  // Delete all assets to avoid conflicts with previously selected assets
+  await deleteExistingAssets(meldingId, token, assetIds)
+
+  return await fetchAssetFeatures(assetTypeId, typeNames, assetIds)
+}
+
 export default async () => {
   const cookieStore = await cookies()
   // We check for the existence of these cookies in our proxy, so non-null assertion is safe here.
-  const meldingId = cookieStore.get(COOKIES.ID)!.value
+  const meldingId = parseInt(cookieStore.get(COOKIES.ID)!.value, 10)
   const token = cookieStore.get(COOKIES.TOKEN)!.value
 
   const { data, error } = await getMeldingByMeldingIdMelder({
-    path: {
-      melding_id: parseInt(meldingId, 10),
-    },
+    path: { melding_id: meldingId },
     query: { token },
   })
 
@@ -104,7 +126,8 @@ export default async () => {
   }
 
   const assetTypeId = data?.classification?.asset_type?.id
-  const typeNames = data?.classification?.asset_type?.arguments?.type_names as string | undefined
+  const assetTypeConfig = getAssetTypeConfig(data)
+  const { typeNames } = assetTypeConfig
 
   const selectedAssets =
     assetTypeId && typeNames ? await getAssetsFromMelding(meldingId, token, assetTypeId, typeNames) : []
@@ -114,24 +137,22 @@ export default async () => {
     lng: data.geo_location.geometry.coordinates[1],
   }
 
-  const labelConfig = data?.classification?.asset_type?.arguments?.label as string | undefined
-
   return (
     <SelectLocation
       assetTypeIconConfig={{
-        iconEntry: data?.classification?.asset_type?.arguments?.icon_entry as string | undefined,
-        iconFolder: data?.classification?.asset_type?.arguments?.icon_folder as string | undefined,
+        iconEntry: assetTypeConfig.iconEntry,
+        iconFolder: assetTypeConfig.iconFolder,
       }}
       coordinates={coordinates}
-      labelConfig={labelConfig}
-      maxAssets={data?.classification?.asset_type?.max_assets ?? MAX_ASSETS_FALLBACK}
+      labelConfig={assetTypeConfig.label}
+      maxAssets={assetTypeConfig.maxAssets}
       selectedAssets={selectedAssets}
       wfsQuery={{
-        assetTypeId: assetTypeId,
+        assetTypeId,
         classification: data?.classification?.name,
-        filter: data?.classification?.asset_type?.arguments?.filter as string | undefined,
-        srsName: data?.classification?.asset_type?.arguments?.srs_name as string | undefined,
-        typeNames: typeNames,
+        filter: assetTypeConfig.filter,
+        srsName: assetTypeConfig.srsName,
+        typeNames: assetTypeConfig.typeNames,
       }}
     />
   )
