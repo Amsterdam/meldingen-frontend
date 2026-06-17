cfe8019d8710fc6a19af121a83e8b81a90a34173
Aram <a.limpens@amsterdam.nl>
2026-06-17 10:47:05 +0200

    Add assets to Back Office and fetch term from asset type config

---
+++ apps/back-office/src/app/melding/[meldingId]/_utils/getAssetsData.ts
@@ -0,0 +1,21 @@
+import type { MeldingOutput } from '~/app/_api-client/proxy'
+
+import { getMeldingByMeldingIdAssets } from '~/app/_api-client/proxy'
+
+export const getAssetsData = async (data: MeldingOutput, meldingId: number) => {
+  const { data: assetIds, error: assetIdError } = await getMeldingByMeldingIdAssets({
+    path: { melding_id: meldingId },
+  })
+
+  if (assetIdError) {
+    // TODO: Log the error to an error reporting service
+    // eslint-disable-next-line no-console
+    console.error(assetIdError)
+    return { assets: [] }
+  }
+
+  return {
+    assets: assetIds,
+    assetTerm: data.classification?.asset_type?.arguments?.term as string | undefined,
+  }
+}
