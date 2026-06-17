4de902a91359b4a2c107546d75844178c593eaaa
Aram <a.limpens@amsterdam.nl>
2026-06-17 13:48:19 +0200

    Use consistent return type, use correct name

--- apps/back-office/src/app/melding/[meldingId]/_utils/getAssetsData.ts
+++ apps/back-office/src/app/melding/[meldingId]/_utils/getAssetsData.ts
@@ -3,19 +3,19 @@ import type { MeldingOutput } from '~/app/_api-client/proxy'
 import { getMeldingByMeldingIdAssets } from '~/app/_api-client/proxy'
 
 export const getAssetsData = async (data: MeldingOutput, meldingId: number) => {
-  const { data: assetIds, error: assetIdError } = await getMeldingByMeldingIdAssets({
+  const { data: assets, error: assetsError } = await getMeldingByMeldingIdAssets({
     path: { melding_id: meldingId },
   })
 
-  if (assetIdError) {
+  if (assetsError) {
     // TODO: Log the error to an error reporting service
     // eslint-disable-next-line no-console
-    console.error(assetIdError)
-    return { assets: [] }
+    console.error(assetsError)
+    return { assets: [], assetsTerm: undefined }
   }
 
   return {
-    assets: assetIds,
+    assets,
     assetsTerm: data.classification?.asset_type?.arguments?.term as string | undefined,
   }
 }
