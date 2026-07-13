843ab7e662ad1da3a513b654140fcf250f512eeb
Aram <a.limpens@amsterdam.nl>
2026-07-13 14:54:37 +0200

    Rename local state to align naming in other places

--- apps/back-office/src/app/_components/RichTextEditor/RichTextEditor.tsx
+++ apps/back-office/src/app/_components/RichTextEditor/RichTextEditor.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { CharacterCount as ADSCharacterCount } from '@amsterdam/design-system-react'
-import { CharacterCount, UndoRedo } from '@tiptap/extensions'
+import { CharacterCount as TipTapCharacterCount, UndoRedo } from '@tiptap/extensions'
 import { EditorContent, useEditor } from '@tiptap/react'
 import { useState } from 'react'
 
@@ -32,7 +32,7 @@ export const RichTextEditor = ({
   name,
 }: Props) => {
   const [content, setContent] = useState(() => markdownToJSON(defaultValue))
-  const [charactersCount, setCharactersCount] = useState<number>()
+  const [characterCount, setCharacterCount] = useState<number>()
   const [hasLoaded, setHasLoaded] = useState(false)
 
   const editor = useEditor({
@@ -50,18 +50,18 @@ export const RichTextEditor = ({
         role: 'textbox',
       },
     },
-    extensions: [...richTextExtensions, CharacterCount, UndoRedo],
+    extensions: [...richTextExtensions, TipTapCharacterCount, UndoRedo],
     immediatelyRender: false, // Don't render immediately on the server to avoid SSR issues
     onCreate: ({ editor: createdEditor }) => {
       // Pass around JSON rather than markdown so we can count characters more precisely on the server side
       // (markdown serialization can lose information, e.g. whitespace-only content).
       setContent(JSON.stringify(createdEditor.getJSON()))
-      setCharactersCount(createdEditor.storage.characterCount.characters())
+      setCharacterCount(createdEditor.storage.characterCount.characters())
       setHasLoaded(true)
     },
     onUpdate: ({ editor: updatedEditor }) => {
       setContent(JSON.stringify(updatedEditor.getJSON()))
-      setCharactersCount(updatedEditor.storage.characterCount.characters())
+      setCharacterCount(updatedEditor.storage.characterCount.characters())
     },
   })
 
@@ -78,8 +78,8 @@ export const RichTextEditor = ({
     <>
       <Toolbar editor={editor} id={id} />
       <EditorContent editor={editor} />
-      {charactersCount !== undefined && (
-        <ADSCharacterCount className={styles.characterCount} length={charactersCount} maxLength={MAX_NOTE_LENGTH} />
+      {characterCount !== undefined && (
+        <ADSCharacterCount className={styles.characterCount} length={characterCount} maxLength={MAX_NOTE_LENGTH} />
       )}
       <input name={name} type="hidden" value={content} />
     </>
