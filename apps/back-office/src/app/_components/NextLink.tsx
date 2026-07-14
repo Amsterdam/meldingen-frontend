6ed6ab1e07f48e16c1bbe89346951404665dd055
Aram <a.limpens@amsterdam.nl>
2026-07-14 16:25:30 +0200

    Update comment

--- apps/back-office/src/app/_components/NextLink.tsx
+++ apps/back-office/src/app/_components/NextLink.tsx
@@ -3,6 +3,6 @@ import type { AnchorHTMLAttributes } from 'react'
 
 import Link from 'next/link'
 
-// There is a type mismatch between the NextLink component and the TabNavigation.Link component.
+// There is a type mismatch between the NextLink component and Amsterdam Design System links.
 // This typecast is safe as long as we pass an href to the component we use it on.
 export const NextLink = Link as unknown as ComponentType<AnchorHTMLAttributes<HTMLAnchorElement>>
