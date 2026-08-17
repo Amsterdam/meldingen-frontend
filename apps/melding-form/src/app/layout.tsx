7887ad58ebc7bb945ccc1c0aacda2bac1c3288a8
Frank Filius <f.filius@amsterdam.nl>
2026-08-17 14:29:03 +0200

    Improve layout structure to make tests pass

--- apps/melding-form/src/app/layout.tsx
+++ apps/melding-form/src/app/layout.tsx
@@ -30,10 +30,10 @@ const RootLayout = async ({ children }: PropsWithChildren) => {
 
   return (
     <html dir="ltr" lang={locale}>
-      <ApiClientInitializer />
-      <NextIntlClientProvider>
-        <body className="ams-theme">{children}</body>
-      </NextIntlClientProvider>
+      <body className="ams-theme">
+        <ApiClientInitializer />
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
     </html>
   )
 }
