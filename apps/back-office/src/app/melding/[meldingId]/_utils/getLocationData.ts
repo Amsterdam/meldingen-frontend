291055e7bdd3cbd9b532fa022200e26388bb3a44
Kevin Wang <16762495+kyunwang@users.noreply.github.com>
2026-08-17 14:08:38 +0200

    Back Office adres copy for when there is only geolocation (#1400)

--- apps/back-office/src/app/melding/[meldingId]/_utils/getLocationData.ts
+++ apps/back-office/src/app/melding/[meldingId]/_utils/getLocationData.ts
@@ -5,11 +5,9 @@ import { getFullNLAddress } from '~/app/_utils/getFullNLAddress'
 export const getLocationData = (data: MeldingOutput, t: (key: string) => string) => {
   const address = getFullNLAddress(data)
 
-  if (!address) return undefined
-
   return [
     {
-      description: address,
+      description: address ?? t('detail.location.no-address'),
       key: 'address',
       term: t('detail.location.address'),
     },
