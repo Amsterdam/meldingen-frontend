fd9c3f822fb27ab4024011607933f8ee2724b280
kyunwang <kevin.wang@amsterdam.nl>
2026-08-24 16:47:08 +0200

    Remove 'server-only' as it prevents normal server usage outside of react/next context

--- apps/back-office/src/env/server.ts
+++ apps/back-office/src/env/server.ts
@@ -1,5 +1,9 @@
-import 'server-only'
-
 import { validateServerEnv } from './validate'
+
+if (typeof window !== 'undefined') {
+  throw new Error(
+    `server.ts should only be imported on the server side. \n Import env/validate directly if you need this outside of app code (tests, scripts)`,
+  )
+}
 
 export const serverEnv = validateServerEnv()
