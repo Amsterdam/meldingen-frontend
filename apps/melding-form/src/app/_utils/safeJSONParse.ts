e40d2d639778adefdf291fac62636048a542dcdf
Aram <a.limpens@amsterdam.nl>
2026-08-27 14:51:29 +0200

    Use shared function

--- apps/melding-form/src/app/_utils/safeJSONParse.ts
+++
@@ -1,9 +0,0 @@
-export const safeJSONParse = <T, F = undefined>(value: unknown, fallback: F): T | F => {
-  if (!value || typeof value !== 'string') return fallback
-
-  try {
-    return JSON.parse(value) as T
-  } catch {
-    return fallback
-  }
-}
