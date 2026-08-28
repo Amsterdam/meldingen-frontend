720ea4a146ced7164b568cc3e8c6cbbde61c30f3
kyunwang <kevin.wang@amsterdam.nl>
2026-08-28 10:34:51 +0200

    Add clarifying comment on client env validation

--- apps/back-office/src/env/validate.ts
+++ apps/back-office/src/env/validate.ts
@@ -22,6 +22,9 @@ export const validateServerEnv = () => {
   return Object.freeze(parsed.data)
 }
 
+// Next.js NEXT_PUBLIC varaibles are "inlined" at build time with hard-coded values.
+// Meaning, we need to reference them 1-by-1 compared to the server and auth env vars, which can be referenced as a whole object.
+// See https://nextjs.org/docs/app/guides/environment-variables#bundling-environment-variables-for-the-browser
 export const validateClientEnv = () => {
   const parsed = z.safeParse(clientSchema, {
     NEXT_PUBLIC_BACKEND_BASE_URL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
