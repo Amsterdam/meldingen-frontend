a131171fc5dbd830dbb940afeb0b04738b72566a
kyunwang <kevin.wang@amsterdam.nl>
2026-09-02 14:58:56 +0200

    file-upload: css correction and more explicit ButtonProp definition

--- libs/file-upload/src/FileUpload/FileUpload.tsx
+++ libs/file-upload/src/FileUpload/FileUpload.tsx
@@ -24,7 +24,7 @@ const isContainingFiles = (dataTransfer: DataTransfer): boolean => {
   return hasNoTypesInfo || isDraggingFiles
 }
 
-type ButtonProps = ComponentProps<typeof Button> & { text?: string }
+type ButtonProps = { text?: string; variant: ComponentProps<typeof Button>['variant'] }
 
 type Props = InputHTMLAttributes<HTMLInputElement> & {
   button?: ButtonProps
