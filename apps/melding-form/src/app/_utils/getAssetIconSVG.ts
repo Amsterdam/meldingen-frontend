23555f888767a2441ca8b10020672cc3f054fe37
kyunwang <kevin.wang@amsterdam.nl>
2026-09-07 11:04:25 +0200

    Make const naming more targeted & replace relevant  magic strings with it

--- apps/melding-form/src/app/_utils/getAssetIconSVG.ts
+++ apps/melding-form/src/app/_utils/getAssetIconSVG.ts
@@ -1,8 +1,10 @@
 import type { AssetItem } from '~/app/(general)/_utils/formatAssetItem'
 
+import { ASSET_FALLBACK_SRC } from '~/constants'
+
 const formatIconPath = (icon: AssetItem['icon'], assetTypeName?: string): string => {
   if (!icon?.folder || !assetTypeName) {
-    return '/asset-fallback.svg'
+    return ASSET_FALLBACK_SRC
   }
 
   return `/${icon.folder}/${assetTypeName.toLowerCase()}.svg`
