ebe0ac038374f0a5d6ea123c763012a9b767a470
kyunwang <kevin.wang@amsterdam.nl>
2026-09-16 11:38:39 +0200

    Enable BO auth env variables validation - This was previously disabled because the pipeline had a misconfiguration to sort out first

--- apps/back-office/src/instrumentation.ts
+++ apps/back-office/src/instrumentation.ts
@@ -1,8 +1,7 @@
 export const register = async () => {
-  const { validateClientEnv, validateServerEnv } = await import('./env/validate')
+  const { validateAuthEnv, validateClientEnv, validateServerEnv } = await import('./env/validate')
 
   validateServerEnv()
-  // TODO: Temporary commented out due to pipeline settings injecting both Entra & Keycloak env vars, which causes validation to fail.
-  // validateAuthEnv()
+  validateAuthEnv()
   validateClientEnv()
 }
