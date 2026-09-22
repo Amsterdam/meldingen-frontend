18901339beb0306572623adc800491e6d311c1e6
Frank Filius <f.filius@amsterdam.nl>
2026-09-22 14:08:44 +0200

    Add rule that warns if missing lines between statements and auto-fixes warning on save

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -141,6 +141,29 @@ export default defineConfig(
           returnStyle: 'implicit',
         },
       ],
+      'padding-line-between-statements': [
+        'warn',
+        {
+          blankLine: 'always',
+          prev: '*',
+          next: ['block', 'block-like', 'return'],
+        },
+        {
+          blankLine: 'always',
+          prev: ['block', 'block-like'],
+          next: '*',
+        },
+        {
+          blankLine: 'always',
+          prev: ['const', 'let', 'var'],
+          next: '*',
+        },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var'],
+        },
+      ],
 
       // Perfectionist
       'perfectionist/sort-imports': ['error', perfectionistImportGroups],
