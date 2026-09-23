39f87b786d647ff0a553958e7af1e40b5569449c
kyunwang <kevin.wang@amsterdam.nl>
2026-09-23 09:00:38 +0200

    Remove redundant getClientEnv()

--- apps/admin/src/env/clientEnv.ts
+++ apps/admin/src/env/clientEnv.ts
@@ -1,6 +1,3 @@
 import { validateClientEnv } from './validateClientEnv.ts'
 
-// Accommodates tests env stubbing
-export const getClientEnv = () => validateClientEnv(import.meta.env)
-
-export const clientEnv = getClientEnv()
+export const clientEnv = validateClientEnv(import.meta.env)
